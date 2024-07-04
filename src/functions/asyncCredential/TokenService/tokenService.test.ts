19c97f059d7b790dd8acc11f93ffb3a09ff75a4d
Jason Humbert <jason.humbert@digital.cabinet-office.gov.uk>
2024-07-04 09:58:38 +0100

    DCMAW-8264: Create jwt test data

--- src/functions/asyncCredential/TokenService/tokenService.test.ts
+++ src/functions/asyncCredential/TokenService/tokenService.test.ts
@@ -3,6 +3,9 @@ import { LogOrValue, log, value } from "../../types/logOrValue";
 import { mockClient } from "aws-sdk-client-mock";
 import format from "ecdsa-sig-formatter";
 
+// Generated using jwt.io with thier public and private keys
+const mockJwt = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.tyh-VfuzIxCyGYDlkBA7DfyjrqmSHu6pQ2hoZuFqUSLPNY2N0mpHb3nk5K17HWP_3cYHBw7AhHale5wky6-sVA"
+
 describe("Token Service", () => {
   describe("Given the token signature is not valid", () => {
     it("Returns a log", async () => {
@@ -11,7 +14,7 @@ describe("Token Service", () => {
       const tokenService = new TokenService();
       const result = await tokenService.verifyTokenSignature(
         "mockKeyId",
-        "TO-DO - INSERT MOCK JWT HERE",
+        mockJwt,
       );
       expect(result.isLog).toBe(true);
       expect(result.value).toEqual("TOKEN_SIGNATURE_INVALID");
