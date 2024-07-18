1fa88cc1ca4b30f9542024bd1c22cffd3feb769e
sandy-may <115020902+sandy-may@users.noreply.github.com>
2024-07-18 13:42:48 +0100

    DCMAW-8264: Log when jwt claims are invalid (#28)
    ​DCMAW-8264
    ### What changed
    Logs when JWT claims are missing or do not pass validation
    https://govukverify.atlassian.net/wiki/spaces/DCMAW/pages/3917447301/Strategic+App+DCMAW-7690+Implementing+Client+Credentials+Grant+Flow+and+Asynchronous+CRI+credential+requests
    
    ### Why did it change
    Observability
    
    ## Checklists
    <!-- Merging this PR is effectively deploying to production. Be mindful
    to answer accurately. -->
    
    - [x] There is a ticket raised for this PR that is present in the branch
    name
    - [x] No PII data logged. [See guidance
    here](https://govukverify.atlassian.net/wiki/spaces/DCMAW/pages/3502407722/PII+Logging+Considerations)
    - [ ] Demo to a BA, TA, and the team.
    - [x] Update [README](./blob/main/README.md) with any new instructions
    or tasks

--- src/functions/asyncCredential/registeredLogs.ts
+++ src/functions/asyncCredential/registeredLogs.ts
@@ -4,11 +4,17 @@ import {
 } from "../services/logging/commonRegisteredLogs";
 import { RegisteredLogMessages } from "../services/logging/types";
 
-export type MessageName = "AUTHENTICATION_HEADER_INVALID" | CommonMessageNames;
+export type MessageName =
+  | "AUTHENTICATION_HEADER_INVALID"
+  | "JWT_CLAIM_INVALID"
+  | CommonMessageNames;
 
 export const registeredLogs: RegisteredLogMessages<MessageName> = {
   AUTHENTICATION_HEADER_INVALID: {
     messageCode: "MOBILE_ASYNC_AUTHENTICATION_HEADER_INVALID",
+  },
+  JWT_CLAIM_INVALID: {
+    messageCode: "MOBILE_ASYNC_JWT_CLAIM_INVALID",
   },
   ...commonMessages,
 };
