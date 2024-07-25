4e646f5e568e4339492b3b8c900161b38d7c1441
James Mooney <93214884+jamesamooney@users.noreply.github.com>
2024-07-25 15:14:15 +0100

    DCMAW-9802: Move token claim validation into token service (#43)

--- backend-api/src/functions/asyncCredential/TokenService/tokenService.ts
+++ backend-api/src/functions/asyncCredential/TokenService/tokenService.ts
@@ -5,15 +5,32 @@ import {
   errorResponse,
   successResponse,
 } from "../../types/errorOrValue";
+import { IJwtPayload } from "../../types/jwt";
 
-export interface IVerifyTokenSignature {
-  verifyTokenSignature: (
-    keyId: string,
-    jwt: string,
-  ) => Promise<ErrorOrSuccess<null>>;
-}
+export class TokenService implements IDecodeToken, IVerifyTokenSignature {
+  getDecodedToken(config: IDecodeTokenConfig): ErrorOrSuccess<IDecodedToken> {
+    const encodedJwt = config.authorizationHeader.split(" ")[1];
 
-export class TokenService implements IVerifyTokenSignature {
+    const decodedJwtOrError = this.decodeToken(encodedJwt);
+    if (decodedJwtOrError.isError) {
+      return errorResponse(decodedJwtOrError.value as string);
+    }
+    const jwtPayload = decodedJwtOrError.value as IJwtPayload;
+
+    const validClaimsOrError = this.validateTokenClaims(
+      jwtPayload,
+      config.issuer,
+    );
+    if (validClaimsOrError.isError) {
+      return errorResponse(validClaimsOrError.value as string);
+    }
+
+    return successResponse({
+      encodedJwt,
+      jwtPayload,
+    });
+  }
+
   async verifyTokenSignature(
     keyId: string,
     jwt: string,
@@ -34,4 +51,92 @@ export class TokenService implements IVerifyTokenSignature {
 
     return successResponse(null);
   }
+
+  private decodeToken(encodedJwt: string): ErrorOrSuccess<IJwtPayload> {
+    const payload = encodedJwt.split(".")[1];
+    let jwtPayload;
+    try {
+      jwtPayload = JSON.parse(Buffer.from(payload, "base64").toString("utf-8"));
+    } catch (error) {
+      return errorResponse("JWT payload not valid JSON");
+    }
+
+    return successResponse(jwtPayload);
+  }
+
+  private validateTokenClaims(
+    jwtPayload: IJwtPayload,
+    issuer: string,
+  ): ErrorOrSuccess<null> {
+    if (!jwtPayload.exp) {
+      return errorResponse("Missing exp claim");
+    }
+
+    if (jwtPayload.exp <= Math.floor(Date.now() / 1000)) {
+      return errorResponse("exp claim is in the past");
+    }
+
+    if (jwtPayload.iat) {
+      if (jwtPayload.iat >= Math.floor(Date.now() / 1000)) {
+        return errorResponse("iat claim is in the future");
+      }
+    }
+
+    if (jwtPayload.nbf) {
+      if (jwtPayload.nbf >= Math.floor(Date.now() / 1000)) {
+        return errorResponse("nbf claim is in the future");
+      }
+    }
+
+    if (!jwtPayload.iss) {
+      return errorResponse("Missing iss claim");
+    }
+
+    if (jwtPayload.iss !== issuer) {
+      return errorResponse(
+        "iss claim does not match ISSUER environment variable",
+      );
+    }
+
+    if (!jwtPayload.scope) {
+      return errorResponse("Missing scope claim");
+    }
+
+    if (jwtPayload.scope !== "dcmaw.session.async_create") {
+      return errorResponse("Invalid scope claim");
+    }
+
+    if (!jwtPayload.client_id) {
+      return errorResponse("Missing client_id claim");
+    }
+
+    if (!jwtPayload.aud) {
+      return errorResponse("Missing aud claim");
+    }
+
+    return successResponse(null);
+  }
+}
+
+export interface IDecodeToken {
+  getDecodedToken: (
+    config: IDecodeTokenConfig,
+  ) => ErrorOrSuccess<{ encodedJwt: string; jwtPayload: IJwtPayload }>;
+}
+
+export interface IVerifyTokenSignature {
+  verifyTokenSignature: (
+    keyId: string,
+    jwt: string,
+  ) => Promise<ErrorOrSuccess<null>>;
+}
+
+export interface IDecodedToken {
+  encodedJwt: string;
+  jwtPayload: IJwtPayload;
+}
+
+export interface IDecodeTokenConfig {
+  authorizationHeader: string;
+  issuer: string;
 }
