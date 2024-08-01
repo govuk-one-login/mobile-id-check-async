b2ee8f171823fba8f1ee39946c5482353f81d2cc
Jason Humbert <jason.humbert@digital.cabinet-office.gov.uk>
2024-08-01 12:50:08 +0100

    DCMAW-9665: WIP - add pact test file

---
+++ backend-api/src/functions/tests/pact/pact.test.ts
@@ -0,0 +1,47 @@
+import { Application } from "express";
+import { createApp } from "./createApp";
+import { Verifier } from "@pact-foundation/pact";
+import path from "path";
+
+describe("Provider API contract verification", () => {
+  const port: number = 2025;
+  let app: Application;
+  let server: any;
+
+  beforeAll(async () => {
+    app = await createApp();
+
+    server = app.listen(port, () => {
+      console.log(`Server listening on port ${port}.`);
+    });
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it("validates adherence to all consumer contracts", () => {
+    const verifier = new Verifier({
+      provider: "DcmawCriProvider",
+      logLevel: "info",
+      pactUrls: [
+        path.resolve(
+          process.cwd(),
+          "./pactFiles/IpvCoreBack-DcmawCriProvider.json",
+        ),
+      ],
+      providerBaseUrl: `http://localhost:${port}`,
+    });
+
+    // Return the promise to Jest
+    return verifier
+      .verifyProvider()
+      .then((output) => {
+        console.log(output);
+      })
+      .catch((error) => {
+        console.error("Failed to verify pacts", error);
+        throw error; // Rethrow to ensure Jest marks the test as failed
+      });
+  });
+});
