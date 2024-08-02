f8ee25c0f5e5b5e0bed38036b20dfbb1498bde54
Douglas Thomson <douglas.thomson@digital.cabinet-office.gov.uk>
2024-08-02 10:25:37 +0100

    Test app deployment

--- infra/terraform/secure_pipelines/main.tf
+++ infra/terraform/secure_pipelines/main.tf
@@ -34,59 +34,8 @@ locals {
     prod        = { account_id = "339712924890" }
   }
 
-  parameters = {
-    mob_async_backend_pl = {
-      dev = {
-        OneLoginRepositoryName = "mobile-id-check-async"
 
-        IncludePromotion = "No"
 
-        SigningProfileArn        = one(data.aws_cloudformation_stack.signer_dev[*].outputs["SigningProfileArn"])
-        SigningProfileVersionArn = one(data.aws_cloudformation_stack.signer_dev[*].outputs["SigningProfileVersionArn"])
-      }
-
-      build = {
-        OneLoginRepositoryName = "mobile-id-check-async"
-
-        IncludePromotion = "Yes"
-        AllowedAccounts  = local.account_vars.staging.account_id
-
-        SigningProfileArn        = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileArn"])
-        SigningProfileVersionArn = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileVersionArn"])
-      }
-
-      staging = {
-        ArtifactSourceBucketArn                 = one(data.aws_cloudformation_stack.mob_async_backend_pl_build[*].outputs["ArtifactPromotionBucketArn"])
-        ArtifactSourceBucketEventTriggerRoleArn = one(data.aws_cloudformation_stack.mob_async_backend_pl_build[*].outputs["ArtifactPromotionBucketEventTriggerRoleArn"])
-
-        IncludePromotion = "Yes"
-        AllowedAccounts  = join(",", [local.account_vars.integration.account_id, local.account_vars.prod.account_id])
-
-        SigningProfileArn        = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileArn"])
-        SigningProfileVersionArn = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileVersionArn"])
-      }
-
-      integration = {
-        ArtifactSourceBucketArn                 = one(data.aws_cloudformation_stack.mob_async_backend_pl_staging[*].outputs["ArtifactPromotionBucketArn"])
-        ArtifactSourceBucketEventTriggerRoleArn = one(data.aws_cloudformation_stack.mob_async_backend_pl_staging[*].outputs["ArtifactPromotionBucketEventTriggerRoleArn"])
-
-        IncludePromotion = "No"
-
-        SigningProfileArn        = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileArn"])
-        SigningProfileVersionArn = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileVersionArn"])
-      }
-
-      prod = {
-        ArtifactSourceBucketArn                 = one(data.aws_cloudformation_stack.mob_async_backend_pl_staging[*].outputs["ArtifactPromotionBucketArn"])
-        ArtifactSourceBucketEventTriggerRoleArn = one(data.aws_cloudformation_stack.mob_async_backend_pl_staging[*].outputs["ArtifactPromotionBucketEventTriggerRoleArn"])
-
-        IncludePromotion = "No"
-
-        SigningProfileArn        = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileArn"])
-        SigningProfileVersionArn = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileVersionArn"])
-      }
-    }
-  }
 }
 
 data "aws_cloudformation_stack" "mob_async_backend_pl_build" {
@@ -111,31 +60,4 @@ data "aws_cloudformation_stack" "signer_build" {
   provider = aws.build
   count    = contains(["build", "staging", "integration", "prod"], var.environment) ? 1 : 0
   name     = "devplatform-signer"
-}
-
-
-resource "aws_cloudformation_stack" "mob_async_backend_pl" {
-  name = "mob-async-backend-pl"
-
-  template_url = format(local.preformat_template_url,
-    "sam-deploy-pipeline",             # https://github.com/govuk-one-login/devplatform-deploy/tree/main/sam-deploy-pipeline
-    "NGOxM_hPmNoY1pTz6vo8P_El2EqVz6sa" # v2.61.1
-  )
-
-  parameters = merge(
-    {
-      Environment  = var.environment
-      SAMStackName = "mob-async-backend"
-
-      VpcStackName                     = "devplatform-vpc"
-      CustomKmsKeyArns                 = "arn:aws:kms:eu-west-2:216552277552:key/4bc58ab5-c9bb-4702-a2c3-5d339604a8fe" # To support Dynatrace
-      AdditionalCodeSigningVersionArns = "arn:aws:signer:eu-west-2:216552277552:/signing-profiles/DynatraceSigner/5uwzCCGTPq"
-
-      BuildNotificationStackName = "devplatform-build-notifications"
-      SlackNotificationType      = "All"
-    },
-    local.parameters.mob_async_backend_pl[var.environment]
-  )
-
-  capabilities = ["CAPABILITY_NAMED_IAM"]
 }
---
+++ infra/terraform/secure_pipelines/mob_async_backend_pl.tf
@@ -0,0 +1,85 @@
+resource "aws_cloudformation_stack" "mob_async_backend_pl" {
+  name = "mob-async-backend-pl"
+
+  template_url = format(local.preformat_template_url,
+    "sam-deploy-pipeline",             # https://github.com/govuk-one-login/devplatform-deploy/tree/main/sam-deploy-pipeline
+    "NGOxM_hPmNoY1pTz6vo8P_El2EqVz6sa" # v2.61.1
+  )
+
+  // Default parameters. Can be overwritten by using the locals below.
+  parameters = merge(
+    {
+      Environment  = var.environment
+      SAMStackName = "mob-async-backend"
+
+      VpcStackName                     = "devplatform-vpc"
+      CustomKmsKeyArns                 = "arn:aws:kms:eu-west-2:216552277552:key/4bc58ab5-c9bb-4702-a2c3-5d339604a8fe" # To support Dynatrace
+      AdditionalCodeSigningVersionArns = "arn:aws:signer:eu-west-2:216552277552:/signing-profiles/DynatraceSigner/5uwzCCGTPq"
+
+      BuildNotificationStackName = "devplatform-build-notifications"
+      SlackNotificationType      = "All"
+    },
+    local.mob_async_backend_pl[var.environment]
+  )
+
+  capabilities = ["CAPABILITY_NAMED_IAM"]
+}
+
+locals {
+    // Define environemnt specific parameters here. Will be merged and take precedence over the
+    // parameters defined in the resource above.
+    // https://developer.hashicorp.com/terraform/language/functions/merge
+    mob_async_backend_pl = {
+      dev = {
+        OneLoginRepositoryName = "mobile-id-check-async"
+
+        IncludePromotion = "No"
+
+        SigningProfileArn        = one(data.aws_cloudformation_stack.signer_dev[*].outputs["SigningProfileArn"])
+        SigningProfileVersionArn = one(data.aws_cloudformation_stack.signer_dev[*].outputs["SigningProfileVersionArn"])
+      }
+
+      build = {
+        OneLoginRepositoryName = "mobile-id-check-async"
+
+        IncludePromotion = "Yes"
+        AllowedAccounts  = local.account_vars.staging.account_id
+
+        SigningProfileArn        = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileArn"])
+        SigningProfileVersionArn = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileVersionArn"])
+      }
+
+      staging = {
+        ArtifactSourceBucketArn                 = one(data.aws_cloudformation_stack.mob_async_backend_pl_build[*].outputs["ArtifactPromotionBucketArn"])
+        ArtifactSourceBucketEventTriggerRoleArn = one(data.aws_cloudformation_stack.mob_async_backend_pl_build[*].outputs["ArtifactPromotionBucketEventTriggerRoleArn"])
+
+        IncludePromotion = "Yes"
+        AllowedAccounts  = join(",", [local.account_vars.integration.account_id, local.account_vars.prod.account_id])
+
+        SigningProfileArn        = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileArn"])
+        SigningProfileVersionArn = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileVersionArn"])
+      }
+
+      integration = {
+        ArtifactSourceBucketArn                 = one(data.aws_cloudformation_stack.mob_async_backend_pl_staging[*].outputs["ArtifactPromotionBucketArn"])
+        ArtifactSourceBucketEventTriggerRoleArn = one(data.aws_cloudformation_stack.mob_async_backend_pl_staging[*].outputs["ArtifactPromotionBucketEventTriggerRoleArn"])
+
+        IncludePromotion = "No"
+
+        SigningProfileArn        = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileArn"])
+        SigningProfileVersionArn = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileVersionArn"])
+      }
+
+      prod = {
+        Environment = "production"
+
+        ArtifactSourceBucketArn                 = one(data.aws_cloudformation_stack.mob_async_backend_pl_staging[*].outputs["ArtifactPromotionBucketArn"])
+        ArtifactSourceBucketEventTriggerRoleArn = one(data.aws_cloudformation_stack.mob_async_backend_pl_staging[*].outputs["ArtifactPromotionBucketEventTriggerRoleArn"])
+
+        IncludePromotion = "No"
+
+        SigningProfileArn        = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileArn"])
+        SigningProfileVersionArn = one(data.aws_cloudformation_stack.signer_build[*].outputs["SigningProfileVersionArn"])
+      }
+    }
+}
