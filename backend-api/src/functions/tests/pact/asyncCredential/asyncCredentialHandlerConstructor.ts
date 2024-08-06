380d4f62a4bcbc8283fd48a5416a428779203f99
James Andrew Mooney <james.andrew-mooney@digital.cabinet-office.gov.uk>
2024-08-06 14:22:02 +0100

    Remove unused import

--- backend-api/src/functions/tests/pact/asyncCredential/asyncCredentialHandlerConstructor.ts
+++ backend-api/src/functions/tests/pact/asyncCredential/asyncCredentialHandlerConstructor.ts
@@ -1,14 +1,12 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { lambdaHandlerConstructor } from "../../../asyncCredential/asyncCredentialHandler";
 import { buildRequest } from "../../../testUtils/mockRequest";
-import { buildLambdaContext } from "../../../testUtils/mockContext";
 import { Dependencies } from "../../../asyncCredential/handlerDependencies";
 
 export async function asyncCredentialHandlerConstructor(
   requestConfig: IAsyncCredentialHandlerConfig,
 ): Promise<APIGatewayProxyResult> {
   const event: APIGatewayProxyEvent = buildRequest(requestConfig);
-  const context = buildLambdaContext();
 
   return await lambdaHandlerConstructor(
     requestConfig.dependencies,
