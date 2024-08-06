91991dff5159ab4df0dab8a6298d057bd5d6e356
Jason Humbert <jason.humbert@digital.cabinet-office.gov.uk>
2024-08-06 12:31:35 +0100

    DCMAW-9664: Add AsyncCredentialStateConfiguration

---
+++ backend-api/src/functions/tests/pact/asyncCredential/asyncCredentialStateConfiguration.ts
@@ -0,0 +1,57 @@
+import { Dependencies } from "../../../asyncCredential/handlerDependencies";
+import { registeredLogs } from "../../../asyncCredential/registeredLogs";
+import { MockEventWriterSuccess } from "../../../services/events/tests/mocks";
+import { Logger } from "../../../services/logging/logger";
+import { MockLoggingAdapter } from "../../../services/logging/tests/mockLogger";
+import { MockClientRegistryServiceGetPartialClientSuccessResult, MockSessionServiceCreateSessionSuccessResult, MockTokenServiceSuccess } from "../../../testUtils/asyncCredentialMocks";
+
+const env = {
+  SIGNING_KEY_ID: "mockKid",
+  ISSUER: "mockIssuer",
+  SESSION_TABLE_NAME: "mockTableName",
+  SESSION_TABLE_SUBJECT_IDENTIFIER_INDEX_NAME: "mockIndexName",
+  SESSION_TTL_IN_MILLISECONDS: "12345",
+  SQS_QUEUE: "mockSqsQueue",
+  CLIENT_REGISTRY_PARAMETER_NAME: "mockParmaterName",
+}
+
+export class AsyncCredentialStateConfiguration {
+  secret: string = "";
+  dependencies: Dependencies = {
+    env,
+    eventService: () => new MockEventWriterSuccess(),
+    logger: () => new Logger(new MockLoggingAdapter(), registeredLogs),
+    clientRegistryService: () => new MockClientRegistryServiceGetPartialClientSuccessResult(),
+    tokenService: () => new MockTokenServiceSuccess(),
+    sessionService: () => new MockSessionServiceCreateSessionSuccessResult(env.SESSION_TABLE_NAME, env.SESSION_TABLE_SUBJECT_IDENTIFIER_INDEX_NAME),
+  };
+
+  get dependenciesValue() {
+    return this.dependencies;
+  }
+
+  set dependenciesValue(value: Dependencies) {
+    this.dependencies = value;
+  }
+
+  resetToPassingDependencies() {
+    this.dependencies = {
+      env: {
+        SIGNING_KEY_ID: "mockKid",
+        ISSUER: "mockIssuer",
+        SESSION_TABLE_NAME: "mockTableName",
+        SESSION_TABLE_SUBJECT_IDENTIFIER_INDEX_NAME: "mockIndexName",
+        SESSION_TTL_IN_MILLISECONDS: "12345",
+        SQS_QUEUE: "mockSqsQueue",
+        CLIENT_REGISTRY_PARAMETER_NAME: "mockParmaterName",
+      },
+      eventService: () => new MockEventWriterSuccess(),
+      logger: () => new Logger(new MockLoggingAdapter(), registeredLogs),
+      clientRegistryService: () => new MockClientRegistryServiceGetPartialClientSuccessResult(),
+      tokenService: () => new MockTokenServiceSuccess(),
+      sessionService: () => new MockSessionServiceCreateSessionSuccessResult("mockTableName", "mockIndexName"),
+    };
+  }
+}
+
+export const asyncCredentialStateConfig = new AsyncCredentialStateConfiguration();
