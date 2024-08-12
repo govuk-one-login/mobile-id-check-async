d92f85788c3547d8c998e8d29054588c42e74f0b
Sandy May <115020902+sandy-may@users.noreply.github.com>
2024-08-12 15:47:17 +0100

    DCMAW-9664: Sonar implementation duplication

--- backend-api/src/functions/asyncCredential/tokenService/tests/mocks.ts
+++ backend-api/src/functions/asyncCredential/tokenService/tests/mocks.ts
@@ -5,6 +5,18 @@ import {
   IVerifyTokenSignature,
 } from "../tokenService";
 
+const getDecodedTokenSuccessResult = {
+  encodedJwt:
+    "eyJhbGciOiJIUzI1NiIsInR5cGUiOiJKV1QifQ.eyJleHAiOjE3MjE5MDExNDMwMDAsImlzcyI6Im1vY2tJc3N1ZXIiLCJhdWQiOiJtb2NrSXNzdWVyIiwic2NvcGUiOiJkY21hdy5zZXNzaW9uLmFzeW5jX2NyZWF0ZSIsImNsaWVudF9pZCI6Im1vY2tDbGllbnRJZCJ9.Ik_kbkTVKzlXadti994bAtiHaFO1KsD4_yJGt4wpjr8",
+  jwtPayload: {
+    aud: "mockIssuer",
+    client_id: "mockClientId",
+    exp: 1721901143000,
+    iss: "mockIssuer",
+    scope: "dcmaw.session.async_create",
+  },
+}
+
 export class MockTokenServiceGetDecodedTokenErrorResult
   implements IDecodeToken, IVerifyTokenSignature
 {
@@ -23,17 +35,7 @@ export class MockTokenServiceInvalidSignatureErrorResult
   implements IDecodeToken, IVerifyTokenSignature
 {
   getDecodedToken(): Result<IDecodedToken> {
-    return successResult({
-      encodedJwt:
-        "eyJhbGciOiJIUzI1NiIsInR5cGUiOiJKV1QifQ.eyJleHAiOjE3MjE5MDExNDMwMDAsImlzcyI6Im1vY2tJc3N1ZXIiLCJhdWQiOiJtb2NrSXNzdWVyIiwic2NvcGUiOiJkY21hdy5zZXNzaW9uLmFzeW5jX2NyZWF0ZSIsImNsaWVudF9pZCI6Im1vY2tDbGllbnRJZCJ9.Ik_kbkTVKzlXadti994bAtiHaFO1KsD4_yJGt4wpjr8",
-      jwtPayload: {
-        aud: "mockIssuer",
-        client_id: "mockClientId",
-        exp: 1721901143000,
-        iss: "mockIssuer",
-        scope: "dcmaw.session.async_create",
-      },
-    });
+    return successResult(getDecodedTokenSuccessResult);
   }
   verifyTokenSignature(): Promise<Result<null>> {
     return Promise.resolve(
@@ -49,17 +51,7 @@ export class MockTokenServiceSuccess
   implements IDecodeToken, IVerifyTokenSignature
 {
   getDecodedToken(): Result<IDecodedToken> {
-    return successResult({
-      encodedJwt:
-        "eyJhbGciOiJIUzI1NiIsInR5cGUiOiJKV1QifQ.eyJleHAiOjE3MjE5MDExNDMwMDAsImlzcyI6Im1vY2tJc3N1ZXIiLCJhdWQiOiJtb2NrSXNzdWVyIiwic2NvcGUiOiJkY21hdy5zZXNzaW9uLmFzeW5jX2NyZWF0ZSIsImNsaWVudF9pZCI6Im1vY2tDbGllbnRJZCJ9.Ik_kbkTVKzlXadti994bAtiHaFO1KsD4_yJGt4wpjr8",
-      jwtPayload: {
-        aud: "mockIssuer",
-        client_id: "mockClientId",
-        exp: 1721901143000,
-        iss: "mockIssuer",
-        scope: "dcmaw.session.async_create",
-      },
-    });
+    return successResult(getDecodedTokenSuccessResult);
   }
   verifyTokenSignature(): Promise<Result<null>> {
     return Promise.resolve(successResult(null));
