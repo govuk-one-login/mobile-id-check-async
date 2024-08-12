9bf2887800e2c76a08ac61fbd29595e2d4db9a2c
Sandy May <115020902+sandy-may@users.noreply.github.com>
2024-08-12 20:49:30 +0100

    DCMAW-96606: fix timestamp bug

--- backend-api/src/functions/services/events/eventService.ts
+++ backend-api/src/functions/services/events/eventService.ts
@@ -52,7 +52,7 @@ export class EventService implements IEventService {
         session_id: eventConfig.sessionId,
         govuk_signin_journey_id: eventConfig.govukSigninJourneyId,
       },
-      timestamp: eventConfig.getNowInMilliseconds(),
+      timestamp: Math.floor(eventConfig.getNowInMilliseconds()/1000),
       event_name: eventConfig.eventName,
       component_id: eventConfig.componentId,
     };
