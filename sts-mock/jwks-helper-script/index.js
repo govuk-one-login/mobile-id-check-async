b01d24d957a815c034989471a25b38da2f49a1c3
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-08-16 10:26:46 +0100

    chore(DCMAW-9887): delete files not required for this PR

--- sts-mock/jwks-helper-script/index.js
+++ sts-mock/jwks-helper-script/index.js
@@ -1,15 +1,20 @@
-import { generateKeyPairSync } from "node:crypto";
+// import { generateKeyPairSync } from "node:crypto";
+import { generateKeyPair, exportJWK } from "jose";
 
-const { privateKey, publicKey } = generateKeyPairSync('rsa', {
-    modulusLength: 2048,
-    publicKeyEncoding: {
-        type: 'spki',
-        format: 'jwk'
-    },
-    privateKeyEncoding: {
-        type: 'pkcs8',
-        format: 'jwk'
-    }
+// const { privateKey, publicKey } = generateKeyPairSync('ec', {
+//     namedCurve: 'secp256r1', // Implementing options
+//     publicKeyEncoding: {
+//         type: 'spki',
+//         format: 'jwk'
+//     },
+//     privateKeyEncoding: {
+//         type: 'pkcs8',
+//         format: 'jwk'
+//     }
+// })
+
+const keyPair = await generateKeyPair('ES256', {
+    extractable: true,
 })
-
-console.log(privateKey, publicKey)
+console.log(await exportJWK(keyPair.privateKey))
+console.log(await exportJWK(keyPair.publicKey))
