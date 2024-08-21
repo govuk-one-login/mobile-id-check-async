a231de576a7fcb48fb2365e487e4e22cc2cc8cca
Matthew Barber <matthew.barber@digital.cabinet-office.gov.uk>
2024-08-21 13:19:00 +0100

    DCMAW-9930 Remove template config from main.tf as per PR comments

--- infra/terraform/base_stacks_2/main.tf
+++ infra/terraform/base_stacks_2/main.tf
@@ -1,9 +1,5 @@
 locals {
-  template_bucket        = data.aws_ssm_parameter.template_storage_bucket.insecure_value
-  preformat_template_url = "https://${data.aws_ssm_parameter.template_storage_bucket.insecure_value}.s3.amazonaws.com/%s/template.yaml?versionId=%s"
-
-  is_artifact_account = contains(["dev", "build"], var.environment)
-
+  
   account_vars = {
     dev         = { account_id = "211125300205" }
     build       = { account_id = "058264551042" }
