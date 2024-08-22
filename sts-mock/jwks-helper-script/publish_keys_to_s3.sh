5470288298771f9b90f83a22788be25a388a0517
Ana Hickmann <hickmann.al@gmail.com>
2024-08-22 11:53:28 +0100

    feat(DCMAW-8966): script to generate and publish key pair to S3

---
+++ sts-mock/jwks-helper-script/publish_keys_to_s3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PRIVATE_KEY_FILE_NAME="private-key.json";
+JWKS_FILE_NAME="jwks.json";
+
+aws sts get-caller-identity >/dev/null 2>&1
+if [ $? -gt 0 ]; then
+  echo "Not logged in to an AWS Account."
+  exit
+fi
+
+if [ $# != 1 ]; then
+  echo "Incorrect number of parameters supplied. Supply the stack name." 1>&2
+  exit 1
+fi
+
+echo "Generating keys"
+npm install
+node jwksHelperScript.mjs $PRIVATE_KEY_FILE_NAME $JWKS_FILE_NAME
+
+STACK_NAME="$1"
+
+echo "Uploading keys"
+aws s3 cp $PRIVATE_KEY_FILE_NAME s3://"$STACK_NAME"-jwks/
+aws s3 cp $JWKS_FILE_NAME s3://"$STACK_NAME"-jwks/.well-known/
