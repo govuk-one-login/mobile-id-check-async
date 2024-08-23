4797368079afe06d1c02eb5e0daace6fb2616792
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2024-08-23 17:05:20 +0100

    Remove useless env var test

--- backend-api/src/functions/asyncCredential/asyncCredentialHandler.test.ts
+++ backend-api/src/functions/asyncCredential/asyncCredentialHandler.test.ts
@@ -39,7 +39,6 @@ const env = {
   SIGNING_KEY_ID: "mockKid",
   ISSUER: "mockIssuer",
   SESSION_TABLE_NAME: "mockTableName",
-  SESSION_TABLE_SUBJECT_IDENTIFIER_INDEX_NAME: "mockIndexName",
   SESSION_TTL_IN_MILLISECONDS: "12345",
   TXMA_SQS: "mockSqsQueue",
   CLIENT_REGISTRY_SECRET_NAME: "mockParmaterName",
