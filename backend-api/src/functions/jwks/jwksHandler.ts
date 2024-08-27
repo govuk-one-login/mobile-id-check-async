d5d0a2fe67c01f491ee1bc6e4f3fafbea591c3c5
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-08-27 18:19:31 +0100

    feat(DCMAW-10041): fix issue causing lambda to have no code

--- backend-api/src/functions/jwks/jwksHandler.ts
+++ backend-api/src/functions/jwks/jwksHandler.ts
@@ -2,63 +2,59 @@ import { CloudFormationCustomResourceEvent, Context } from "aws-lambda";
 import { ConfigService } from "./configService/configService";
 import { dependencies, IJwksDependencies } from "./handlerDependencies";
 
-export const lambdaHandlerConstructor = (dependencies: IJwksDependencies) => {
-  return async (
-    event: CloudFormationCustomResourceEvent,
-    context: Context,
-  ): Promise<void> => {
-    console.log(JSON.stringify(event));
-    const resultSender = dependencies.customResourceResultSender(
-      event,
-      context,
-    );
+export async function lambdaHandlerConstructor(
+  dependencies: IJwksDependencies,
+  event: CloudFormationCustomResourceEvent,
+  context: Context,
+): Promise<void> {
+  console.log(JSON.stringify(event));
+  const resultSender = dependencies.customResourceResultSender(event, context);
 
-    const logger = dependencies.logger();
-    logger.addContext(context);
-    logger.log("STARTED");
+  const logger = dependencies.logger();
+  logger.addContext(context);
+  logger.log("STARTED");
 
-    const configResult = new ConfigService().getConfig(dependencies.env);
-    if (configResult.isError) {
-      logger.log("ENVIRONMENT_VARIABLE_MISSING", {
-        errorMessage: configResult.value.errorMessage,
-      });
-      await resultSender.sendResult("FAILED");
-      return;
-    }
-
-    console.log(JSON.stringify(configResult.value));
-
-    const environmentVariables = configResult.value;
-    console.log(JSON.stringify(environmentVariables));
-
-    const jwksBuilder = dependencies.jwksBuilder(environmentVariables.KEY_ID);
-    const getJwksResult = await jwksBuilder.buildJwks();
-    if (getJwksResult.isError) {
-      logger.log("INTERNAL_SERVER_ERROR", {
-        errorMessage: getJwksResult.value.errorMessage,
-      });
-      await resultSender.sendResult("FAILED");
-      return;
-    }
-
-    const jwks = getJwksResult.value;
-    const jwksUploader = dependencies.jwksUploader();
-    const uploadJwksResult = await jwksUploader.uploadJwks(
-      jwks,
-      environmentVariables.JWKS_BUCKET_NAME,
-      environmentVariables.JWKS_FILE_NAME,
-    );
-    if (uploadJwksResult.isError) {
-      logger.log("INTERNAL_SERVER_ERROR", {
-        errorMessage: uploadJwksResult.value.errorMessage,
-      });
-      await resultSender.sendResult("FAILED");
-      return;
-    }
-
-    await resultSender.sendResult("SUCCESS");
+  const configResult = new ConfigService().getConfig(dependencies.env);
+  if (configResult.isError) {
+    logger.log("ENVIRONMENT_VARIABLE_MISSING", {
+      errorMessage: configResult.value.errorMessage,
+    });
+    await resultSender.sendResult("FAILED");
     return;
-  };
-};
+  }
+
+  console.log(JSON.stringify(configResult.value));
+
+  const environmentVariables = configResult.value;
+  console.log(JSON.stringify(environmentVariables));
+
+  const jwksBuilder = dependencies.jwksBuilder(environmentVariables.KEY_ID);
+  const getJwksResult = await jwksBuilder.buildJwks();
+  if (getJwksResult.isError) {
+    logger.log("INTERNAL_SERVER_ERROR", {
+      errorMessage: getJwksResult.value.errorMessage,
+    });
+    await resultSender.sendResult("FAILED");
+    return;
+  }
+
+  const jwks = getJwksResult.value;
+  const jwksUploader = dependencies.jwksUploader();
+  const uploadJwksResult = await jwksUploader.uploadJwks(
+    jwks,
+    environmentVariables.JWKS_BUCKET_NAME,
+    environmentVariables.JWKS_FILE_NAME,
+  );
+  if (uploadJwksResult.isError) {
+    logger.log("INTERNAL_SERVER_ERROR", {
+      errorMessage: uploadJwksResult.value.errorMessage,
+    });
+    await resultSender.sendResult("FAILED");
+    return;
+  }
+
+  await resultSender.sendResult("SUCCESS");
+  return;
+}
 
 export const lambdaHandler = lambdaHandlerConstructor.bind(null, dependencies);
