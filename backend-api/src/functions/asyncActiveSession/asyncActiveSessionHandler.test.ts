2853bbbaabc588c51764a19bf030deaf136d70bb
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2024-08-28 11:54:51 +0100

    Formatting

--- backend-api/src/functions/asyncActiveSession/asyncActiveSessionHandler.test.ts
+++ backend-api/src/functions/asyncActiveSession/asyncActiveSessionHandler.test.ts
@@ -1,14 +1,14 @@
-import { lambdaHandler } from "./asyncActiveSessionHandler"
+import { lambdaHandler } from "./asyncActiveSessionHandler";
 
 describe("Async Active Session", () => {
   describe("Given a request is received", () => {
     it("Returns 200 response", async () => {
-      const result = await lambdaHandler()
+      const result = await lambdaHandler();
 
       expect(result).toStrictEqual({
         statusCode: 200,
-        body: "Hello, World"
-      })
-    })
-  })
-})
+        body: "Hello, World",
+      });
+    });
+  });
+});
--- backend-api/src/functions/asyncActiveSession/asyncActiveSessionHandler.ts
+++ backend-api/src/functions/asyncActiveSession/asyncActiveSessionHandler.ts
@@ -3,6 +3,6 @@ import { APIGatewayProxyResult } from "aws-lambda";
 export async function lambdaHandler(): Promise<APIGatewayProxyResult> {
   return {
     statusCode: 200,
-    body: "Hello, World"
-  }
+    body: "Hello, World",
+  };
 }
