0702885abc7a561c8678269af5556c1ae4c4d28f
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2024-08-28 10:24:56 +0100

    Remove event from test - not needed yet

--- backend-api/src/functions/asyncActiveSession/asyncActiveSessionHandler.test.ts
+++ backend-api/src/functions/asyncActiveSession/asyncActiveSessionHandler.test.ts
@@ -1,7 +1,9 @@
+import { lambdaHandlerConstructor } from "./asyncActiveSessionHandler"
+
 describe("Async Active Session", () => {
   describe("Given a request is made", () => {
     it("Returns 200 Hello, World response", async () => {
-      const result = await lambdaHandlerConstructor(event)
+      const result = await lambdaHandlerConstructor()
 
       expect(result).toStrictEqual({
         statusCode: 200,
