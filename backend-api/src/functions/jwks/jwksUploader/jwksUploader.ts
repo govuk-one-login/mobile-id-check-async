da4309c6c1dd5ee18937ce64412a9a9a951e3f2f
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2024-09-05 14:00:25 +0100

    Update node-handler import

--- backend-api/src/functions/jwks/jwksUploader/jwksUploader.ts
+++ backend-api/src/functions/jwks/jwksUploader/jwksUploader.ts
@@ -1,4 +1,4 @@
-import { NodeHttpHandler } from "@aws-sdk/node-http-handler";
+import { NodeHttpHandler } from "@smithy/node-http-handler";
 import { errorResult, Result, successResult } from "../../utils/result";
 import {
   PutObjectCommand,
