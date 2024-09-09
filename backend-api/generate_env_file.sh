9ce21ec375ee30f445bdaa0bffb6a9749bf3a280
Sandy May <51717217+sandymay-dd@users.noreply.github.com>
2024-09-09 18:00:41 +0100

    DCMAW-8456: fix generate env script

--- backend-api/generate_env_file.sh
+++ backend-api/generate_env_file.sh
@@ -13,5 +13,5 @@ echo "Generating .env file for the $SAM_STACK stack"
 
 SELF=$(aws cloudformation describe-stacks --stack-name "$SAM_STACK" --query "Stacks[0].Outputs[?OutputKey=='PublicAPIGWBaseURL'].OutputValue" --output text)
 
-echo "SELF_PUBLIC=$SELF" >> .env
+echo "SELF_PUBLIC=$SELF" > .env
 echo "PROXY_API_URL=https://proxy-$SAM_STACK.review-b-async.$ENV.account.gov.uk" >> .env
