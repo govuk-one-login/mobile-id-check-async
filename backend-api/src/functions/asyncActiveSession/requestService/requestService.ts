f044de903fce2e874d1ee2f9075c3fb847eb9ed8
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2024-09-19 11:23:00 +0100

    create request service

---
+++ backend-api/src/functions/asyncActiveSession/requestService/requestService.ts
@@ -0,0 +1,44 @@
+import { errorResult, Result, successResult } from "../../utils/result";
+
+export class RequestService implements IRequestService {
+  getAuthorizationHeader = (
+    authorizationHeader: string | undefined,
+  ): Result<string> => {
+    if (authorizationHeader == null) {
+      return errorResult({
+        errorMessage: "No Authentication header present",
+        errorCategory: "CLIENT_ERROR",
+      });
+    }
+
+    // if (!authorizationHeader.startsWith("Bearer ")) {
+    //   return errorResult({
+    //     errorMessage:
+    //       "Invalid authentication header format - does not start with Bearer",
+    //     errorCategory: "CLIENT_ERROR",
+    //   });
+    // }
+
+    // if (authorizationHeader.split(" ").length !== 2) {
+    //   return errorResult({
+    //     errorMessage: "Invalid authentication header format - contains spaces",
+    //     errorCategory: "CLIENT_ERROR",
+    //   });
+    // }
+
+    // if (authorizationHeader.split(" ")[1].length == 0) {
+    //   return errorResult({
+    //     errorMessage: "Invalid authentication header format - missing token",
+    //     errorCategory: "CLIENT_ERROR",
+    //   });
+    // }
+
+    return successResult(authorizationHeader);
+  };
+}
+
+export interface IRequestService {
+  getAuthorizationHeader: (
+    authorizationHeader: string | undefined,
+  ) => Result<string>;
+}
