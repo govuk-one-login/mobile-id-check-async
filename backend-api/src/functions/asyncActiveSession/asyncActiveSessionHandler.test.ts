d893d0d8689ed5a1b0df0709fc1141326be59104
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2024-09-19 11:22:22 +0100

    create lambda test for header validation

--- backend-api/src/functions/asyncActiveSession/asyncActiveSessionHandler.test.ts
+++ backend-api/src/functions/asyncActiveSession/asyncActiveSessionHandler.test.ts
@@ -1,13 +1,47 @@
-import { lambdaHandler } from "./asyncActiveSessionHandler";
+import { APIGatewayProxyResult } from "aws-lambda";
+import { buildRequest } from "../testUtils/mockRequest";
+import { lambdaHandlerConstructor } from "./asyncActiveSessionHandler";
+import { IAsyncActiveSessionDependencies } from "./handlerDependencies";
+import { MockLoggingAdapter } from "../services/logging/tests/mockLogger";
+import { MessageName, registeredLogs } from "./registeredLogs";
+import { Logger } from "../services/logging/logger";
 
 describe("Async Active Session", () => {
-  describe("Given a request is received", () => {
-    it("Returns 200 response", async () => {
-      const result = await lambdaHandler();
+  let dependencies: IAsyncActiveSessionDependencies;
+  let mockLogger: MockLoggingAdapter<MessageName>;
 
-      expect(result).toStrictEqual({
-        statusCode: 200,
-        body: "Hello, World",
+  beforeEach(() => {
+    mockLogger = new MockLoggingAdapter();
+    dependencies = {
+      logger: () => new Logger(mockLogger, registeredLogs),
+    };
+  });
+
+  describe("Access token validation", () => {
+    describe("Given Authentication header is missing", () => {
+      it("Returns 401 Unauthorized", async () => {
+        const event = buildRequest();
+
+        const result: APIGatewayProxyResult = await lambdaHandlerConstructor(
+          dependencies,
+          event,
+        );
+
+        expect(mockLogger.getLogMessages()[0].logMessage.message).toBe(
+          "AUTHENTICATION_HEADER_INVALID",
+        );
+        expect(mockLogger.getLogMessages()[0].data).toStrictEqual({
+          errorMessage: "No Authentication header present",
+        });
+
+        expect(result).toStrictEqual({
+          headers: { "Content-Type": "application/json" },
+          statusCode: 401,
+          body: JSON.stringify({
+            error: "Unauthorized",
+            error_description: "Invalid token",
+          }),
+        });
       });
     });
   });
