2d5555c0335ac37bc838dd0f7e02be9931e4cfab
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-09-19 12:34:13 +0100

    feat(DCMAW-8965): fix code smells

--- sts-mock/src/token/tokenSigner/tokenSigner.ts
+++ sts-mock/src/token/tokenSigner/tokenSigner.ts
@@ -8,18 +8,16 @@ export class TokenSigner implements ITokenSigner {
     payload: JWTPayload,
     signingKey: KeyObject,
   ): Promise<Result<string>> {
-    {
-      try {
-        const jwt = await new SignJWT(payload)
-          .setProtectedHeader({ alg: "ES256", typ: "JWT", kid: keyId })
-          .sign(signingKey);
-        return successResult(jwt);
-      } catch {
-        return errorResult({
-          errorMessage: "Error signing token",
-          errorCategory: "SERVER_ERROR",
-        });
-      }
+    try {
+      const jwt = await new SignJWT(payload)
+        .setProtectedHeader({ alg: "ES256", typ: "JWT", kid: keyId })
+        .sign(signingKey);
+      return successResult(jwt);
+    } catch {
+      return errorResult({
+        errorMessage: "Error signing token",
+        errorCategory: "SERVER_ERROR",
+      });
     }
   }
 }
