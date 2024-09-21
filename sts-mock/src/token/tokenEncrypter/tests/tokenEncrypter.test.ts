4ecfc90eabb4c50be7b05d3b379f9a6a1fceb5f0
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-09-21 16:52:40 +0100

    test(DCMAW-8965): unit test tokenEncrypter

--- sts-mock/src/token/tokenEncrypter/tests/tokenEncrypter.test.ts
+++ sts-mock/src/token/tokenEncrypter/tests/tokenEncrypter.test.ts
@@ -12,7 +12,10 @@ describe("Token Encrypter", () => {
       Promise.resolve({
         status: 200,
         ok: true,
-        text: () => Promise.resolve(""),
+        text: () =>
+          Promise.resolve(
+            '{"keys":[{"kty":"RSA","n":"0wLh4PMAjSt17zLNFw9nnBdV901AWp0uuHQzGaz1-Wz1lAs-jN7nI90sQAyiv8MDlYWLrfUZKcQAAA0yjISp9UyTr8qgqsyAKiFBIcnoH7l4qV-U-VXe3rcMjr5BzrKdVK664YiF9coGaal-QDDd1VY0fvvom3DhGnh8MoezBQPKl6pynIaSiDHZUdSe8B9LdsjsKHt4SujGRR_QlERYISC0s4pCQu2gA9qsP-pFDfcklbLtskFtWa_utiPe48Y5xgrhj5r-hMz9Zi4R55mX6nymC9gypk7q6iiXGEQcMzxPMy0kgF4437PqA-0GmjJE24pGmVhnr33UL2i0tsfviw","e":"AQAB","use":"enc","alg":"RS256","kid":"456d2da6-9ca8-4e1d-b8c8-081109d73015"}]}',
+          ),
       } as Response),
     );
   });
@@ -21,7 +24,7 @@ describe("Token Encrypter", () => {
     jest.restoreAllMocks();
   });
 
-  describe("Given an error happens when requesting the encryption key", () => {
+  describe("Given an error happens when requesting the JWKS", () => {
     it("Returns an error response", async () => {
       jest.spyOn(global, "fetch").mockImplementation(() =>
         Promise.resolve({
@@ -40,7 +43,7 @@ describe("Token Encrypter", () => {
     });
   });
 
-  describe("Given an unexpected network error happens when requesting the encryption key", () => {
+  describe("Given an unexpected network error happens when requesting the JWKS", () => {
     it("Returns an error response", async () => {
       jest.spyOn(global, "fetch").mockImplementationOnce(() => {
         throw new Error("Unexpected network error");
@@ -116,7 +119,7 @@ describe("Token Encrypter", () => {
     });
   });
 
-  describe("Given the JWKS does not contain an encryption key", () => {
+  describe("Given the JWKS does not contain an encryption JWK", () => {
     it("Returns an error response", async () => {
       jest.spyOn(global, "fetch").mockImplementation(() =>
         Promise.resolve({
@@ -131,6 +134,26 @@ describe("Token Encrypter", () => {
       expect(result.isError).toBe(true);
       expect(result.value).toStrictEqual({
         errorMessage: "No encryption key in JWKS",
+        errorCategory: "SERVER_ERROR",
+      });
+    });
+  });
+
+  describe("Given the encryption JWK is invalid and therefore cannot be parsed as a KeyObject", () => {
+    it("Returns an error response", async () => {
+      jest.spyOn(global, "fetch").mockImplementation(() =>
+        Promise.resolve({
+          status: 200,
+          ok: true,
+          text: () => Promise.resolve('{"keys":[{"kty":"RSA","use":"enc"}]}'),
+        } as Response),
+      );
+
+      const result = await tokenEncrypter.encrypt(dummyJwt);
+
+      expect(result.isError).toBe(true);
+      expect(result.value).toStrictEqual({
+        errorMessage: "Error creating public encryption key",
         errorCategory: "SERVER_ERROR",
       });
     });
@@ -153,6 +176,45 @@ describe("Token Encrypter", () => {
         errorMessage: "Error creating public encryption key",
         errorCategory: "SERVER_ERROR",
       });
+    });
+  });
+
+  describe("Given an error happens trying to encrypt the JWT (e.g. JWK type is not compatible with encryption algorithm)", () => {
+    it("Returns an error response", async () => {
+      jest.spyOn(global, "fetch").mockImplementation(() =>
+        Promise.resolve({
+          status: 200,
+          ok: true,
+          text: () =>
+            Promise.resolve(
+              '{"keys":[{"kty":"EC","x":"-JBGGl6V4K-9VJZ_UfPljiHlteQCqTwbbMHEAxv0_NA","y":"ZgDwVZCrtEHfdrJwgq3n7a2pdPUFLabCYLUu6Un3VXE","crv":"P-256","kid":"SNFJEFlxy-FxCkRtvj1VD38VuotQ-ta6a2w7p4j6jhY","use":"enc","alg":"ES256"}]}',
+            ),
+        } as Response),
+      );
+
+      const result = await tokenEncrypter.encrypt(dummyJwt);
+
+      expect(result.isError).toBe(true);
+      expect(result.value).toStrictEqual({
+        errorMessage: "Error encrypting token",
+        errorCategory: "SERVER_ERROR",
+      });
+    });
+  });
+
+  describe("Given the JWT is encrypted", () => {
+    it("Returns a success response with the JWE", async () => {
+      const result = await tokenEncrypter.encrypt(dummyJwt);
+
+      const resultValue = result.value as string;
+
+      expect(result.isError).toBe(false);
+      expect(resultValue.split(".")).toHaveLength(5);
+      expect(
+        resultValue.startsWith(
+          "eyJhbGciOiJSU0EtT0FFUC0yNTYiLCJlbmMiOiJBMjU2R0NNIn0",
+        ),
+      ).toBeTruthy();
     });
   });
 });
