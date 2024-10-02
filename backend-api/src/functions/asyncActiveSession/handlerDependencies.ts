46089b8ef40dcf3da799b4abb3c3ef81dfe17e60
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-10-02 16:35:48 +0100

    DCMAW-8229: KMS adapter (#192)
    <!-- Include the Jira ticket number in square brackets as prefix, eg
    `[DCMAW-XXXX] PR Title` -->
    ​DCMAW-8229
    
    ### What changed
    - Make the `decrypt` function accept the key ARN
    - Return error result if `Plaintext` in the output of the decrypt
    command is `null`
    - Import `KmsAdapter` in the `TokenService` instead of making it a
    property of that class
    
    ![Screenshot 2024-10-01 at 14 12
    05](https://github.com/user-attachments/assets/9c25ec76-d43f-4678-b582-33a7fd97a3c2)
    
    ### Why did it change
    <!-- Describe the reason these changes were made - the "why" -->
    
    ## Checklists
    <!-- Merging this PR is effectively deploying to production. Be mindful
    to answer accurately. -->
    
    - [x] There is a ticket raised for this PR that is present in the branch
    name
    - [x] No PII data logged. [See guidance
    here](https://govukverify.atlassian.net/wiki/spaces/DCMAW/pages/3502407722/PII+Logging+Considerations)
    - [ ] Demo to a BA, TA, and the team.
    - [ ] Update [README](./blob/main/README.md) with any new instructions
    or tasks

--- backend-api/src/functions/asyncActiveSession/handlerDependencies.ts
+++ backend-api/src/functions/asyncActiveSession/handlerDependencies.ts
@@ -2,18 +2,15 @@ import { Logger } from "../services/logging/logger";
 import { Logger as PowertoolsLogger } from "@aws-lambda-powertools/logger";
 import { MessageName, registeredLogs } from "./registeredLogs";
 import { ITokenService, TokenService } from "./tokenService/tokenService";
-import { IKmsAdapter, KMSAdapter } from "../adapters/kmsAdapter";
 
 export interface IAsyncActiveSessionDependencies {
   env: NodeJS.ProcessEnv;
   logger: () => Logger<MessageName>;
-  kmsAdapter: (encryptionKeyArn: string) => IKmsAdapter;
-  tokenService: (kmsAdapter: IKmsAdapter) => ITokenService;
+  tokenService: () => ITokenService;
 }
 
 export const dependencies: IAsyncActiveSessionDependencies = {
   env: process.env,
   logger: () => new Logger<MessageName>(new PowertoolsLogger(), registeredLogs),
-  kmsAdapter: (encryptionKeyArn: string) => new KMSAdapter(encryptionKeyArn),
-  tokenService: (kmsAdapter: IKmsAdapter) => new TokenService(kmsAdapter),
+  tokenService: () => new TokenService(),
 };
