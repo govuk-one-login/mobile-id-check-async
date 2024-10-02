c288e473236a6124a68d027cfd2baa0205c85462
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2024-10-02 12:58:46 +0100

    DCMAW-8229: Adds sendHttpRequest function and enables retrying of STS jwks call (#187)

--- backend-api/src/functions/asyncActiveSession/tokenService/tests/tokenService.test.ts
+++ backend-api/src/functions/asyncActiveSession/tokenService/tests/tokenService.test.ts
@@ -17,19 +17,24 @@ describe("Token Service", () => {
       Promise.resolve({
         status: 200,
         ok: true,
-        json: () =>
-          Promise.resolve({
-            keys: [
-              {
-                kty: "mockKty",
-                x: "mockX",
-                y: "mockY",
-                crv: "mockCrv",
-                d: "mockD",
-                kid: "mockKid",
-              },
-            ],
-          }),
+        headers: new Headers({
+          header: "mockHeader",
+        }),
+        text: () =>
+          Promise.resolve(
+            JSON.stringify({
+              keys: [
+                {
+                  kty: "mockKty",
+                  x: "mockX",
+                  y: "mockY",
+                  crv: "mockCrv",
+                  d: "mockD",
+                  kid: "mockKid",
+                },
+              ],
+            }),
+          ),
       } as Response),
     );
   });
@@ -50,6 +55,7 @@ describe("Token Service", () => {
             const result = await tokenService.getSubFromToken(
               "https://mockJwksEndpoint.com",
               "mockJwe",
+              { maxAttempts: 3, delayInMillis: 1 },
             );
 
             expect(mockFetch).toHaveBeenCalledWith(
@@ -60,7 +66,7 @@ describe("Token Service", () => {
             );
             expect(result.isError).toBe(true);
             expect(result.value).toStrictEqual({
-              errorMessage: "Unexpected error retrieving STS public key",
+              errorMessage: "Unexpected network error: Error: mockError",
               errorCategory: "SERVER_ERROR",
             });
           });
@@ -72,12 +78,14 @@ describe("Token Service", () => {
               Promise.resolve({
                 status: 500,
                 ok: false,
+                text: () => Promise.resolve("mockErrorInformaton"),
               } as Response),
             );
 
             const result = await tokenService.getSubFromToken(
               "https://mockJwksEndpoint.com",
               "mockJwe",
+              { maxAttempts: 3, delayInMillis: 1 },
             );
 
             expect(mockFetch).toHaveBeenCalledWith(
@@ -88,7 +96,40 @@ describe("Token Service", () => {
             );
             expect(result.isError).toBe(true);
             expect(result.value).toStrictEqual({
-              errorMessage: "Error retrieving STS public key",
+              errorMessage: "Error making http request: mockErrorInformaton",
+              errorCategory: "SERVER_ERROR",
+            });
+          });
+        });
+
+        describe("Given the response is empty", () => {
+          it("Returns an error response", async () => {
+            mockFetch = jest.spyOn(global, "fetch").mockImplementation(() =>
+              Promise.resolve({
+                status: 200,
+                ok: true,
+                headers: new Headers({
+                  header: "mockHeader",
+                }),
+                text: () => Promise.resolve(""),
+              } as Response),
+            );
+
+            const result = await tokenService.getSubFromToken(
+              "https://mockJwksEndpoint.com",
+              "mockJwe",
+              { maxAttempts: 3, delayInMillis: 1 },
+            );
+
+            expect(mockFetch).toHaveBeenCalledWith(
+              "https://mockJwksEndpoint.com",
+              {
+                method: "GET",
+              },
+            );
+            expect(result.isError).toBe(true);
+            expect(result.value).toStrictEqual({
+              errorMessage: "Response body empty",
               errorCategory: "SERVER_ERROR",
             });
           });
@@ -100,13 +141,17 @@ describe("Token Service", () => {
               Promise.resolve({
                 status: 200,
                 ok: true,
-                json: () => Promise.reject(new Error("mockInvalidJSON")),
+                headers: new Headers({
+                  header: "mockHeader",
+                }),
+                text: () => Promise.resolve("undefined"),
               } as Response),
             );
 
             const result = await tokenService.getSubFromToken(
               "https://mockJwksEndpoint.com",
               "mockJwe",
+              { maxAttempts: 3, delayInMillis: 1 },
             );
 
             expect(mockFetch).toHaveBeenCalledWith(
@@ -122,6 +167,182 @@ describe("Token Service", () => {
             });
           });
         });
+
+        describe("Given the response is not in the shape of a public key", () => {
+          it("Returns an error response", async () => {
+            mockFetch = jest.spyOn(global, "fetch").mockImplementation(() =>
+              Promise.resolve({
+                status: 200,
+                ok: true,
+                headers: new Headers({
+                  header: "mockHeader",
+                }),
+                text: () =>
+                  Promise.resolve(
+                    JSON.stringify({
+                      keys: ["mockNotAnObject"],
+                    }),
+                  ),
+              } as Response),
+            );
+
+            const result = await tokenService.getSubFromToken(
+              "https://mockJwksEndpoint.com",
+              "mockJwe",
+              { maxAttempts: 3, delayInMillis: 1 },
+            );
+
+            expect(mockFetch).toHaveBeenCalledWith(
+              "https://mockJwksEndpoint.com",
+              {
+                method: "GET",
+              },
+            );
+            expect(result.isError).toBe(true);
+            expect(result.value).toStrictEqual({
+              errorMessage:
+                "Response does not match the expected JWKS structure",
+              errorCategory: "SERVER_ERROR",
+            });
+          });
+        });
+
+        describe("Retry policy", () => {
+          describe("Given there is an error retrieving the public key on the first attempt", () => {
+            it("Makes second attempt to get STS key", async () => {
+              mockFetch = jest
+                .spyOn(global, "fetch")
+                .mockImplementationOnce(() =>
+                  Promise.reject(new Error("mockError")),
+                )
+                .mockImplementationOnce(() =>
+                  Promise.resolve({
+                    status: 200,
+                    ok: true,
+                    headers: new Headers({
+                      header: "mockHeader",
+                    }),
+                    text: () =>
+                      Promise.resolve(
+                        JSON.stringify({
+                          keys: [
+                            {
+                              kty: "mockKty",
+                              x: "mockX",
+                              y: "mockY",
+                              crv: "mockCrv",
+                              d: "mockD",
+                              kid: "mockKid",
+                            },
+                          ],
+                        }),
+                      ),
+                  } as Response),
+                );
+
+              await tokenService.getSubFromToken(
+                "https://mockJwksEndpoint.com",
+                "mockJwe",
+                { maxAttempts: 3, delayInMillis: 1 },
+              );
+
+              expect(mockFetch).toHaveBeenCalledWith(
+                "https://mockJwksEndpoint.com",
+                {
+                  method: "GET",
+                },
+              );
+              expect(mockFetch).toHaveBeenCalledTimes(2);
+            });
+          });
+
+          describe("Given there is an error retrieving the public key on the second attempt", () => {
+            it("Makes third attempt to get STS key", async () => {
+              mockFetch = jest
+                .spyOn(global, "fetch")
+                .mockImplementationOnce(() =>
+                  Promise.reject(new Error("mockError")),
+                )
+                .mockImplementationOnce(() =>
+                  Promise.reject(new Error("mockError")),
+                )
+                .mockImplementationOnce(() =>
+                  Promise.resolve({
+                    status: 200,
+                    ok: true,
+                    headers: new Headers({
+                      header: "mockHeader",
+                    }),
+                    text: () =>
+                      Promise.resolve(
+                        JSON.stringify({
+                          keys: [
+                            {
+                              kty: "mockKty",
+                              x: "mockX",
+                              y: "mockY",
+                              crv: "mockCrv",
+                              d: "mockD",
+                              kid: "mockKid",
+                            },
+                          ],
+                        }),
+                      ),
+                  } as Response),
+                );
+
+              await tokenService.getSubFromToken(
+                "https://mockJwksEndpoint.com",
+                "mockJwe",
+                { maxAttempts: 3, delayInMillis: 1 },
+              );
+
+              expect(mockFetch).toHaveBeenCalledWith(
+                "https://mockJwksEndpoint.com",
+                {
+                  method: "GET",
+                },
+              );
+              expect(mockFetch).toHaveBeenCalledTimes(3);
+            });
+          });
+
+          describe("Given there is an error retrieving the public key on the third attempt", () => {
+            it("Returns error result", async () => {
+              mockFetch = jest
+                .spyOn(global, "fetch")
+                .mockImplementationOnce(() =>
+                  Promise.reject(new Error("mockError")),
+                )
+                .mockImplementationOnce(() =>
+                  Promise.reject(new Error("mockError")),
+                )
+                .mockImplementationOnce(() =>
+                  Promise.reject(new Error("mockError")),
+                );
+
+              const result = await tokenService.getSubFromToken(
+                "https://mockJwksEndpoint.com",
+                "mockJwe",
+                { maxAttempts: 3, delayInMillis: 1 },
+              );
+
+              expect(mockFetch).toHaveBeenCalledWith(
+                "https://mockJwksEndpoint.com",
+                {
+                  method: "GET",
+                },
+              );
+              expect(mockFetch).toHaveBeenCalledTimes(3);
+
+              expect(result.isError).toBe(true);
+              expect(result.value).toStrictEqual({
+                errorMessage: "Unexpected network error: Error: mockError",
+                errorCategory: "SERVER_ERROR",
+              });
+            });
+          });
+        });
       });
     });
 
@@ -134,6 +355,7 @@ describe("Token Service", () => {
           const result = await tokenService.getSubFromToken(
             "https://mockJwksEndpoint.com",
             "one.two.three.four",
+            { maxAttempts: 3, delayInMillis: 1 },
           );
 
           expect(result.isError).toBe(true);
@@ -157,6 +379,7 @@ describe("Token Service", () => {
           const result = await tokenService.getSubFromToken(
             "https://mockJwksEndpoint.com",
             "one.two.three.four.five",
+            { maxAttempts: 3, delayInMillis: 1 },
           );
 
           expect(result.isError).toBe(true);
@@ -175,6 +398,7 @@ describe("Token Service", () => {
           const result = await tokenService.getSubFromToken(
             "https://mockJwksEndpoint.com",
             "one.two.three.four.five",
+            { maxAttempts: 3, delayInMillis: 1 },
           );
 
           expect(result.isError).toBe(true);
