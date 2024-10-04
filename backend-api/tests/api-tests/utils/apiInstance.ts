cace122b22d4bed691259b94b15833322106ad18
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-10-04 11:50:59 +0100

    test: test private and proxy API

--- backend-api/tests/api-tests/utils/apiInstance.ts
+++ backend-api/tests/api-tests/utils/apiInstance.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import axiosRetry from "axios-retry";
 import "dotenv/config";
+import { aws4Interceptor } from "aws4-axios";
+import { fromNodeProviderChain } from "@aws-sdk/credential-providers";
 
 function getInstance(baseUrl: string) {
   const apiInstance = axios.create({ baseURL: baseUrl });
@@ -9,15 +11,51 @@ function getInstance(baseUrl: string) {
     retryDelay: (retryCount) => retryCount * 200,
   });
   apiInstance.defaults.validateStatus = () => true;
+  axiosRetry(apiInstance, {
+    retries: 2,
+    retryDelay: (retryCount) => retryCount * 200,
+  });
+
+  const interceptor = aws4Interceptor({
+    options: {
+      region: "eu-west-2",
+      service: "execute-api",
+    },
+    credentials: {
+      getCredentials: fromNodeProviderChain({
+        timeout: 1000,
+        maxRetries: 1,
+        profile: process.env.AWS_PROFILE,
+      }),
+    },
+  });
+
+  apiInstance.interceptors.request.use(interceptor);
 
   return apiInstance;
 }
 
 function getSessionsApiInstance() {
-  const sessionsApiUrl = process.env.SESSIONS_API_URL;
-  if (!sessionsApiUrl)
+  const apiUrl = process.env.SESSIONS_API_URL;
+  if (!apiUrl)
     throw new Error("SESSIONS_API_URL needs to be defined for API tests");
-  return getInstance(sessionsApiUrl);
+  return getInstance(apiUrl);
+}
+
+function getProxyApiInstance() {
+  const apiUrl = process.env.PROXY_API_URL;
+  if (!apiUrl)
+    throw new Error("PROXY_API_URL needs to be defined for API tests");
+  return getInstance(apiUrl);
+}
+
+function getPrivateApiInstance() {
+  const apiUrl = process.env.PRIVATE_API_URL;
+  if (!apiUrl)
+    throw new Error("PRIVATE_API_URL needs to be defined for API tests");
+  return getInstance(apiUrl);
 }
 
 export const SESSIONS_API_INSTANCE = getSessionsApiInstance();
+export const PROXY_API_INSTANCE = getProxyApiInstance();
+export const PRIVATE_API_INSTANCE = getPrivateApiInstance();
