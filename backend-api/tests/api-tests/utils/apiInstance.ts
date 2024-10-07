3420139ab5d5fc9f9fee2fdf212ed2f47fba1bec
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-10-07 16:30:19 +0100

    fix: use aws4Interceptor with proxy api only

--- backend-api/tests/api-tests/utils/apiInstance.ts
+++ backend-api/tests/api-tests/utils/apiInstance.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 import { aws4Interceptor } from "aws4-axios";
 import { fromNodeProviderChain } from "@aws-sdk/credential-providers";
 
-function getInstance(baseUrl: string, isProxy: boolean = false) {
+function getInstance(baseUrl: string, isProxyApi: boolean = false) {
   const apiInstance = axios.create({ baseURL: baseUrl });
   axiosRetry(apiInstance, {
     retries: 2,
@@ -12,7 +12,7 @@ function getInstance(baseUrl: string, isProxy: boolean = false) {
   });
   apiInstance.defaults.validateStatus = () => true;
 
-  if (isProxy) {
+  if (isProxyApi) {
     const interceptor = aws4Interceptor({
       options: {
         region: "eu-west-2",
