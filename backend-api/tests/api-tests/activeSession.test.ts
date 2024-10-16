9d990dab69a994a3cc9c519b3ab6e32ccd336a59
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2024-10-16 14:54:23 +0100

    DCMAW-8229: update test descriptions

--- backend-api/tests/api-tests/activeSession.test.ts
+++ backend-api/tests/api-tests/activeSession.test.ts
@@ -2,7 +2,7 @@ import { SESSIONS_API_INSTANCE } from "./utils/apiInstance";
 
 describe("GET /async/activeSession", () => {
   describe("Given service token is missing in the request header", () => {
-    it("returns 401 status code with error", async () => {
+    it("Returns 401 status code with error", async () => {
       const response = await SESSIONS_API_INSTANCE.get("/async/activeSession");
 
       expect(response.status).toBe(401);
@@ -13,8 +13,8 @@ describe("GET /async/activeSession", () => {
     });
   });
 
-  describe("Given a successful call is made", () => {
-    it("returns 200 status code and the Json Web Key Set", async () => {
+  describe("Given a successful call is made to /async/activeSession", () => {
+    it("Returns 200 status code, sessionId, redirect_uri and state", async () => {
       const response = await SESSIONS_API_INSTANCE.get("/async/activeSession");
 
       expect(response.status).toBe(200);
