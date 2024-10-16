14b214aa98a67b5a1024bb83ac42a243fd458a3b
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2024-10-16 17:24:43 +0100

    DCMAW-8229: format

--- backend-api/tests/api-tests/activeSession.test.ts
+++ backend-api/tests/api-tests/activeSession.test.ts
@@ -1,9 +1,12 @@
-import { SESSIONS_API_INSTANCE, STS_MOCK_API_INSTANCE } from "./utils/apiInstance";
+import {
+  SESSIONS_API_INSTANCE,
+  STS_MOCK_API_INSTANCE,
+} from "./utils/apiInstance";
 
 describe("GET /async/activeSession", () => {
-  let stsMockResponse
-  let requestBody
-  let accessToken: string
+  let stsMockResponse;
+  let requestBody;
+  let accessToken: string;
 
   beforeAll(async () => {
     requestBody = new URLSearchParams({
@@ -12,10 +15,9 @@ describe("GET /async/activeSession", () => {
       grant_type: "urn:ietf:params:oauth:grant-type:token-exchange",
       subject_token_type: "urn:ietf:params:oauth:token-type:access_token",
     });
-    stsMockResponse = await STS_MOCK_API_INSTANCE.post("/token", requestBody)
-    accessToken = stsMockResponse.data.access_token
-  })
-
+    stsMockResponse = await STS_MOCK_API_INSTANCE.post("/token", requestBody);
+    accessToken = stsMockResponse.data.access_token;
+  });
 
   describe("Given service token is missing in the request header", () => {
     it("Returns an error and 401 status code", async () => {
@@ -24,64 +26,65 @@ describe("GET /async/activeSession", () => {
       expect(response.status).toBe(401);
       expect(response.data).toStrictEqual({
         error: "unauthorized",
-        error_description: "Invalid authorization header"
-      })
+        error_description: "Invalid authorization header",
+      });
     });
   });
 
   describe("Given there is an error decrypting the service token", () => {
     it("Returns an error and 400 status code", async () => {
       const response = await SESSIONS_API_INSTANCE.get("/async/activeSession", {
-        headers: { Authorization: "Bearer one.two.three.four.five"}
+        headers: { Authorization: "Bearer one.two.three.four.five" },
       });
 
       expect(response.status).toBe(400);
       expect(response.data).toStrictEqual({
         error: "invalid_request",
         error_description: "Failed decrypting service token JWE",
-      })
+      });
     });
   });
 
   // describe("Given there is an error verifying token signature", () => {
   //   it("Returns an error and 400 status code", async () => {
   //     const response = await SESSIONS_API_INSTANCE.get("/async/activeSession", {
-  //       headers: { Authorization: `Bearer ${accessToken}`}
+  //       headers: { Authorization: `Bearer ${accessToken}` },
   //     });
 
   //     expect(response.status).toBe(400);
   //     expect(response.data).toStrictEqual({
   //       error: "invalid_request",
   //       error_description: "Error verifying service token signature",
-  //     })
+  //     });
   //   });
   // });
 
   // describe("Given there is an error validating service token payload", () => {
   //   it("Returns an error and 400 status code", async () => {
   //     const response = await SESSIONS_API_INSTANCE.get("/async/activeSession", {
-  //       headers: { Authorization: `Bearer ${accessToken}`}
+  //       headers: { Authorization: `Bearer ${accessToken}` },
   //     });
 
   //     expect(response.status).toBe(400);
   //     expect(response.data).toStrictEqual({
   //       error: "invalid_request",
   //       error_description: "Error verifying token signature",
-  //     })
+  //     });
   //   });
   // });
 
   describe("Given active session is not found", () => {
     it("Returns an error and 400 status code", async () => {
       const response = await SESSIONS_API_INSTANCE.get("/async/activeSession", {
-        headers: { Authorization: `Bearer ${accessToken}`}
+        headers: { Authorization: `Bearer ${accessToken}` },
       });
 
       expect(response.status).toBe(404);
       expect(response.data).toStrictEqual({
         error: "session_not_found",
-        error_description: "No active session found for the given sub identifier",
-      })
+        error_description:
+          "No active session found for the given sub identifier",
+      });
     });
   });
 
@@ -91,9 +94,9 @@ describe("GET /async/activeSession", () => {
 
   //     expect(response.status).toBe(200);
   //     expect(response.data.keys.length).toBeGreaterThanOrEqual(2);
-  //     expect(response.data["sessionId"]).toBeDefined()
-  //     expect(response.data["redirect_uri"]).toBeDefined()
-  //     expect(response.data["state"]).toBeDefined()
+  //     expect(response.data["sessionId"]).toBeDefined();
+  //     expect(response.data["redirect_uri"]).toBeDefined();
+  //     expect(response.data["state"]).toBeDefined();
   //   });
   // });
 });
