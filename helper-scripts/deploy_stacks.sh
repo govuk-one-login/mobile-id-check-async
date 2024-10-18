8d2cc233d5240416c4269249a9d77abcac2b377c
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2024-10-18 16:45:08 +0100

    Adds check if dev wants to deploy sts mock stack

--- helper-scripts/deploy_stacks.sh
+++ helper-scripts/deploy_stacks.sh
@@ -21,7 +21,7 @@ DEV_OVERRIDE_ASYNC_BACKEND_BASE_URL="DevOverrideAsyncBackendBaseUrl"
 # Deploy backend-api
 echo "Building and deploying backend-api stack..."
 cd ../backend-api || exit 1
-sam build
+sam build --cached
 sam deploy \
     --stack-name "$BACKEND_STACK_NAME" \
     --parameter-overrides "$DEV_OVERRIDE_STS_BASE_URL=https://${STS_MOCK_STACK_NAME}.review-b-async.dev.account.gov.uk" \
@@ -29,13 +29,29 @@ sam deploy \
     --resolve-s3
 
 # Deploy STS Mock
-echo "Building and deploying STS Mock stack..."
-cd ../sts-mock || exit 1
-sam build
-sam deploy \
-    --stack-name "$STS_MOCK_STACK_NAME" \
-    --parameter-overrides "$DEV_OVERRIDE_ASYNC_BACKEND_BASE_URL=https://sessions-${BACKEND_STACK_NAME}-async-backend.review-b-async.${Environment}.account.gov.uk" \
-    --capabilities CAPABILITY_NAMED_IAM
+while true; do
+    read -r -p "Do you want deploy a custom STS mock? [y/n]: " yn
+
+    case "$yn" in
+        [yY] )
+            echo "Building and deploying STS Mock stack..."
+            cd ../sts-mock || exit 1
+            sam build
+            sam deploy \
+                --stack-name "$STS_MOCK_STACK_NAME" \
+                --parameter-overrides "$DEV_OVERRIDE_ASYNC_BACKEND_BASE_URL=https://sessions-${BACKEND_STACK_NAME}-async-backend.review-b-async.${Environment}.account.gov.uk" \
+                --capabilities CAPABILITY_NAMED_IAM
+            break
+            ;;
+        [nN] )
+            echo "Skipping STS mock stack deployment"
+            break
+            ;;
+        * )
+            echo "Invalid input. Please enter 'y' or 'n'.\n"
+            ;;
+    esac
+done
 
 # After deploying STS Mock, generate keys
 while true; do
@@ -43,13 +59,13 @@ while true; do
 
     case "$yn" in
         [yY] )
-            echo "Generating keys"
+            echo "\nGenerating keys"
             cd ../sts-mock/jwks-helper-script || exit 1
             ./publish_keys_to_s3.sh "${STS_MOCK_STACK_NAME}" "dev"
             break
             ;;
         [nN] )
-            echo "Skipping key generation"
+            echo "\nSkipping key generation"
             break
             ;;
         * )
@@ -58,4 +74,4 @@ while true; do
     esac
 done
 
-echo "Deployment complete!"
+echo "\nDeployment complete!"
