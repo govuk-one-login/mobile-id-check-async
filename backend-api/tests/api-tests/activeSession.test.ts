c3dc0b0e4ad1d95277be67e6c434a14d68868427
jhumbert-dd <178896864+jhumbert-dd@users.noreply.github.com>
2024-10-18 16:16:27 +0100

    DCMAW-8229: Add API tests for activeSession (#230)
    ### What changed
    - Adds API tests for the Active Session lambda
    - Adds output in the template.yaml for the STS Mock API URL
    - Updates generate_env_file.sh with STS Mock API URL
    - Extracts test function for getting registered client
    
    ---------
    
    Co-authored-by: Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>

---
+++ backend-api/tests/api-tests/activeSession.test.ts
@@ -0,0 +1,119 @@
+import { randomUUID } from "crypto";
+import {
+  PROXY_API_INSTANCE,
+  SESSIONS_API_INSTANCE,
+  STS_MOCK_API_INSTANCE,
+} from "./utils/apiInstance";
+import { getFirstRegisteredClient } from "./utils/getRegisteredClient";
+
+jest.setTimeout(4 * 5000);
+
+describe("GET /async/activeSession", () => {
+  describe("Given service token is missing in the request header", () => {
+    it("Returns an error and 401 status code", async () => {
+      const response = await SESSIONS_API_INSTANCE.get("/async/activeSession");
+
+      expect(response.status).toBe(401);
+      expect(response.data).toStrictEqual({
+        error: "unauthorized",
+        error_description: "Invalid authorization header",
+      });
+    });
+  });
+
+  describe("Given service token is invalid", () => {
+    it("Returns an error and 400 status code", async () => {
+      const response = await SESSIONS_API_INSTANCE.get("/async/activeSession", {
+        headers: { Authorization: "Bearer one.two.three.four.five" },
+      });
+
+      expect(response.status).toBe(400);
+      expect(response.data).toStrictEqual({
+        error: "invalid_request",
+        error_description: "Failed to decrypt service token",
+      });
+    });
+  });
+
+  describe("Given active session is not found", () => {
+    it("Returns an error and 404 status code", async () => {
+      const accessToken = await getAccessToken(
+        "478E3BF8-C3D5-4EA2-A6EE-38B07F2EE0FD",
+      );
+
+      const response = await SESSIONS_API_INSTANCE.get("/async/activeSession", {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      });
+
+      expect(response.status).toBe(404);
+      expect(response.data).toStrictEqual({
+        error: "session_not_found",
+        error_description:
+          "No active session found for the given sub identifier",
+      });
+    });
+  });
+
+  describe("Given the request is valid and a session is found", () => {
+    it("Returns 200 status code, sessionId, redirectUri and state", async () => {
+      const sub = randomUUID();
+      await createSessionForSub(sub);
+      const accessToken = await getAccessToken(sub);
+
+      const response = await SESSIONS_API_INSTANCE.get("/async/activeSession", {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      });
+
+      expect(response.status).toBe(200);
+      expect(response.data["sessionId"]).toBeDefined();
+      expect(response.data["redirectUri"]).toBeDefined();
+      expect(response.data["state"]).toBeDefined();
+    });
+  });
+});
+
+async function getAccessToken(sub: string) {
+  const requestBody = new URLSearchParams({
+    subject_token: sub,
+    scope: "idCheck.activeSession.read",
+    grant_type: "urn:ietf:params:oauth:grant-type:token-exchange",
+    subject_token_type: "urn:ietf:params:oauth:token-type:access_token",
+  });
+  const stsMockResponse = await STS_MOCK_API_INSTANCE.post(
+    "/token",
+    requestBody,
+  );
+  return stsMockResponse.data.access_token;
+}
+
+async function createSessionForSub(sub: string) {
+  const clientDetails = await getFirstRegisteredClient();
+  const clientIdAndSecret = `${clientDetails.client_id}:${clientDetails.client_secret}`;
+  const clientIdAndSecretB64 =
+    Buffer.from(clientIdAndSecret).toString("base64");
+  const asyncTokenResponse = await PROXY_API_INSTANCE.post(
+    "/async/token",
+    "grant_type=client_credentials",
+    {
+      headers: {
+        "x-custom-auth": `Basic ${clientIdAndSecretB64}`,
+      },
+    },
+  );
+  const asyncCredentialResponse = await PROXY_API_INSTANCE.post(
+    "/async/credential",
+    {
+      sub: sub ?? randomUUID(),
+      govuk_signin_journey_id: "44444444-4444-4444-4444-444444444444",
+      client_id: clientDetails.client_id,
+      state: "testState",
+      redirect_uri: clientDetails.redirect_uri,
+    },
+    {
+      headers: {
+        "x-custom-auth": `Bearer ${asyncTokenResponse.data.access_token}`,
+      },
+    },
+  );
+  return asyncCredentialResponse.data;
+}
--- backend-api/tests/api-tests/clientCredentialsGrantFlow.test.ts
+++ backend-api/tests/api-tests/clientCredentialsGrantFlow.test.ts
@@ -1,11 +1,11 @@
-import "dotenv/config";
-import {
-  GetSecretValueCommand,
-  SecretsManagerClient,
-} from "@aws-sdk/client-secrets-manager";
-import { randomUUID, UUID } from "crypto";
-import { PRIVATE_API_INSTANCE, PROXY_API_INSTANCE } from "./utils/apiInstance";
 import { AxiosInstance } from "axios";
+import { randomUUID, UUID } from "crypto";
+import "dotenv/config";
+import { PRIVATE_API_INSTANCE, PROXY_API_INSTANCE } from "./utils/apiInstance";
+import {
+  ClientDetails,
+  getFirstRegisteredClient,
+} from "./utils/getRegisteredClient";
 
 const getApisToTest = (): {
   apiName: string;
@@ -335,29 +335,6 @@ describe.each(apis)(
     });
   },
 );
-
-interface ClientDetails {
-  client_id: string;
-  client_secret: string;
-  redirect_uri: string;
-}
-
-async function getRegisteredClients(): Promise<ClientDetails[]> {
-  const secretsManagerClient = new SecretsManagerClient({
-    region: "eu-west-2",
-  });
-  const secretName = `${process.env.TEST_ENVIRONMENT}/clientRegistryApiTest`;
-  const command = new GetSecretValueCommand({
-    SecretId: secretName,
-  });
-  const response = await secretsManagerClient.send(command);
-  return JSON.parse(response.SecretString!);
-}
-
-async function getFirstRegisteredClient(): Promise<ClientDetails> {
-  const clientsDetails = await getRegisteredClients();
-  return clientsDetails[0];
-}
 
 function toBase64(value: string): string {
   return Buffer.from(value).toString("base64");
