c8825bb9a67cdde32e543a4d40d6b8e61dc328e1
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2024-10-18 11:04:49 +0100

    DCMAW-8229: add sts mock api url to generate env file and add output to template.yaml

--- backend-api/generate_env_file.sh
+++ backend-api/generate_env_file.sh
@@ -12,9 +12,11 @@ echo "Generating .env file for the $SAM_STACK stack"
 SESSIONS_API_URL=$(aws cloudformation describe-stacks --stack-name "$SAM_STACK" --query "Stacks[0].Outputs[?OutputKey=='SessionsApiUrl'].OutputValue" --output text)
 PROXY_API_URL=$(aws cloudformation describe-stacks --stack-name "$SAM_STACK" --query "Stacks[0].Outputs[?OutputKey=='ProxyApiUrl'].OutputValue" --output text)
 PRIVATE_API_URL=$(aws cloudformation describe-stacks --stack-name "$SAM_STACK" --query "Stacks[0].Outputs[?OutputKey=='PrivateApiUrl'].OutputValue" --output text)
+STS_MOCK_API_URL=$(aws cloudformation describe-stacks --stack-name "$SAM_STACK" --query "Stacks[0].Outputs[?OutputKey=='StsMockApiUrl'].OutputValue" --output text)
 
 echo "SESSIONS_API_URL=$SESSIONS_API_URL" > .env
 echo "PROXY_API_URL=$PROXY_API_URL" >> .env
 echo "PRIVATE_API_URL=$PRIVATE_API_URL" >> .env
+echo "STS_MOCK_API_URL=$STS_MOCK_API_URL" >> .env
 echo "TEST_ENVIRONMENT=dev" >> .env
 echo "IS_LOCAL_TEST=true" >> .env
