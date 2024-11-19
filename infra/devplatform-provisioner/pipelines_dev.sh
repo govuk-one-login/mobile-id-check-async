8ee9e1687582de2964bfcf9dabf79bb718ebcd38
Douglas Thomson <douglas.thomson@digital.cabinet-office.gov.uk>
2024-11-19 10:30:27 +0000

    tempplate

--- infra/devplatform-provisioner/pipelines_dev.sh
+++ infra/devplatform-provisioner/pipelines_dev.sh
@@ -5,7 +5,7 @@ aws_account_name=di-mobile-id-check-async-dev
 
 sh account_check.sh ${aws_account_name}
 
-provisioner legacy --aws-account-name "${aws_account_name}" --stack-name mob-async-backend-pl  --template-name sam-deploy-pipeline   --template-version v2.68.1
+provisioner legacy --aws-account-name "${aws_account_name}" --stack-name mob-async-backend-pl --template-name sam-deploy-pipeline --template-version v2.68.1
 provisioner legacy --aws-account-name "${aws_account_name}" --stack-name mob-async-backend-tir --template-name test-image-repository --template-version v1.1.10
-provisioner legacy --aws-account-name "${aws_account_name}" --stack-name mob-sts-mock-pl       --template-name sam-deploy-pipeline   --template-version v2.68.1
-provisioner legacy --aws-account-name "${aws_account_name}" --stack-name mob-sts-mock-tir      --template-name test-image-repository --template-version v1.2.0
+provisioner legacy --aws-account-name "${aws_account_name}" --stack-name mob-sts-mock-pl --template-name sam-deploy-pipeline --template-version v2.68.1
+provisioner legacy --aws-account-name "${aws_account_name}" --stack-name mob-sts-mock-tir --template-name test-image-repository --template-version v1.2.0
