2e417c31e0080fa7c8858aea630a7a2b09dc46ca
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2024-11-27 15:18:38 +0000

    handle sqs events in dequeue lambda

--- backend-api/src/functions/txma/dequeueTxmaEventsHandler.ts
+++ backend-api/src/functions/txma/dequeueTxmaEventsHandler.ts
@@ -3,7 +3,25 @@ import { Context, SQSEvent } from "aws-lambda";
 export const lambdaHandler = async (event: SQSEvent, context: Context): Promise<void> => {
   console.log("STARTED")
 
-  console.log(event.Records)
+  const records = event.Records
+
+  let events: Event[] | null = []
+  records.forEach((record) => {
+    const messageId = record.messageId
+    const event_name = JSON.parse(record.body).event_name
+
+    events.push({
+      messageId,
+      event_name
+    })
+  })
+
+  console.log(events)
 
   console.log("COMPLETED")
+}
+
+interface Event {
+  messageId: string
+  event_name: string
 }
---
+++ backend-api/src/functions/txma/dequeueTxmaEventsUnit.test.ts
@@ -0,0 +1,41 @@
+import { Context, SQSEvent } from "aws-lambda"
+import { buildLambdaContext } from "../testUtils/mockContext"
+import { lambdaHandler } from "./dequeueTxmaEventsHandler"
+
+describe("Dequeue TxMA events", () => {
+  describe("Given one or messages in the request", () => {
+    it("Returns the messageId and event_name for each message", async () => {
+      const consoleLogSpy = jest.spyOn(global.console, "log")
+      const event: SQSEvent = {
+        Records: [
+          {
+            "messageId": "E8CA2168-36C2-4CAF-8CAC-9915B849E1E5",
+            "receiptHandle": "AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...",
+            "body": JSON.stringify({
+              event_name: "MOCK_EVENT_NAME",
+            }),
+            "attributes": {
+              "ApproximateReceiveCount": "1",
+              "SentTimestamp": "1545082649183",
+              "SenderId": "AIDAIENQZJOLO23YVJ4VO",
+              "ApproximateFirstReceiveTimestamp": "1545082649185"
+            },
+            "messageAttributes": {},
+            "md5OfBody": "098f6bcd4621d373cade4e832627b4f6",
+            "eventSource": "aws:sqs",
+            "eventSourceARN": "arn:aws:sqs:eu-west-2:111122223333:my-queue",
+            "awsRegion": "eu-west-2"
+          }
+        ]
+      }
+      const context: Context = buildLambdaContext()
+
+      await lambdaHandler(event, context)
+
+      expect(consoleLogSpy).toHaveBeenNthCalledWith(2, [{
+        messageId: "E8CA2168-36C2-4CAF-8CAC-9915B849E1E5",
+        event_name: "MOCK_EVENT_NAME"
+      }])
+    })
+  })
+})
