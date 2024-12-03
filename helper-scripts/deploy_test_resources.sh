45f0e199ccf4df5aacb3a71295f4c4e0919ab48c
jhumbert-dd <178896864+jhumbert-dd@users.noreply.github.com>
2024-12-03 09:04:35 +0000

    DCAMW-10645: Add dequeue Lambda that logs when reading messages (#272)

---
+++ helper-scripts/deploy_test_resources.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 <stack-name> <optional-custom-backend-stack-name>"
+  exit 1
+fi
+
+if [ "$(aws sts get-caller-identity --output text --query 'Account')" != "211125300205" ]; then
+  echo "This script is attempting to be run in a NON DEV account. Please verify your AWS environment configuration"
+  exit 1
+fi
+
+STACK_NAME=$1
+
+TEST_RESOURCES_STACK_NAME="${STACK_NAME}-test-resources"
+BACKEND_STACK_NAME=${2:-"mob-async-backend"}
+
+while true; do
+  echo
+  read -r -p "Do you want to deploy a test-resources stack? [Y/n]: " yn
+
+  case "$yn" in
+    [yY] | "")
+      echo "Building and deploying test-resources stack: $TEST_RESOURCES_STACK_NAME"
+      echo
+      cd ../test-resources
+      npm i
+      sam build --cached
+      sam deploy \
+        --stack-name "$TEST_RESOURCES_STACK_NAME" \
+        --parameter-overrides "BackendApi=${BACKEND_STACK_NAME}" \
+        --capabilities CAPABILITY_NAMED_IAM \
+        --resolve-s3
+      break
+      ;;
+    [nN])
+      echo "Skipping test-resources deployment"
+      break
+      ;;
+    *)
+      echo "Invalid input. Please enter 'y' or 'n'."
+      ;;
+  esac
+done
---
+++ test-resources/dequeue/dequeueHandler.test.ts
@@ -0,0 +1,104 @@
+import { SQSEvent } from "aws-lambda";
+import { lambdaHandler } from "./dequeueHandler";
+
+describe("Dequeue TxMA events", () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  describe("Given one message is sent in the request", () => {
+    it("Returns the messageId and event_name for each message", async () => {
+      const consoleLogSpy = jest.spyOn(global.console, "log");
+      const event: SQSEvent = {
+        Records: [
+          {
+            messageId: "E8CA2168-36C2-4CAF-8CAC-9915B849E1E5",
+            receiptHandle: "mockReceiptHandle",
+            body: JSON.stringify({
+              event_name: "MOCK_EVENT_NAME",
+            }),
+            attributes: {
+              ApproximateReceiveCount: "1",
+              SentTimestamp: "1545082649183",
+              SenderId: "AIDAIENQZJOLO23YVJ4VO",
+              ApproximateFirstReceiveTimestamp: "1545082649185",
+            },
+            messageAttributes: {},
+            md5OfBody: "098f6bcd4621d373cade4e832627b4f6",
+            eventSource: "aws:sqs",
+            eventSourceARN: "arn:aws:sqs:eu-west-2:111122223333:my-queue",
+            awsRegion: "eu-west-2",
+          },
+        ],
+      };
+
+      await lambdaHandler(event);
+
+      expect(consoleLogSpy).toHaveBeenNthCalledWith(2, [
+        {
+          messageId: "E8CA2168-36C2-4CAF-8CAC-9915B849E1E5",
+          eventName: "MOCK_EVENT_NAME",
+        },
+      ]);
+    });
+  });
+
+  describe("Given more than one message is sent in the request", () => {
+    it("Returns the messageId and event_name for each message", async () => {
+      const consoleLogSpy = jest.spyOn(global.console, "log");
+      const event: SQSEvent = {
+        Records: [
+          {
+            messageId: "E8CA2168-36C2-4CAF-8CAC-9915B849E1E5",
+            receiptHandle: "mockReceiptHandle",
+            body: JSON.stringify({
+              event_name: "MOCK_EVENT_NAME",
+            }),
+            attributes: {
+              ApproximateReceiveCount: "1",
+              SentTimestamp: "1545082649183",
+              SenderId: "AIDAIENQZJOLO23YVJ4VO",
+              ApproximateFirstReceiveTimestamp: "1545082649185",
+            },
+            messageAttributes: {},
+            md5OfBody: "098f6bcd4621d373cade4e832627b4f6",
+            eventSource: "aws:sqs",
+            eventSourceARN: "arn:aws:sqs:eu-west-2:111122223333:my-queue",
+            awsRegion: "eu-west-2",
+          },
+          {
+            messageId: "4008E4FD-10A1-461F-9B34-910BCE726C55",
+            receiptHandle: "mockReceiptHandle",
+            body: JSON.stringify({
+              event_name: "MOCK_EVENT_NAME_2",
+            }),
+            attributes: {
+              ApproximateReceiveCount: "1",
+              SentTimestamp: "1545082649183",
+              SenderId: "AIDAIENQZJOLO23YVJ4VO",
+              ApproximateFirstReceiveTimestamp: "1545082649185",
+            },
+            messageAttributes: {},
+            md5OfBody: "098f6bcd4621d373cade4e832627b4f6",
+            eventSource: "aws:sqs",
+            eventSourceARN: "arn:aws:sqs:eu-west-2:111122223333:my-queue",
+            awsRegion: "eu-west-2",
+          },
+        ],
+      };
+
+      await lambdaHandler(event);
+
+      expect(consoleLogSpy).toHaveBeenNthCalledWith(2, [
+        {
+          messageId: "E8CA2168-36C2-4CAF-8CAC-9915B849E1E5",
+          eventName: "MOCK_EVENT_NAME",
+        },
+        {
+          messageId: "4008E4FD-10A1-461F-9B34-910BCE726C55",
+          eventName: "MOCK_EVENT_NAME_2",
+        },
+      ]);
+    });
+  });
+});
