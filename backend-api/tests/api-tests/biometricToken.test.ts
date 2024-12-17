3eed5cc5078c4ee864ea50ad608301b096bc225d
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2024-12-17 12:26:41 +0000

    Adds API test - asserts 501 when request is made

---
+++ backend-api/tests/api-tests/biometricToken.test.ts
@@ -0,0 +1,12 @@
+import { SESSIONS_API_INSTANCE } from "./utils/apiInstance";
+
+describe("GET /async/activeSession", () => {
+  describe("Given there is a request", () => {
+    it("Returns an error and 501 status code", async () => {
+      const response = await SESSIONS_API_INSTANCE.post("/async/biometricToken");
+
+      expect(response.status).toBe(501);
+      expect(response.data).toStrictEqual({ error: "Not Implemented" })
+    })
+  })
+})
