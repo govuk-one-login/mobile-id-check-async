19a1302aa5c03c717e10109c5787673a94ecc531
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2024-12-19 19:50:42 +0000

    Add happy test for validateRequestBody func

--- backend-api/src/functions/asyncBiometricToken/validateRequestBody/validateRequestBody.test.ts
+++ backend-api/src/functions/asyncBiometricToken/validateRequestBody/validateRequestBody.test.ts
@@ -1,77 +1,101 @@
 import { validateRequestBody } from "./validateRequestBody";
 
 describe("Validate request body", () => {
-  describe.each([
-    [
-      "Given body is null or undefined",
-      null,
-      "Request body is either null or undefined.",
-    ],
-    [
-      "Given body cannot be parsed as JSON",
-      "invalidJSON",
-      `Request body could not be parsed as JSON. SyntaxError: Unexpected token 'i', "invalidJSON" is not valid JSON`,
-    ],
-  ])("%s", (_description, body, errorMessage) => {
-    it("Returns an error result", () => {
-      const result = validateRequestBody(body);
+  describe("Given body is invalid", () => {
+    describe.each([
+      [
+        "Given body is null or undefined",
+        null,
+        "Request body is either null or undefined.",
+      ],
+      [
+        "Given body cannot be parsed as JSON",
+        "invalidJSON",
+        `Request body could not be parsed as JSON. SyntaxError: Unexpected token 'i', "invalidJSON" is not valid JSON`,
+      ],
+    ])("%s", (_description, body, errorMessage) => {
+      it("Returns an error result", () => {
+        const result = validateRequestBody(body);
 
-      expect(result).toStrictEqual({
-        isError: true,
-        value: {
-          errorMessage,
+        expect(result).toStrictEqual({
+          isError: true,
+          value: {
+            errorMessage,
+          },
+        });
+      });
+    });
+
+    describe.each([
+      [
+        "Given sessionId is not present",
+        {},
+        "sessionId in request body is either null or undefined.",
+      ],
+      [
+        "Given sessionId is not a string",
+        { sessionId: 123 },
+        "sessionId in request body is not of type string. sessionId: 123",
+      ],
+      [
+        "Given sessionId is an empty string",
+        { sessionId: "" },
+        "sessionId in request body is an empty string.",
+      ],
+      [
+        "Given documentType is not present",
+        { sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4" },
+        "documentType in request body is either null or undefined.",
+      ],
+      [
+        "Given documentType is not a string",
+        {
+          sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4",
+          documentType: 123,
         },
+        "documentType in request body is not of type string. documentType: 123",
+      ],
+      [
+        "Given documentType is an empty string",
+        { sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4", documentType: "" },
+        "documentType in request body is an empty string.",
+      ],
+      [
+        "Given documentType is an invalid document type",
+        {
+          sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4",
+          documentType: "BUS_PASS",
+        },
+        "documentType in request body is invalid. documentType: BUS_PASS",
+      ],
+    ])("%s", (_description, body, errorMessage) => {
+      it("Returns an error result", () => {
+        const result = validateRequestBody(JSON.stringify(body));
+
+        expect(result).toStrictEqual({
+          isError: true,
+          value: {
+            errorMessage,
+          },
+        });
       });
     });
   });
 
-  describe.each([
-    [
-      "Given sessionId is not present",
-      {},
-      "sessionId in request body is either null or undefined.",
-    ],
-    [
-      "Given sessionId is not a string",
-      { sessionId: 123 },
-      "sessionId in request body is not of type string. sessionId: 123",
-    ],
-    [
-      "Given sessionId is an empty string",
-      { sessionId: "" },
-      "sessionId in request body is an empty string.",
-    ],
-    [
-      "Given documentType is not present",
-      { sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4" },
-      "documentType in request body is either null or undefined.",
-    ],
-    [
-      "Given documentType is not a string",
-      { sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4", documentType: 123 },
-      "documentType in request body is not of type string. documentType: 123",
-    ],
-    [
-      "Given documentType is an empty string",
-      { sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4", documentType: "" },
-      "documentType in request body is an empty string.",
-    ],
-    [
-      "Given documentType is an invalid document type",
-      {
-        sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4",
-        documentType: "BUS_PASS",
-      },
-      "documentType in request body is invalid. documentType: BUS_PASS",
-    ],
-  ])("%s", (_description, body, errorMessage) => {
-    it("Returns an error result", () => {
-      const result = validateRequestBody(JSON.stringify(body));
+  describe("Given body is valid", () => {
+    it("Returns a successResult with a valid parsed body as value", () => {
+      const result = validateRequestBody(
+        JSON.stringify({
+          sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4",
+          documentType: "NFC_PASSPORT",
+        }),
+      );
 
       expect(result).toStrictEqual({
-        isError: true,
+        isError: false,
         value: {
-          errorMessage,
+          sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4",
+          documentType: "NFC_PASSPORT",
         },
       });
     });
