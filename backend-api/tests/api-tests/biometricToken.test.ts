767be26ba77a3d864eb1d9b8414586b3506f8c0b
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2024-12-19 16:57:11 +0000

    Update api test to reflect error_description change

--- backend-api/tests/api-tests/biometricToken.test.ts
+++ backend-api/tests/api-tests/biometricToken.test.ts
@@ -1,9 +1,13 @@
 import { SESSIONS_API_INSTANCE } from "./utils/apiInstance";
 
 describe("POST /async/biometricToken", () => {
+  const sessionId = "58f4281d-d988-49ce-9586-6ef70a2be0b4";
   describe("Given request body is invalid", () => {
     it("Returns an error and 400 status code", async () => {
-      const requestBody = { documentType: 123 };
+      const requestBody = {
+        sessionId,
+        documentType: "BUS_PASS",
+      };
 
       const response = await SESSIONS_API_INSTANCE.post(
         "/async/biometricToken",
@@ -13,7 +17,8 @@ describe("POST /async/biometricToken", () => {
       expect(response.status).toBe(400);
       expect(response.data).toStrictEqual({
         error: "invalid_request",
-        error_description: "Request body invalid",
+        error_description:
+          "documentType in request body is invalid. documentType: BUS_PASS",
       });
     });
   });
@@ -21,7 +26,7 @@ describe("POST /async/biometricToken", () => {
   describe("Given there is a valid request", () => {
     it("Returns an error and 501 status code", async () => {
       const requestBody = {
-        sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4",
+        sessionId,
         documentType: "NFC_PASSPORT",
       };
 
