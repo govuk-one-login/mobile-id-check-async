65c70adbe1d6ef702e8dcc43a1bae08b70b46e33
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2024-12-19 15:15:42 +0000

    Remove error cat as always client error with req body validation

--- backend-api/src/functions/asyncBiometricToken/getParsedRequestBody/getParsedRequestBody.ts
+++ backend-api/src/functions/asyncBiometricToken/getParsedRequestBody/getParsedRequestBody.ts
@@ -6,7 +6,6 @@ export function getParsedRequestBody(
   if (body == null) {
     return errorResult({
       errorMessage: `Request body is either null or undefined. Request body: ${body}`,
-      errorCategory: "CLIENT_ERROR",
     });
   }
 
@@ -16,7 +15,6 @@ export function getParsedRequestBody(
   } catch (error: unknown) {
     return errorResult({
       errorMessage: `Request body could not be parsed as JSON. ${error}`,
-      errorCategory: "CLIENT_ERROR",
     });
   }
   const { sessionId, documentType } = parsedBody;
@@ -24,49 +22,42 @@ export function getParsedRequestBody(
   if (sessionId == null) {
     return errorResult({
       errorMessage: `sessionId in request body is either null or undefined. sessionId: ${sessionId}`,
-      errorCategory: "CLIENT_ERROR",
     });
   }
 
   if (!isString(sessionId)) {
     return errorResult({
       errorMessage: `sessionId in request body is not of type string. sessionId: ${sessionId}`,
-      errorCategory: "CLIENT_ERROR"
     })
   }
 
   if (sessionId === "") {
     return errorResult({
       errorMessage: `sessionId in request body is an empty string. sessionId: ${sessionId}`,
-      errorCategory: "CLIENT_ERROR",
     });
   }
 
   if (documentType == null) {
     return errorResult({
       errorMessage: `documentType in request body is either null or undefined. documentType: ${documentType}`,
-      errorCategory: "CLIENT_ERROR",
     });
   }
 
   if (!isString(documentType)) {
     return errorResult({
       errorMessage: `documentType in request body is not of type string. documentType: ${documentType}`,
-      errorCategory: "CLIENT_ERROR"
     })
   }
 
   if (documentType === "") {
     return errorResult({
       errorMessage: `documentType in request body is an empty string. documentType: ${documentType}`,
-      errorCategory: "CLIENT_ERROR",
     });
   }
 
   if (!isAllowableDocument(documentType)) {
     return errorResult({
       errorMessage: `documentType in request body is invalid. documentType: ${documentType}`,
-      errorCategory: "CLIENT_ERROR",
     });
   }
 
