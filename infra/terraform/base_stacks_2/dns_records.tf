8d2b90c80232782703b716ef3db59d51a8599109
Douglas Thomson <douglas.thomson@digital.cabinet-office.gov.uk>
2024-12-20 14:35:01 +0000

    Removing infra directory as it has moved to mobile-id-check-async-infra

--- infra/terraform/base_stacks_2/dns_records.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_cloudformation_stack" "dns_records" {
-    name = "platform-dns-records"
-
-    template_body = file("../../templates/dns-records/template.yaml")
-
-    parameters = {
-        Environment = var.environment
-    }
-}
