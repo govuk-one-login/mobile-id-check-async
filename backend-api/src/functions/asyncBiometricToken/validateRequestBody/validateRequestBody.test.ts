9024ecb2287f031d3e644737408893fa131e66f9
Chris Cooksley <chris.cooksley@digital.cabinet-office.gov.uk>
2024-12-20 09:58:06 +0000

    Add keys to parameterised tests

--- backend-api/src/functions/asyncBiometricToken/validateRequestBody/validateRequestBody.test.ts
+++ backend-api/src/functions/asyncBiometricToken/validateRequestBody/validateRequestBody.test.ts
@@ -3,79 +3,88 @@ import { validateRequestBody } from "./validateRequestBody";
 describe("Validate request body", () => {
   describe("Given body is invalid", () => {
     describe.each([
-      [
-        "Given body is null or undefined",
-        null,
-        "Request body is either null or undefined.",
-      ],
-      [
-        "Given body cannot be parsed as JSON",
-        "invalidJSON",
-        `Request body could not be parsed as JSON. SyntaxError: Unexpected token 'i', "invalidJSON" is not valid JSON`,
-      ],
-    ])("%s", (_description, body, errorMessage) => {
+      {
+        scenario: "Given body is null or undefined",
+        requestBody: null,
+        expectedErrorMessage: "Request body is either null or undefined.",
+      },
+      {
+        scenario: "Given body cannot be parsed as JSON",
+        requestBody: "invalidJSON",
+        expectedErrorMessage: `Request body could not be parsed as JSON. SyntaxError: Unexpected token 'i', "invalidJSON" is not valid JSON`,
+      },
+    ])("$scenario", ({ requestBody, expectedErrorMessage }) => {
       it("Returns an error result", () => {
-        const result = validateRequestBody(body);
+        const result = validateRequestBody(requestBody);
 
         expect(result).toStrictEqual({
           isError: true,
           value: {
-            errorMessage,
+            errorMessage: expectedErrorMessage,
           },
         });
       });
     });
 
     describe.each([
-      [
-        "Given sessionId is not present",
-        {},
-        "sessionId in request body is either null or undefined.",
-      ],
-      [
-        "Given sessionId is not a string",
-        { sessionId: 123 },
-        "sessionId in request body is not of type string. sessionId: 123",
-      ],
-      [
-        "Given sessionId is an empty string",
-        { sessionId: "" },
-        "sessionId in request body is an empty string.",
-      ],
-      [
-        "Given documentType is not present",
-        { sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4" },
-        "documentType in request body is either null or undefined.",
-      ],
-      [
-        "Given documentType is not a string",
-        {
+      {
+        scenario: "Given sessionId is not present",
+        requestBody: {},
+        expectedErrorMessage:
+          "sessionId in request body is either null or undefined.",
+      },
+      {
+        scenario: "Given sessionId is not a string",
+        requestBody: { sessionId: 123 },
+        expectedErrorMessage:
+          "sessionId in request body is not of type string. sessionId: 123",
+      },
+      {
+        scenario: "Given sessionId is an empty string",
+        requestBody: { sessionId: "" },
+        expectedErrorMessage: "sessionId in request body is an empty string.",
+      },
+      {
+        scenario: "Given documentType is not present",
+        requestBody: { sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4" },
+        expectedErrorMessage:
+          "documentType in request body is either null or undefined.",
+      },
+      {
+        scenario: "Given documentType is not a string",
+        requestBody: {
           sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4",
           documentType: 123,
         },
-        "documentType in request body is not of type string. documentType: 123",
-      ],
-      [
-        "Given documentType is an empty string",
-        { sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4", documentType: "" },
-        "documentType in request body is an empty string.",
-      ],
-      [
-        "Given documentType is an invalid document type",
-        {
+        expectedErrorMessage:
+          "documentType in request body is not of type string. documentType: 123",
+      },
+      {
+        scenario: "Given documentType is an empty string",
+        requestBody: {
+          sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4",
+          documentType: "",
+        },
+        expectedErrorMessage:
+          "documentType in request body is an empty string.",
+      },
+      {
+        scenario: "Given documentType is an invalid document type",
+        requestBody: {
           sessionId: "58f4281d-d988-49ce-9586-6ef70a2be0b4",
           documentType: "BUS_PASS",
         },
-        "documentType in request body is invalid. documentType: BUS_PASS",
-      ],
-    ])("%s", (_description, body, errorMessage) => {
+        expectedErrorMessage:
+          "documentType in request body is invalid. documentType: BUS_PASS",
+      },
+    ])("$scenario", ({ requestBody, expectedErrorMessage }) => {
       it("Returns an error result", () => {
-        const result = validateRequestBody(JSON.stringify(body));
+        const result = validateRequestBody(JSON.stringify(requestBody));
 
         expect(result).toStrictEqual({
           isError: true,
           value: {
-            errorMessage,
+            errorMessage: expectedErrorMessage,
           },
         });
       });
