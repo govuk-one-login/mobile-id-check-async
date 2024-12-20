82d4ecb501949cf187a61ee11627cc550012e5de
Douglas Thomson <douglas.thomson@digital.cabinet-office.gov.uk>
2024-12-20 14:22:56 +0000

    Removing provisioner files since we have moved to mobile-id-check-async-infra

--- infra/devplatform-provisioner/pipelines_integration.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-aws_account_name=di-mobile-id-check-async-integration
-
-sh account_check.sh ${aws_account_name}
-
-provisioner legacy --aws-account-name "${aws_account_name}" --stack-name mob-async-backend-pl --template-name sam-deploy-pipeline --template-version v2.68.1
