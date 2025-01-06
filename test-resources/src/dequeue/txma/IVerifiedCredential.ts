e09902e71b9fe297e14889f15bc9e387f6cec922
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2025-01-06 14:48:19 +0000

    add txma and credential types

--- test-resources/src/dequeue/txma/IVerifiedCredential.ts
+++ test-resources/src/dequeue/txma/IVerifiedCredential.ts
@@ -1,120 +1,119 @@
-
 export interface CredentialSubject {
-  name: object[]
-  birthDate: object[]
-  passport?: Passport[]
-  residencePermit?: ResidencePermit[]
-  address?: object[]
-  drivingPermit?: DrivingPermit[]
-  deviceId?: DeviceId[]
-  flaggedRecord?: FlaggedRecord[]
+  name: object[];
+  birthDate: object[];
+  passport?: Passport[];
+  residencePermit?: ResidencePermit[];
+  address?: object[];
+  drivingPermit?: DrivingPermit[];
+  deviceId?: DeviceId[];
+  flaggedRecord?: FlaggedRecord[];
 }
 
 export interface FlaggedRecord {
-  dateOfBirth?: TypeValuePair[]
+  dateOfBirth?: TypeValuePair[];
 }
 
 export interface TypeValuePair {
-  type: string
-  value: string | null
+  type: string;
+  value: string | null;
 }
 
 export type DcmawFlags = {
-  [key in DcmawFlagsType]?: Flags
-}
+  [key in DcmawFlagsType]?: Flags;
+};
 
 export enum DcmawFlagsType {
-  PASSPORT = 'dcmawFlagsPassport',
-  DRIVERS_LICENCE = 'dcmawFlagsDL',
-  BRP = 'dcmawFlagsBRP',
-  INVALID = 'invalid'
+  PASSPORT = "dcmawFlagsPassport",
+  DRIVERS_LICENCE = "dcmawFlagsDL",
+  BRP = "dcmawFlagsBRP",
+  INVALID = "invalid",
 }
 
 export enum nfcFlagTypes {
-  PASSPORT = 'dcmawFlagsPassport',
-  BRP = 'dcmawFlagsBRP'
+  PASSPORT = "dcmawFlagsPassport",
+  BRP = "dcmawFlagsBRP",
 }
 
 export interface Flags {
-  doBUnknown?: boolean
-  doBMonthGreaterThan12?: boolean
-  doBDateGreaterThan31?: boolean
-  doBMoreThan100?: boolean
-  doBMismatched?: boolean
-  doBLicenceCheck?: boolean
-  doEInPast?: boolean | null
-  doEMonthGreaterThan12?: boolean
-  doEDateGreaterThan31?: boolean
-  doEMismatched?: boolean
-  doEGreaterThan31Dec2024?: boolean
-  doIMonthGreaterThan12?: boolean
-  doIDateGreaterThan31?: boolean
+  doBUnknown?: boolean;
+  doBMonthGreaterThan12?: boolean;
+  doBDateGreaterThan31?: boolean;
+  doBMoreThan100?: boolean;
+  doBMismatched?: boolean;
+  doBLicenceCheck?: boolean;
+  doEInPast?: boolean | null;
+  doEMonthGreaterThan12?: boolean;
+  doEDateGreaterThan31?: boolean;
+  doEMismatched?: boolean;
+  doEGreaterThan31Dec2024?: boolean;
+  doIMonthGreaterThan12?: boolean;
+  doIDateGreaterThan31?: boolean;
 }
 
 export interface DeviceId {
-  value: string | null
+  value: string | null;
 }
 
 export interface VerifiedCredential {
-  '@context': string[]
-  type: string[]
-  credentialSubject: CredentialSubject
-  evidence: Array<PassEvidence | FailEvidence>
+  "@context": string[];
+  type: string[];
+  credentialSubject: CredentialSubject;
+  evidence: Array<PassEvidence | FailEvidence>;
 }
 
 export interface Passport {
-  documentNumber: string | null
-  expiryDate: string | null
-  icaoIssuerCode: string | null
+  documentNumber: string | null;
+  expiryDate: string | null;
+  icaoIssuerCode: string | null;
 }
 
 export interface ResidencePermit {
-  documentNumber: string | null
-  expiryDate: string | null
-  icaoIssuerCode: string | null
-  documentType: string | null
+  documentNumber: string | null;
+  expiryDate: string | null;
+  icaoIssuerCode: string | null;
+  documentType: string | null;
 }
 
 export interface DrivingPermit {
-  personalNumber: string | null
-  issueNumber: string | null
-  issuedBy: string | null
-  issueDate: string | null
-  expiryDate: string | null
-  fullAddress: string | null
+  personalNumber: string | null;
+  issueNumber: string | null;
+  issuedBy: string | null;
+  issueDate: string | null;
+  expiryDate: string | null;
+  fullAddress: string | null;
 }
 export interface PassEvidence {
-  type: 'IdentityCheck'
-  txn: string
-  strengthScore: number
-  validityScore: number
-  activityHistoryScore?: number
-  ci?: string[]
-  checkDetails: any[]
-  txmaContraIndicators?: any[]
+  type: "IdentityCheck";
+  txn: string;
+  strengthScore: number;
+  validityScore: number;
+  activityHistoryScore?: number;
+  ci?: string[];
+  checkDetails: any[];
+  txmaContraIndicators?: any[];
 }
 export interface FailEvidence {
-  type: 'IdentityCheck'
-  txn: string
-  strengthScore: number
-  validityScore: number
-  ci: string[]
-  failedCheckDetails: any[]
-  txmaContraIndicators?: any[]
+  type: "IdentityCheck";
+  txn: string;
+  strengthScore: number;
+  validityScore: number;
+  ci: string[];
+  failedCheckDetails: any[];
+  txmaContraIndicators?: any[];
 }
 
 export interface CredentialJwt {
-  iat: number
-  iss: string
-  nbf: number
-  sub: string
-  aud: string
-  exp?: number
-  jti: string
-  vc: VerifiedCredential
+  iat: number;
+  iss: string;
+  nbf: number;
+  sub: string;
+  aud: string;
+  exp?: number;
+  jti: string;
+  vc: VerifiedCredential;
 }
 
 export interface SignedCredentialJwt {
-  sub: string
-  'https://vocab.account.gov.uk/v1/credentialJWT': [string]
+  sub: string;
+  "https://vocab.account.gov.uk/v1/credentialJWT": [string];
 }
--- test-resources/src/dequeue/txma/TxmaEventTypes.ts
+++ test-resources/src/dequeue/txma/TxmaEventTypes.ts
@@ -1,178 +1,193 @@
-import { Flags, VerifiedCredential } from './IVerifiedCredential'
+import { Flags, VerifiedCredential } from "./IVerifiedCredential";
 
-export const TXMA_HEADER = 'txma-audit-encoded'
+export const TXMA_HEADER = "txma-audit-encoded";
 
-export type EncodedHeader = string | string[] | undefined
-export type IpAddress = string | string[]
+export type EncodedHeader = string | string[] | undefined;
+export type IpAddress = string | string[];
 
 export interface TxmaUser {
-  user_id: string
-  transaction_id: string
-  session_id: string
-  govuk_signin_journey_id: string
-  ip_address?: string | string[]
-  deviceId?: string
+  user_id: string;
+  transaction_id: string;
+  session_id: string;
+  govuk_signin_journey_id: string;
+  ip_address?: string | string[];
+  deviceId?: string;
 }
 
 export interface TxmaDeviceInfo {
   deviceInfo: {
-    brand: string | null
-    manufacturer: string | null
-    model: string | null
-    OSVersion: string | null
-    platform: string | null
-    timestamp: string | null
-  }
+    brand: string | null;
+    manufacturer: string | null;
+    model: string | null;
+    OSVersion: string | null;
+    platform: string | null;
+    timestamp: string | null;
+  };
 }
 
-export type AbortTxmaEventName =
-  'DCMAW_ABORT_APP'
-  | 'DCMAW_ABORT_WEB'
+export type AbortTxmaEventName = "DCMAW_ABORT_APP" | "DCMAW_ABORT_WEB";
 
-type ErrorTxmaEventName =
-  'DCMAW_CRI_4XXERROR'
-  | 'DCMAW_CRI_5XXERROR'
+type ErrorTxmaEventName = "DCMAW_CRI_4XXERROR" | "DCMAW_CRI_5XXERROR";
 
 type RedirectTxmaEventName =
-  'DCMAW_REDIRECT_SUCCESS'
-  | 'DCMAW_REDIRECT_ABORT'
-  | 'DCMAW_MISSING_CONTEXT_AFTER_ABORT'
-  | 'DCMAW_MISSING_CONTEXT_AFTER_COMPLETION'
+  | "DCMAW_REDIRECT_SUCCESS"
+  | "DCMAW_REDIRECT_ABORT"
+  | "DCMAW_MISSING_CONTEXT_AFTER_ABORT"
+  | "DCMAW_MISSING_CONTEXT_AFTER_COMPLETION";
 
 export type DocumentSelectedTxmaEventName =
-  'DCMAW_PASSPORT_SELECTED'
-  | 'DCMAW_BRP_SELECTED'
-  | 'DCMAW_DRIVING_LICENCE_SELECTED'
+  | "DCMAW_PASSPORT_SELECTED"
+  | "DCMAW_BRP_SELECTED"
+  | "DCMAW_DRIVING_LICENCE_SELECTED";
 
 export type GenericTxmaEventName =
-  ErrorTxmaEventName
-  | 'DCMAW_CRI_END'
-  | 'DCMAW_CRI_ABORT'
+  | ErrorTxmaEventName
+  | "DCMAW_CRI_END"
+  | "DCMAW_CRI_ABORT";
 
-export type AppHandoffTxmaEventName = 'DCMAW_APP_HANDOFF_START'
+export type AppHandoffTxmaEventName = "DCMAW_APP_HANDOFF_START";
 
 export type BillingTxmaEventName =
-  'DCMAW_HYBRID_BILLING_STARTED'
-  | 'DCMAW_IPROOV_BILLING_STARTED'
-  | 'DCMAW_READID_NFC_BILLING_STARTED'
+  | "DCMAW_HYBRID_BILLING_STARTED"
+  | "DCMAW_IPROOV_BILLING_STARTED"
+  | "DCMAW_READID_NFC_BILLING_STARTED";
 
 export type UserInteractionTxmaEventName =
-  AppHandoffTxmaEventName
+  | AppHandoffTxmaEventName
   | BillingTxmaEventName
   | AbortTxmaEventName
   | ErrorTxmaEventName
   | DocumentSelectedTxmaEventName
   | RedirectTxmaEventName
-  | 'DCMAW_CRI_START'
-  | 'DCMAW_APP_END'
+  | "DCMAW_CRI_START"
+  | "DCMAW_APP_END";
 
 export interface GenericTxmaEvent {
-  event_name: GenericTxmaEventName
-  user: TxmaUser
-  client_id: string
-  timestamp: number
-  event_timestamp_ms: number
-  component_id: string
+  event_name: GenericTxmaEventName;
+  user: TxmaUser;
+  client_id: string;
+  timestamp: number;
+  event_timestamp_ms: number;
+  component_id: string;
 }
 
 export interface UserInteractionTxmaEvent {
-  event_name: UserInteractionTxmaEventName
-  user: TxmaUser
-  client_id: string
-  timestamp: number
-  event_timestamp_ms: number
-  component_id: string
+  event_name: UserInteractionTxmaEventName;
+  user: TxmaUser;
+  client_id: string;
+  timestamp: number;
+  event_timestamp_ms: number;
+  component_id: string;
   restricted?: {
     device_information: {
-      encoded: string | string[]
-    }
-  }
+      encoded: string | string[];
+    };
+  };
 }
 
 export interface SessionRecoveredTxmaEvent {
-  event_name: 'DCMAW_SESSION_RECOVERED'
-  user: TxmaUser
-  client_id: string
-  timestamp: number
-  event_timestamp_ms: number
-  component_id: string
+  event_name: "DCMAW_SESSION_RECOVERED";
+  user: TxmaUser;
+  client_id: string;
+  timestamp: number;
+  event_timestamp_ms: number;
+  component_id: string;
   extensions: {
-    previous_govuk_signin_journey_id: string
-  }
+    previous_govuk_signin_journey_id: string;
+  };
   restricted?: {
     device_information: {
-      encoded: string | string[]
-    }
-  }
+      encoded: string | string[];
+    };
+  };
 }
 
 export interface AppStartTxmaEvent {
-  event_name: 'DCMAW_APP_START'
-  user: TxmaUser
+  event_name: "DCMAW_APP_START";
+  user: TxmaUser;
   extensions: {
-    opaque_id: string
-  }
-  client_id: string
-  timestamp: number
-  event_timestamp_ms: number
-  component_id: string
+    opaque_id: string;
+  };
+  client_id: string;
+  timestamp: number;
+  event_timestamp_ms: number;
+  component_id: string;
   restricted?: {
     device_information: {
-      encoded: string | string[]
-    }
-  }
+      encoded: string | string[];
+    };
+  };
 }
 
-export type UserInteractionSuspectedFraudSignal = 'AUTH_SESSION_TOO_OLD'
+export type UserInteractionSuspectedFraudSignal = "AUTH_SESSION_TOO_OLD";
 
 export interface UserInteractionCri4xxSuspectedFraudTxmaEvent {
-  event_name: 'DCMAW_CRI_4XXERROR'
-  client_id: string
-  component_id: string
-  event_timestamp_ms: number
-  timestamp: number
-  user: TxmaUser
+  event_name: "DCMAW_CRI_4XXERROR";
+  client_id: string;
+  component_id: string;
+  event_timestamp_ms: number;
+  timestamp: number;
+  user: TxmaUser;
   extensions: {
-    suspected_fraud_signal: UserInteractionSuspectedFraudSignal
-  }
+    suspected_fraud_signal: UserInteractionSuspectedFraudSignal;
+  };
   restricted?: {
     device_information: {
-      encoded: string | string[]
-    }
-  }
+      encoded: string | string[];
+    };
+  };
 }
 
 export type SuspectedFraudSignal =
-  'BIOMETRIC_SESSION_OLDER_THAN_AUTH_SESSION'
-  | 'BIOMETRIC_SESSION_OPAQUEID_MISMATCH'
+  | "BIOMETRIC_SESSION_OLDER_THAN_AUTH_SESSION"
+  | "BIOMETRIC_SESSION_OPAQUEID_MISMATCH";
 
 export interface Cri4xxSuspectedFraudTxmaEvent {
-  event_name: 'DCMAW_CRI_4XXERROR'
-  client_id: string
-  component_id: string
-  event_timestamp_ms: number
-  timestamp: number
-  user: TxmaUser
+  event_name: "DCMAW_CRI_4XXERROR";
+  client_id: string;
+  component_id: string;
+  event_timestamp_ms: number;
+  timestamp: number;
+  user: TxmaUser;
   extensions: {
-    suspected_fraud_signal: SuspectedFraudSignal
-  }
+    suspected_fraud_signal: SuspectedFraudSignal;
+  };
 }
 
 export interface VcIssuedTxmaEvent {
-  event_name: 'DCMAW_CRI_VC_ISSUED'
-  user: TxmaUser
-  client_id: string
-  timestamp: number
-  event_timestamp_ms: number
-  component_id: string
-  restricted: VerifiedCredential['credentialSubject']
+  event_name: "DCMAW_CRI_VC_ISSUED";
+  user: TxmaUser;
+  client_id: string;
+  timestamp: number;
+  event_timestamp_ms: number;
+  component_id: string;
+  restricted: VerifiedCredential["credentialSubject"];
   extensions: {
-    evidence: VerifiedCredential['evidence']
-    dcmawFlagsPassport?: Flags
-    dcmawFlagsDL?: Flags
-    dcmawFlagsBRP?: Flags
-  }
-  platform: TxmaDeviceInfo[]
+    evidence: VerifiedCredential["evidence"];
+    dcmawFlagsPassport?: Flags;
+    dcmawFlagsDL?: Flags;
+    dcmawFlagsBRP?: Flags;
+  };
+  platform: TxmaDeviceInfo[];
 }
 
-export type TxmaEvent = GenericTxmaEvent | UserInteractionTxmaEvent | SessionRecoveredTxmaEvent | AppStartTxmaEvent | VcIssuedTxmaEvent
+export type TxmaEvent =
+  | GenericTxmaEvent
+  | UserInteractionTxmaEvent
+  | SessionRecoveredTxmaEvent
+  | AppStartTxmaEvent
+  | VcIssuedTxmaEvent;
+
+const genericTxmaEvent = {
+  event_name: "MOCK_EVENT_NAME",
+  user: {
+    user_id: "mockUserId",
+    transaction_id: "mockTransactionId",
+    session_id: "mockSessionId",
+    govuk_signin_journey_id: "mockJourneyId",
+  },
+  client_id: "mockClientId",
+  timestamp: "mockTimestamp",
+  event_timestamp_ms: "mockTimestampMs",
+  component_id: "mockComponentId",
+};
