3c4c84dac6dc9803b5e1b9c17ad8ead693cb65f6
Sandy May <51717217+sandymay-dd@users.noreply.github.com>
2025-01-06 13:28:20 +0000

    DCMAW-10903: Remove infra folder following provisioner tool migration (#317)

--- infra/terraform/base_stacks_2/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "environment" {
-  description = "The configured environment to deploy to, 1-2-1 mapping to each account"
-  type        = string
-
-  validation {
-    condition     = contains(["dev", "build", "staging", "integration", "production"], var.environment)
-    error_message = "The environment provided must be a valid environment"
-  }
-}
