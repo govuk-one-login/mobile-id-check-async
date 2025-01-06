642952dc0d9a9678ed61d84405e094da891abca1
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2025-01-06 14:50:38 +0000

    write txma events to test-resources dynamodb table

--- test-resources/src/dequeue/dequeueHandler.ts
+++ test-resources/src/dequeue/dequeueHandler.ts
@@ -1,32 +1,68 @@
+import {
+  BatchWriteItemCommand,
+  DynamoDBClient,
+  PutRequest,
+} from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
+import { NodeHttpHandler } from "@smithy/node-http-handler";
 import { SQSEvent } from "aws-lambda";
+import { TxmaEvent } from "./txma/TxmaEventTypes";
 
 export const lambdaHandler = async (event: SQSEvent): Promise<void> => {
   console.log("STARTED");
 
   const records = event.Records;
-  const processedEvents: ProcessedEvent[] = [];
-  for (const record of records) {
-    const { messageId } = record;
-
-    let recordBody
-    let eventName: string
-    try {
-      recordBody = JSON.parse(record.body)
-      eventName = recordBody.event_name;
-      processedEvents.push({ messageId, eventName });
-      console.log(`Successfully processed message - messageId: ${messageId}`)
-    } catch (error) {
-      console.log(`Failed to process message - messageId: ${messageId}`)
-    }
+  const tableName = "jh-test-resources-dequeue-table";
+  const input: IDynamoDBBatchWriteItemInput = {
+    RequestItems: {
+      [tableName]: [],
+    },
   };
 
-  console.log(processedEvents);
+  for (const record of records) {
+    const txmaEvent: TxmaEvent = JSON.parse(record.body);
+
+    const putRequest: IPutRequest = {
+      PutRequest: {
+        Item: marshall({
+          pk: `TXMA#SESSION_ID#${txmaEvent.user.session_id}`,
+          sk: `EVENT_NAME#${txmaEvent.event_name}#EVENT_TIMESTAMP#${txmaEvent.timestamp}`,
+          eventBody: JSON.stringify(txmaEvent),
+        }),
+      },
+    };
+
+    input.RequestItems[tableName].push(putRequest);
+  }
+
+  const command = new BatchWriteItemCommand(input);
+
+  try {
+    await ddbClient.send(command);
+  } catch (error) {
+    console.log(`Error writing to DynamoDB: ${error}`);
+  }
 
   console.log("COMPLETED");
 };
 
-interface ProcessedEvent {
-  messageId: string;
-  eventName: string;
+const ddbClient = new DynamoDBClient({
+  region: "eu-west-2",
+  maxAttempts: 2,
+  requestHandler: new NodeHttpHandler({
+    connectionTimeout: 5000,
+    requestTimeout: 5000,
+  }),
+});
+
+interface IDynamoDBBatchWriteItemInput {
+  RequestItems: IRequestItems;
 }
 
+interface IRequestItems {
+  [tableName: string]: IPutRequest[];
+}
+
+interface IPutRequest {
+  PutRequest: PutRequest;
+}
