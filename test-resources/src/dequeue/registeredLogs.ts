fc93ac29c83886789c976db3e9613057a1d0771a
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2025-01-08 12:14:32 +0000

    remove unused log message names

--- test-resources/src/dequeue/registeredLogs.ts
+++ test-resources/src/dequeue/registeredLogs.ts
@@ -5,20 +5,12 @@ import {
 import { RegisteredLogMessages } from "../services/logging/types";
 
 export type MessageName =
-  | "INVALID_REQUEST"
-  | "INTERNAL_SERVER_ERROR"
   | "FAILED_TO_PROCESS_MESSAGES"
   | "PROCESSED_MESSAGES"
   | "ERROR_WRITING_EVENT_TO_DEQUEUE_TABLE"
   | CommonMessageNames;
 
 export const registeredLogs: RegisteredLogMessages<MessageName> = {
-  INVALID_REQUEST: {
-    messageCode: "MOBILE_ASYNC_INVALID_REQUEST",
-  },
-  INTERNAL_SERVER_ERROR: {
-    messageCode: "MOBILE_ASYNC_INTERNAL_SERVER_ERROR",
-  },
   FAILED_TO_PROCESS_MESSAGES: {
     messageCode: "MOBILE_ASYNC_FAILED_TO_PROCESS_MESSAGES",
   },
