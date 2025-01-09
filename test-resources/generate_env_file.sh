7fa9c31ef615a6968b620b7e01bee80ec79e1a36
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2025-01-09 10:57:05 +0000

    rename default stack name in generate_env_file.sh

--- test-resources/generate_env_file.sh
+++ test-resources/generate_env_file.sh
@@ -6,7 +6,7 @@ STACK_NAME_SUFFIX="-test-resources"
 if [ $# -ge 1 ] && [ -n "$1" ] ; then
   STACK_NAME="${1}${STACK_NAME_SUFFIX}"
 else
-  STACK_NAME="mob-async-backend${STACK_NAME_SUFFIX}" # default stack name in dev
+  STACK_NAME="mob-async${STACK_NAME_SUFFIX}" # default stack name in dev
 fi
 
 echo "Generating .env file for the $STACK_NAME stack"
