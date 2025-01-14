6cdf7280154dd8537dbd688ed2e2540f7dd023d0
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-01-14 12:59:43 +0000

    formatting

--- backend-api/src/functions/asyncBiometricToken/handlerDependencies.ts
+++ backend-api/src/functions/asyncBiometricToken/handlerDependencies.ts
@@ -1,6 +1,9 @@
 import { GetSecrets } from "../common/config/secrets";
 import { getSecretsFromParameterStore } from "../adapters/getSecretsFromParameterStore";
-import { getBiometricToken, GetBiometricToken } from "./getBiometricToken/getBiometricToken";
+import {
+  getBiometricToken,
+  GetBiometricToken,
+} from "./getBiometricToken/getBiometricToken";
 
 export type IAsyncBiometricTokenDependencies = {
   env: NodeJS.ProcessEnv;
@@ -8,9 +11,8 @@ export type IAsyncBiometricTokenDependencies = {
   getBiometricToken: GetBiometricToken;
 };
 
-
 export const runtimeDependencies: IAsyncBiometricTokenDependencies = {
   env: process.env,
   getSecrets: getSecretsFromParameterStore,
-  getBiometricToken: getBiometricToken
+  getBiometricToken: getBiometricToken,
 };
