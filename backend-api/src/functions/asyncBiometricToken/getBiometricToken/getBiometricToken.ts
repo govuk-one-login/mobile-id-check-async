2d199816cfe5fcc5a2ebc7fb87b38838ce34fadb
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-01-14 12:46:44 +0000

    Update getBiometricToken type name

--- backend-api/src/functions/asyncBiometricToken/getBiometricToken/getBiometricToken.ts
+++ backend-api/src/functions/asyncBiometricToken/getBiometricToken/getBiometricToken.ts
@@ -1,7 +1,7 @@
 import { Result, successResult } from "../../utils/result"
 
-export type IGetBiometricToken = (url: string, submitterKey: string) => Promise<Result<string, void>>
+export type GetBiometricToken = (url: string, submitterKey: string) => Promise<Result<string, void>>
 
-export const getBiometricToken: IGetBiometricToken = async () => {
+export const getBiometricToken: GetBiometricToken = async () => {
   return successResult('mockBiometricToken')
 }
