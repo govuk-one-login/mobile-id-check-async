5abcd1b3677592cff35c05255f703ebd4de224b9
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-01-14 12:47:07 +0000

    Adds getBiometricToken to lambda deps

--- backend-api/src/functions/asyncBiometricToken/handlerDependencies.ts
+++ backend-api/src/functions/asyncBiometricToken/handlerDependencies.ts
@@ -1,12 +1,16 @@
 import { GetSecrets } from "../common/config/secrets";
 import { getSecretsFromParameterStore } from "../adapters/getSecretsFromParameterStore";
+import { getBiometricToken, GetBiometricToken } from "./getBiometricToken/getBiometricToken";
 
 export type IAsyncBiometricTokenDependencies = {
   env: NodeJS.ProcessEnv;
   getSecrets: GetSecrets;
+  getBiometricToken: GetBiometricToken;
 };
+
 
 export const runtimeDependencies: IAsyncBiometricTokenDependencies = {
   env: process.env,
   getSecrets: getSecretsFromParameterStore,
+  getBiometricToken: getBiometricToken
 };
