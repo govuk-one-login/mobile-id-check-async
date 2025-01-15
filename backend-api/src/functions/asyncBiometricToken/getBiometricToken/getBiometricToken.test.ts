8a1369f0b579117a944e7c01fd83a470346e789d
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-01-15 11:09:03 +0000

    Add test describe blocks for getBiometricToken

--- backend-api/src/functions/asyncBiometricToken/getBiometricToken/getBiometricToken.test.ts
+++ backend-api/src/functions/asyncBiometricToken/getBiometricToken/getBiometricToken.test.ts
@@ -2,6 +2,32 @@ import { successResult } from "../../utils/result";
 import { getBiometricToken } from "./getBiometricToken";
 
 describe("getBiometricToken", () => {
+  describe("Given there is an error making network request", () => {
+    it("Returns an empty failure", async () => {
+      /// Write test
+    })
+  })
+
+  describe("Given the response is invalid", () => {
+    describe("Given response is undefined", () => {
+      it("Returns an empty failure", async () => {
+        /// Write test
+      })
+    })
+
+    describe("Given response body is undefined", () => {
+      it("Returns an empty failure", async () => {
+        /// Write test
+      })
+    })
+
+    describe("Given response body cannot be parsed", () => {
+      it("Returns an empty failure", async () => {
+        /// Write test
+      })
+    })
+  })
+
   it("Returns successResult containing a string", async () => {
     const result = await getBiometricToken("mockUrl", "mockSubmitterKey");
 
