125f8f9dbc06fa7b7b725f88f638e7d5f50831de
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-01-15 11:47:21 +0000

    Adds http request logic and return access token

--- backend-api/src/functions/asyncBiometricToken/getBiometricToken/getBiometricToken.ts
+++ backend-api/src/functions/asyncBiometricToken/getBiometricToken/getBiometricToken.ts
@@ -1,10 +1,47 @@
-import { Result, successResult } from "../../utils/result";
+import {
+  HttpMethod,
+  sendHttpRequest,
+} from "../../services/http/sendHttpRequest";
+import { emptyFailure, Result, successResult } from "../../utils/result";
 
 export type GetBiometricToken = (
   url: string,
   submitterKey: string,
 ) => Promise<Result<string, void>>;
 
-export const getBiometricToken: GetBiometricToken = async () => {
-  return successResult("mockBiometricToken");
+export const getBiometricToken: GetBiometricToken = async (
+  url: string,
+  submitterKey: string,
+) => {
+  const readIdUrl = `${url}/oauth/token?grant_type=client_credentials`;
+  const headers = {
+    "X-Innovalor-Authorization": submitterKey,
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+
+  const httpRequest = {
+    url: readIdUrl,
+    method: "POST" as HttpMethod,
+    headers: headers,
+  };
+
+  let response;
+  try {
+    response = await sendHttpRequest(httpRequest);
+  } catch {
+    return emptyFailure();
+  }
+
+  if (response == null || response.body == null) {
+    return emptyFailure();
+  }
+
+  let parsedBody;
+  try {
+    parsedBody = JSON.parse(response.body);
+  } catch {
+    return emptyFailure();
+  }
+
+  return successResult(parsedBody.access_token);
 };
