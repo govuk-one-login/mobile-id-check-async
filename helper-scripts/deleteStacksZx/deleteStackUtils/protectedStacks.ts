521a869aa13dfd95b8dad909eed054d45a5a6c84
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-01-24 02:13:04 +0000

    add more values to protected stacks

--- helper-scripts/deleteStacksZx/deleteStackUtils/protectedStacks.ts
+++ helper-scripts/deleteStacksZx/deleteStackUtils/protectedStacks.ts
@@ -1,5 +1,9 @@
 export const protectedStacks = [
   "mob-sts-mock",
+  "mob-sts-mock-tir",
+  "mob-sts-mock-pl",
   "mob-async-backend",
+  "mob-async-backend-tir",
+  "mob-async-backend-pl",
   "mob-async-backend-cf-dist",
 ];
