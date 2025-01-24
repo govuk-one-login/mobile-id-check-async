12b7892a41db681af14b762e3689b4276d811233
Tiernan Mulberry <tiernan.mulberry@digital.cabinet-office.gov.uk>
2025-01-24 10:57:33 +0000

    [DCMAW-10844] Organise async backend resources into sub-templates (#345)

---
+++ backend-api/infra/README.md
@@ -0,0 +1,35 @@
+## Cloudformation template structure
+
+The `template.yaml` contains the Cloudformation resources that are deployed into AWS. This is made up of smaller `*.yaml` files that live in the `backend-api/infra` directory. 
+
+The `*.yaml` files in the `backend-api/infra` folder are organised by technology. There is also a `parent.yaml` which defines the Conditionals, Mappings and Globals.
+
+### Generating the template.yaml
+When any changes are made to the `*.yaml` files in the `backend/infra` folder, the `backend-api/template.yaml` needs to be regenerated:
+
+```bash
+# From /backend-api
+npm run build:infra
+```
+
+This script finds all `*.yaml` files in the `backend-api/infra` folder. If you add/remove/update a `.yaml` file in this directory the script will update the `backend-api/template.yaml` automatically.
+
+Note: this script is also run in the pre-commit hook to ensure that the `backend-api/template.yaml` is up-to-date.
+
+Merging templates requires rain to be installed. See https://github.com/aws-cloudformation/rain for installation instructions.
+
+### Adding or updating new infrastructure
+1) If the infrastructure already exists, update the `infra/*.yaml` file with the required changes.
+2) If there isn't, create a file in the relative folder and add resource into it.
+3) Generate the `template.yaml`
+
+### Deleting infrastructure
+1) Locate the infrastructure to be removed in the `infra` folder.
+2) Check if it uses resources from the `parent.yaml` and remove these, if no other resources depend on it.
+3) Delete the `infra/*.yaml` file if empty
+4) Generate the `template.yaml`
+
+### Infrastructure tests
+Infrastructure tests target the `backend-api/template.yaml` given this is the template that is deployed to AWS. 
+
+The exception to this is a test in `tests/infra-tests/template.test.ts` that validates the `backend-api/template.yaml` contains the same resources defined in all `*.yaml` files in the `backend-api/infra` folder.
---
+++ backend-api/tests/infra-tests/template.test.ts
@@ -0,0 +1,164 @@
+import { existsSync, readFileSync, readdirSync } from "fs";
+import * as path from "path";
+import { isDeepStrictEqual } from "util";
+import * as cfnParse from "yaml-cfn";
+import {
+  findDifferences,
+  readAllTemplates,
+  readTemplate,
+} from "../testUtils/testFunctions";
+
+const templateFilePath = path.join(__dirname, "../../template.yaml");
+const infraFolder = path.join(__dirname, "../../infra");
+const parentFilePath = path.join(infraFolder, "parent.yaml");
+
+describe("Template", () => {
+  it("should have a template.yaml file", () => {
+    const mainTemplate = readTemplate(templateFilePath);
+    expect(mainTemplate).toBeDefined();
+  });
+
+  it("should have a parent.yaml file", () => {
+    const parentTemplate = readTemplate(parentFilePath);
+    expect(parentTemplate).toBeDefined();
+  });
+
+  it("compare parameters of parent and sub templates", () => {
+    const parent = readTemplate(templateFilePath);
+    const composed = readAllTemplates(infraFolder, parent);
+
+    const parentKeys = Object.keys(parent.Parameters).sort();
+    const composedKeys = Object.keys(composed.Parameters).sort();
+
+    expect(parentKeys).toEqual(composedKeys);
+
+    parentKeys.forEach((key) => {
+      expect(
+        isDeepStrictEqual(parent.Parameters[key], composed.Parameters[key]),
+      ).toBe(true);
+    });
+  });
+
+  it("compare mappings of parent and sub templates", () => {
+    const main = readTemplate(templateFilePath);
+    const parent = readTemplate(parentFilePath);
+
+    const mainKeys = Object.keys(main.Mappings).sort();
+    const parentKeys = Object.keys(parent.Mappings).sort();
+
+    expect(mainKeys).toEqual(parentKeys);
+
+    parentKeys.forEach((key) => {
+      console.log("main " + key, main.Mappings[key]);
+      console.log("parent " + key, parent.Mappings[key]);
+      expect(isDeepStrictEqual(main.Mappings[key], parent.Mappings[key])).toBe(
+        true,
+      );
+    });
+  });
+
+  it("compare conditions of parent and sub templates", () => {
+    const main = readTemplate(templateFilePath);
+    const parent = readTemplate(parentFilePath);
+
+    const mainKeys = Object.keys(main.Conditions).sort();
+    const parentKeys = Object.keys(parent.Conditions).sort();
+
+    expect(mainKeys).toEqual(parentKeys);
+
+    parentKeys.forEach((key) => {
+      console.log("main " + key, main.Conditions[key]);
+      console.log("parent " + key, parent.Conditions[key]);
+      expect(
+        isDeepStrictEqual(main.Conditions[key], parent.Conditions[key]),
+      ).toBe(true);
+    });
+  });
+
+  it("compare globals of parent and sub templates", () => {
+    const main = readTemplate(templateFilePath);
+    const parent = readTemplate(parentFilePath);
+
+    const mainKeys = Object.keys(main.Globals).sort();
+    const parentKeys = Object.keys(parent.Globals).sort();
+
+    expect(mainKeys).toEqual(parentKeys);
+
+    parentKeys.forEach((key) => {
+      console.log("main " + key, main.Globals[key]);
+      console.log("parent " + key, parent.Globals[key]);
+      expect(isDeepStrictEqual(main.Globals[key], parent.Globals[key])).toBe(
+        true,
+      );
+    });
+  });
+
+  it("compare outputs of parent and sub templates", () => {
+    const main = readTemplate(templateFilePath);
+    const parent = readTemplate(parentFilePath);
+
+    const mainKeys = Object.keys(main.Outputs).sort();
+    const parentKeys = Object.keys(parent.Outputs).sort();
+
+    expect(mainKeys).toEqual(parentKeys);
+
+    parentKeys.forEach((key) => {
+      console.log("main " + key, main.Outputs[key]);
+      console.log("parent " + key, parent.Outputs[key]);
+      expect(isDeepStrictEqual(main.Outputs[key], parent.Outputs[key])).toBe(
+        true,
+      );
+    });
+  });
+
+  it("sub templates should only have resources", () => {
+    readdirSync(infraFolder, { withFileTypes: true }).forEach((item) => {
+      if (
+        item.isFile() &&
+        item.name.endsWith(".yaml") &&
+        !item.name.includes("parent") &&
+        !item.name.includes("template")
+      ) {
+        const template = cfnParse.yamlParse(
+          readFileSync(path.join(infraFolder, item.name), "utf8"),
+        );
+        const allowedKeys = ["Resources"];
+        const invalidKeys = Object.keys(template).filter(
+          (key) => !allowedKeys.includes(key),
+        );
+
+        expect(invalidKeys).toHaveLength(0);
+      }
+    });
+  });
+
+  it("parent template should not have any Resources", () => {
+    const parent = cfnParse.yamlParse(readFileSync(parentFilePath, "utf8"));
+    const resources = parent.Resources || {};
+    const resourceKeys = Object.keys(resources);
+
+    expect(resourceKeys).toHaveLength(1);
+    expect(resourceKeys[0]).toBe("NullResource");
+    expect(resources.NullResource).toEqual({
+      Type: "AWS::CloudFormation::WaitConditionHandle",
+      Condition: "Never",
+    });
+  });
+
+  it("should match template.yaml with sum of its parts", () => {
+    const mainTemplate = readTemplate(templateFilePath);
+    const parentTemplate = readTemplate(parentFilePath);
+    const composedTemplate = readAllTemplates(infraFolder, parentTemplate);
+
+    const differences = findDifferences(
+      "Resources",
+      mainTemplate.Resources,
+      composedTemplate.Resources,
+    );
+
+    if (differences.length > 0) {
+      // differences.forEach(diff => console.log(diff));
+      expect(differences.length).toBe(0); // This will fail with a message showing the differences
+    }
+  });
+});
