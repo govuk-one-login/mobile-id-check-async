babd9985c14bf67f6a708dea33b676ae184cbe86
tmulberry-gds <tiernan.mulberry@digital.cabinet-office.gov.uk>
2025-01-24 12:08:32 +0000

    HOTIFX: Removing unit tests from pipeline

--- backend-api/run-tests.sh
+++ backend-api/run-tests.sh
@@ -13,13 +13,6 @@ export STS_MOCK_API_URL=$(remove_quotes "$CFN_StsMockApiUrl")
 
 mkdir -pv results
 
-if npm run test:unit --ci --silent; then
-  cp -rf results $TEST_REPORT_ABSOLUTE_DIR
-else
-  cp -rf results $TEST_REPORT_ABSOLUTE_DIR
-  exit 1
-fi
-
 if [[ "$TEST_ENVIRONMENT" == "dev" ]] || [[ "$TEST_ENVIRONMENT" == "build" ]]; then
   if npm run test:api; then
     cp -rf results $TEST_REPORT_ABSOLUTE_DIR
