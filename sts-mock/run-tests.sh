cd8510defee984ab6c0e43bd4f4460e6823823cf
Sandy May <51717217+sandymay-dd@users.noreply.github.com>
2025-01-24 14:16:02 +0000

    DCMAW-11057: Re-apply changes to push STS test image to Dev and Build (#362)

---
+++ sts-mock/run-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eu
+
+remove_quotes() {
+  echo "$1" | tr -d '"'
+}
+
+export STS_MOCK_API_URL=$(remove_quotes "$CFN_StsMockApiUrl")
+
+if npm run test:api; then
+    cp -rf results "$TEST_REPORT_ABSOLUTE_DIR"
+else
+    cp -rf results "$TEST_REPORT_ABSOLUTE_DIR"
+    exit 1
+fi
