9244984e19af98e60f5f63ccff10002abc2ed2b7
Sandy May <51717217+sandymay-dd@users.noreply.github.com>
2025-01-26 13:04:39 +0000

    playing around

--- helper-scripts/delete-stacks/dist/delete-stacks.js
+++ helper-scripts/delete-stacks/dist/delete-stacks.js
@@ -1,2 +1,35 @@
-"use strict";
-console.log("HI");
+import inquirer from "inquirer";
+import { CloudFormationClient, DescribeStacksCommand } from "@aws-sdk/client-cloudformation";
+const questions = ["Delete a specific stack?", "Delete a collection of related stacks?"];
+const getBaseStackNames = async () => {
+    const describeStacksCommand = new DescribeStacksCommand();
+    const cloudFormationClient = new CloudFormationClient();
+    const response = await cloudFormationClient.send(describeStacksCommand);
+    if (!response.Stacks)
+        throw Error;
+    const stackNames = response.Stacks?.filter((stack) => {
+        if (stack.StackName) {
+            return stack.StackName.includes(cloudfrontStackNameIdentifier);
+        }
+    }).map((stack) => {
+        return stripCfString(stack.StackName);
+    });
+    const answer = await inquirer.prompt([{ type: "list", choices: stackNames, message: "Select an option", name: "stack name" }]);
+    console.log(answer["stack name"]);
+};
+const stackQuestion = {
+    "Delete a collection of related stacks?": {
+        response: true,
+        next: getBaseStackNames
+    },
+    "Delete a specific stack?": {
+        response: true,
+        next: () => { console.log("individual stack selected"); }
+    }
+};
+const cloudfrontStackNameIdentifier = "-backend-cf-dist";
+const stripCfString = (stackName) => {
+    return stackName.replace(cloudfrontStackNameIdentifier, "");
+};
+const singleStackOrStackCollectionResponse = await inquirer.prompt([{ type: "list", choices: questions, message: "Select an option", name: "response" }]);
+await stackQuestion[singleStackOrStackCollectionResponse.response].next();
--- helper-scripts/delete-stacks/src/delete-stacks.ts
+++ helper-scripts/delete-stacks/src/delete-stacks.ts
@@ -1,1 +1,53 @@
-console.log("HI")
+import inquirer from "inquirer"
+import { CloudFormationClient, DescribeStacksCommand, DescribeStacksCommandInput, ListStacksCommand, StackStatus } from "@aws-sdk/client-cloudformation"
+
+const questions = ["Delete a specific stack?", "Delete a collection of related stacks?"] as const
+ 
+interface StackDeletionHandler {
+  response: boolean;
+  next: (() => Promise<string[]>) | (() => void)
+}
+
+type test = typeof questions[number]
+
+const getBaseStackNames = async (): Promise<void> => {
+  const describeStacksCommand = new DescribeStacksCommand()
+  const cloudFormationClient = new CloudFormationClient()
+  const response = await cloudFormationClient.send(describeStacksCommand)
+
+  if (!response.Stacks) throw Error
+  const stackNames =  response.Stacks?.filter((stack) => {
+    if (stack.StackName) {
+      return stack.StackName.includes(cloudfrontStackNameIdentifier)
+    }
+  }).map((stack) => {
+    return stripCfString(stack.StackName!)
+  })
+
+  const answer = await inquirer.prompt([{ type: "list", choices: stackNames, message: "Select an option", name: "stack name" }])
+  console.log(answer["stack name"])
+
+
+}
+ 
+const stackQuestion: Record<test, StackDeletionHandler> = {
+  "Delete a collection of related stacks?": { 
+    response: true,
+    next: getBaseStackNames},
+    "Delete a specific stack?": {
+    response: true,
+    next: () => { console.log("individual stack selected")}
+  }
+};
+
+const cloudfrontStackNameIdentifier = "-backend-cf-dist"
+const stripCfString = (stackName: string): string => {
+  return stackName.replace(cloudfrontStackNameIdentifier, "")
+}
+
+const singleStackOrStackCollectionResponse = await inquirer.prompt<{response: test}>([{ type: "list", choices: questions, message: "Select an option", name: "response" }])
+
+
+await stackQuestion[singleStackOrStackCollectionResponse.response].next()
+
+
