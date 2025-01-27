8de3dc0c69c7bef201b1107ff61ea763c797eebd
Sandy May <51717217+sandymay-dd@users.noreply.github.com>
2025-01-27 17:30:51 +0000

    DCMAW-11005: Add generate env file

---
+++ test-resources/generate_env_file.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eu
+
+if [ $# -ge 1 ] && [ -n "$1" ] ; then
+  SAM_STACK="$1"
+else
+  SAM_STACK="test-resources" #default stack name in dev
+fi
+
+echo "Generating .env file for the $SAM_STACK stack"
+
+EVENTS_API_URL=$(aws cloudformation describe-stacks --stack-name "$SAM_STACK" --query "Stacks[0].Outputs[?OutputKey=='EventsApiUrl'].OutputValue" --output text)
+PROXY_API_URL=$(aws cloudformation describe-stacks --stack-name "$SAM_STACK" --query "Stacks[0].Outputs[?OutputKey=='ProxyApiUrl'].OutputValue" --output text)
+
+echo "EVENTS_API_URL=$EVENTS_API_URL" > .env
+echo "PROXY_API_URL=$PROXY_API_URL" >> .env
+echo "TEST_ENVIRONMENT=dev" >> .env
