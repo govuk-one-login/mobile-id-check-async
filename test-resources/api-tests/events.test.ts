7645dc3bf7643846b607566f195da78587f27c02
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2025-01-27 23:01:23 +0000

    Add event polling for 200 status test

--- test-resources/api-tests/events.test.ts
+++ test-resources/api-tests/events.test.ts
@@ -76,21 +76,22 @@ describe("GET /events", () => {
           skPrefix: `TXMA#EVENT_NAME#DCMAW_ASYNC_CRI_START`,
         };
 
-        await new Promise((resolve) => setTimeout(resolve, 5000));
-
+        const { pkPrefix, skPrefix } = params;
+        const { pk, sk, event } = (
+          await pollForEvents(pkPrefix, skPrefix, 1)
+        )[0];
         const response = await EVENTS_API_INSTANCE.get("/events", {
           params,
         });
 
         expect(response.status).toBe(200);
         expect(response.statusText).toEqual("OK");
-        expect(response.data[0].pk).toEqual(`SESSION#${sessionId}`);
-        expect(response.data[0].sk).toEqual(
+        expect(pk).toEqual(`SESSION#${sessionId}`);
+        expect(sk).toEqual(
           expect.stringContaining(
             "TXMA#EVENT_NAME#DCMAW_ASYNC_CRI_START#TIMESTAMP#",
           ),
         );
-        const event = response.data[0].event;
         expect(event).toEqual(
           expect.objectContaining({
             event_name: "DCMAW_ASYNC_CRI_START",
@@ -304,4 +305,91 @@ async function getActiveSessionAccessToken(
   );
 
   return stsMockResponse.data.access_token;
+}
+
+type Event = {
+  pk: string;
+  sk: string;
+  event: object;
+};
+
+function isValidEvent(candidate: unknown): candidate is Event {
+  return (
+    typeof candidate === "object" &&
+    candidate !== null &&
+    "pk" in candidate &&
+    typeof candidate.pk === "string" &&
+    "sk" in candidate &&
+    typeof candidate.sk === "string" &&
+    "event" in candidate &&
+    typeof candidate.event === "object"
+  );
+}
+
+async function pollForEvents(
+  partitionKey: string,
+  sortKeyPrefix: string,
+  numberOfEvents: number,
+): Promise<Event[]> {
+  async function wait(delayMillis: number): Promise<void> {
+    await new Promise((resolve) => setTimeout(resolve, delayMillis));
+  }
+
+  function currentTime() {
+    return Date.now();
+  }
+
+  function calculateExponentialBackoff(attempts: number) {
+    return Math.min(2 ** attempts * INITIAL_DELAY_MILLIS, MAX_BACKOFF_MILLIS);
+  }
+
+  const POLLING_DURATION_MILLIS = 40000; // maximum time to poll API
+  const MAX_BACKOFF_MILLIS = 10000; // maximum wait time between API calls
+  const INITIAL_DELAY_MILLIS = 500; // initial wait time before calling API
+
+  const pollEndTime = currentTime() + POLLING_DURATION_MILLIS;
+
+  let events: unknown[] = [];
+  let attempts = 0;
+  let waitTime = 0;
+
+  while (
+    events.length < numberOfEvents &&
+    currentTime() + waitTime < pollEndTime
+  ) {
+    await wait(waitTime);
+    events = await getEvents(partitionKey, sortKeyPrefix);
+
+    waitTime = calculateExponentialBackoff(attempts++);
+  }
+
+  if (events.length < numberOfEvents)
+    throw new Error(
+      `Only found ${events.length} events for pk=${partitionKey} and skPrefix=${sortKeyPrefix}. Expected to find at least ${numberOfEvents} events.`,
+    );
+
+  if (events.some((event) => !isValidEvent(event)))
+    throw new Error("Response from /events is malformed");
+
+  return events as Event[];
+}
+
+// Call /events API
+async function getEvents(
+  partitionKey: string,
+  sortKeyPrefix: string,
+): Promise<unknown[]> {
+  const response = await EVENTS_API_INSTANCE.get("events", {
+    params: {
+      pkPrefix: partitionKey,
+      skPrefix: sortKeyPrefix,
+    },
+  });
+
+  if (response.status === 403)
+    throw new Error("Not authorized to call /events");
+  if (response.status === 400) throw new Error("Invalid request to /events");
+
+  const events = response.data;
+  return Array.isArray(events) ? events : []; // If response is malformed, return empty array so polling can be retried
 }
