19934502405d24b45e7f3deda5adf10e1614a29c
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-01-29 15:41:10 +0000

    Adds test for writing criErrorEvent in eventService

--- backend-api/src/functions/services/events/tests/eventService.test.ts
+++ backend-api/src/functions/services/events/tests/eventService.test.ts
@@ -138,4 +138,81 @@ describe("Event Service", () => {
       });
     });
   });
+
+  describe("Writing CRI Error event to SQS", () => {
+    describe("Given writing event to SQS fails", () => {
+      it("Returns an error response", async () => {
+        const sqsMock = mockClient(sqsClient);
+        const eventWriter = new EventService("mockSqsQueue");
+        sqsMock.on(SendMessageCommand).rejects("Failed to write to SQS");
+
+        const result = await eventWriter.writeCriErrorEvent({
+          eventName: "DCMAW_ASYNC_CRI_4XXERROR",
+          sub: "mockSub",
+          sessionId: "mockSessionId",
+          govukSigninJourneyId: "mockGovukSigninJourneyId",
+          getNowInMilliseconds: () => 1609462861000,
+          componentId: "mockComponentId",
+        });
+
+        expect(result.isError).toBe(true);
+        expect(result.value).toStrictEqual({
+          errorMessage: "Failed to write to SQS",
+          errorCategory: ErrorCategory.SERVER_ERROR,
+        });
+
+        expect(sqsMock.call(0).args[0].input).toEqual({
+          MessageBody: JSON.stringify({
+            user: {
+              user_id: "mockSub",
+              transaction_id: "",
+              session_id: "mockSessionId",
+              govuk_signin_journey_id: "mockGovukSigninJourneyId",
+            },
+            timestamp: 1609462861,
+            event_timestamp_ms: 1609462861000,
+            event_name: "DCMAW_ASYNC_CRI_4XXERROR",
+            component_id: "mockComponentId",
+          }),
+          QueueUrl: "mockSqsQueue",
+        });
+      });
+
+      describe("Given writing to SQS is successful", () => {
+        it("Returns a log", async () => {
+          const sqsMock = mockClient(sqsClient);
+          const eventWriter = new EventService("mockSqsQueue");
+          sqsMock.on(SendMessageCommand).resolves({});
+
+          const result = await eventWriter.writeCriErrorEvent({
+            eventName: "DCMAW_ASYNC_CRI_4XXERROR",
+            sub: "mockSub",
+            sessionId: "mockSessionId",
+            govukSigninJourneyId: "mockGovukSigninJourneyId",
+            getNowInMilliseconds: () => 1609462861000,
+            componentId: "mockComponentId",
+          });
+
+          expect(result.isError).toBe(false);
+          expect(result.value).toEqual(null);
+
+          expect(sqsMock.call(0).args[0].input).toEqual({
+            MessageBody: JSON.stringify({
+              user: {
+                user_id: "mockSub",
+                transaction_id: "",
+                session_id: "mockSessionId",
+                govuk_signin_journey_id: "mockGovukSigninJourneyId",
+              },
+              timestamp: 1609462861,
+              event_timestamp_ms: 1609462861000,
+              event_name: "DCMAW_ASYNC_CRI_4XXERROR",
+              component_id: "mockComponentId",
+            }),
+            QueueUrl: "mockSqsQueue",
+          });
+        });
+      });
+    });
+  });
 });
