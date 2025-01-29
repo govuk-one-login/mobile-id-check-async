0727bf8e7a6b9b69a59ce9609d028ad0669b2695
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-01-29 15:27:59 +0000

    Correct describe block structure in eventService tests

--- backend-api/src/functions/services/events/tests/eventService.test.ts
+++ backend-api/src/functions/services/events/tests/eventService.test.ts
@@ -78,62 +78,62 @@ describe("Event Service", () => {
         });
       });
     });
+  });
 
-    describe("Writing credential token issued event to SQS", () => {
-      describe("Given writing event to SQS fails", () => {
-        it("Returns an error response", async () => {
-          const sqsMock = mockClient(sqsClient);
-          const eventWriter = new EventService("mockSqsQueue");
-          sqsMock.on(SendMessageCommand).rejects("Failed to write to SQS");
+  describe("Writing credential token issued event to SQS", () => {
+    describe("Given writing event to SQS fails", () => {
+      it("Returns an error response", async () => {
+        const sqsMock = mockClient(sqsClient);
+        const eventWriter = new EventService("mockSqsQueue");
+        sqsMock.on(SendMessageCommand).rejects("Failed to write to SQS");
 
-          const result = await eventWriter.writeCredentialTokenIssuedEvent({
-            getNowInMilliseconds: () => 1609462861000,
-            componentId: "mockComponentId",
-            eventName: "DCMAW_ASYNC_CLIENT_CREDENTIALS_TOKEN_ISSUED",
-          });
+        const result = await eventWriter.writeCredentialTokenIssuedEvent({
+          getNowInMilliseconds: () => 1609462861000,
+          componentId: "mockComponentId",
+          eventName: "DCMAW_ASYNC_CLIENT_CREDENTIALS_TOKEN_ISSUED",
+        });
 
-          expect(result.isError).toBe(true);
-          expect(result.value).toStrictEqual({
-            errorMessage: "Failed to write to SQS",
-            errorCategory: ErrorCategory.SERVER_ERROR,
-          });
+        expect(result.isError).toBe(true);
+        expect(result.value).toStrictEqual({
+          errorMessage: "Failed to write to SQS",
+          errorCategory: ErrorCategory.SERVER_ERROR,
+        });
 
-          expect(sqsMock.call(0).args[0].input).toStrictEqual({
-            MessageBody: JSON.stringify({
-              event_name: "DCMAW_ASYNC_CLIENT_CREDENTIALS_TOKEN_ISSUED",
-              component_id: "mockComponentId",
-              timestamp: 1609462861,
-              event_timestamp_ms: 1609462861000,
-            }),
-            QueueUrl: "mockSqsQueue",
-          });
+        expect(sqsMock.call(0).args[0].input).toStrictEqual({
+          MessageBody: JSON.stringify({
+            event_name: "DCMAW_ASYNC_CLIENT_CREDENTIALS_TOKEN_ISSUED",
+            component_id: "mockComponentId",
+            timestamp: 1609462861,
+            event_timestamp_ms: 1609462861000,
+          }),
+          QueueUrl: "mockSqsQueue",
         });
       });
+    });
 
-      describe("Given writing to SQS is successful", () => {
-        it("Returns a log", async () => {
-          const sqsMock = mockClient(sqsClient);
-          const eventWriter = new EventService("mockSqsQueue");
-          sqsMock.on(SendMessageCommand).resolves({});
+    describe("Given writing to SQS is successful", () => {
+      it("Returns a log", async () => {
+        const sqsMock = mockClient(sqsClient);
+        const eventWriter = new EventService("mockSqsQueue");
+        sqsMock.on(SendMessageCommand).resolves({});
 
-          const result = await eventWriter.writeCredentialTokenIssuedEvent({
-            getNowInMilliseconds: () => 1609462861000,
-            componentId: "mockComponentId",
-            eventName: "DCMAW_ASYNC_CLIENT_CREDENTIALS_TOKEN_ISSUED",
-          });
+        const result = await eventWriter.writeCredentialTokenIssuedEvent({
+          getNowInMilliseconds: () => 1609462861000,
+          componentId: "mockComponentId",
+          eventName: "DCMAW_ASYNC_CLIENT_CREDENTIALS_TOKEN_ISSUED",
+        });
 
-          expect(result.isError).toBe(false);
-          expect(result.value).toEqual(null);
+        expect(result.isError).toBe(false);
+        expect(result.value).toEqual(null);
 
-          expect(sqsMock.call(0).args[0].input).toStrictEqual({
-            MessageBody: JSON.stringify({
-              event_name: "DCMAW_ASYNC_CLIENT_CREDENTIALS_TOKEN_ISSUED",
-              component_id: "mockComponentId",
-              timestamp: 1609462861,
-              event_timestamp_ms: 1609462861000,
-            }),
-            QueueUrl: "mockSqsQueue",
-          });
+        expect(sqsMock.call(0).args[0].input).toStrictEqual({
+          MessageBody: JSON.stringify({
+            event_name: "DCMAW_ASYNC_CLIENT_CREDENTIALS_TOKEN_ISSUED",
+            component_id: "mockComponentId",
+            timestamp: 1609462861,
+            event_timestamp_ms: 1609462861000,
+          }),
+          QueueUrl: "mockSqsQueue",
         });
       });
     });
