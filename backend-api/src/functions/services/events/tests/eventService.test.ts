b0c3c8cb208fb9c8ff941ffd686aff83609c1d46
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-02-03 23:34:42 +0000

    Update to describe.each in eventService to cover each generic event

--- backend-api/src/functions/services/events/tests/eventService.test.ts
+++ backend-api/src/functions/services/events/tests/eventService.test.ts
@@ -2,17 +2,21 @@ import { mockClient } from "aws-sdk-client-mock";
 import { SendMessageCommand } from "@aws-sdk/client-sqs";
 import { EventService } from "../eventService";
 import { sqsClient } from "../sqsClient";
+import { GenericEventName } from "../types";
 
 describe("Event Service", () => {
-  describe("Writing generic event to SQS", () => {
-    describe("Given writing event to SQS fails", () => {
+  describe.each<GenericEventName>([
+    "DCMAW_ASYNC_CRI_START",
+    "DCMAW_ASYNC_CRI_4XXERROR",
+  ])("Writing generic event to SQS", (genericEvent) => {
+    describe(`Given writing ${genericEvent} event to SQS fails`, () => {
       it("Returns an error response", async () => {
         const sqsMock = mockClient(sqsClient);
         const eventWriter = new EventService("mockSqsQueue");
         sqsMock.on(SendMessageCommand).rejects("Failed to write to SQS");
 
         const result = await eventWriter.writeGenericEvent({
-          eventName: "DCMAW_ASYNC_CRI_START",
+          eventName: genericEvent,
           sub: "mockSub",
           sessionId: "mockSessionId",
           govukSigninJourneyId: "mockGovukSigninJourneyId",
@@ -35,46 +39,46 @@ describe("Event Service", () => {
             },
             timestamp: 1609462861,
             event_timestamp_ms: 1609462861000,
-            event_name: "DCMAW_ASYNC_CRI_START",
+            event_name: genericEvent,
             component_id: "mockComponentId",
           }),
           QueueUrl: "mockSqsQueue",
         });
       });
+    });
 
-      describe("Given writing to SQS is successful", () => {
-        it("Returns a log", async () => {
-          const sqsMock = mockClient(sqsClient);
-          const eventWriter = new EventService("mockSqsQueue");
-          sqsMock.on(SendMessageCommand).resolves({});
+    describe(`Given writing ${genericEvent} SQS is successful`, () => {
+      it("Returns a log", async () => {
+        const sqsMock = mockClient(sqsClient);
+        const eventWriter = new EventService("mockSqsQueue");
+        sqsMock.on(SendMessageCommand).resolves({});
 
-          const result = await eventWriter.writeGenericEvent({
-            eventName: "DCMAW_ASYNC_CRI_START",
-            sub: "mockSub",
-            sessionId: "mockSessionId",
-            govukSigninJourneyId: "mockGovukSigninJourneyId",
-            getNowInMilliseconds: () => 1609462861000,
-            componentId: "mockComponentId",
-          });
+        const result = await eventWriter.writeGenericEvent({
+          eventName: genericEvent,
+          sub: "mockSub",
+          sessionId: "mockSessionId",
+          govukSigninJourneyId: "mockGovukSigninJourneyId",
+          getNowInMilliseconds: () => 1609462861000,
+          componentId: "mockComponentId",
+        });
 
-          expect(result.isError).toBe(false);
-          expect(result.value).toEqual(null);
+        expect(result.isError).toBe(false);
+        expect(result.value).toEqual(null);
 
-          expect(sqsMock.call(0).args[0].input).toEqual({
-            MessageBody: JSON.stringify({
-              user: {
-                user_id: "mockSub",
-                transaction_id: "",
-                session_id: "mockSessionId",
-                govuk_signin_journey_id: "mockGovukSigninJourneyId",
-              },
-              timestamp: 1609462861,
-              event_timestamp_ms: 1609462861000,
-              event_name: "DCMAW_ASYNC_CRI_START",
-              component_id: "mockComponentId",
-            }),
-            QueueUrl: "mockSqsQueue",
-          });
+        expect(sqsMock.call(0).args[0].input).toEqual({
+          MessageBody: JSON.stringify({
+            user: {
+              user_id: "mockSub",
+              transaction_id: "",
+              session_id: "mockSessionId",
+              govuk_signin_journey_id: "mockGovukSigninJourneyId",
+            },
+            timestamp: 1609462861,
+            event_timestamp_ms: 1609462861000,
+            event_name: genericEvent,
+            component_id: "mockComponentId",
+          }),
+          QueueUrl: "mockSqsQueue",
         });
       });
     });
