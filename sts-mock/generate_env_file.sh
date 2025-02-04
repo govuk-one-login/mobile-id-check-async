d318709ae5399c40db5c31a51b45149cb2b41c5b
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2025-02-04 22:01:27 +0000

    Update generate_env file and add script

--- sts-mock/generate_env_file.sh
+++ sts-mock/generate_env_file.sh
@@ -2,15 +2,25 @@
 set -eu
 
 ENV="dev"
+TEST_RESOURCES="test-resources"
 
 if [ $# -ge 1 ] && [ -n "$1" ] ; then
-  SAM_STACK="$1"
+  STACK_IDENTIFIER="$1"
+  TEST_RESOURCES_STACK_NAME="${STACK_IDENTIFIER}-${TEST_RESOURCES}"
 else
-  SAM_STACK="mob-sts-mock" #default stack name in dev
+  # default stack name in dev
+  TEST_RESOURCES_STACK_NAME=$TEST_RESOURCES
 fi
 
-echo "Generating .env file for the $SAM_STACK stack"
+echo "Generating .env file for the $TEST_RESOURCES_STACK_NAME stack"
 
-STS_MOCK_API_URL=$(aws cloudformation describe-stacks --stack-name "$SAM_STACK" --query "Stacks[0].Outputs[?OutputKey=='StsMockApiUrl'].OutputValue" --output text)
+STS_MOCK_API_URL=$(aws cloudformation describe-stacks --stack-name "$TEST_RESOURCES_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='StsMockApiUrl'].OutputValue" --output text)
+PROXY_API_URL=$(aws cloudformation describe-stacks --stack-name "$TEST_RESOURCES_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='ProxyApiUrl'].OutputValue" --output text)
+SESSIONS_API_URL=$(aws cloudformation describe-stacks --stack-name "$TEST_RESOURCES_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='SessionsApiUrl'].OutputValue" --output text)
+EVENTS_API_URL=$(aws cloudformation describe-stacks --stack-name "$TEST_RESOURCES_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='EventsApiUrl'].OutputValue" --output text)
 
-echo "STS_MOCK_API_URL=$STS_MOCK_API_URL" > .env
+echo "TEST_ENVIRONMENT=dev" > .env
+echo "STS_MOCK_API_URL=$STS_MOCK_API_URL" >> .env
+echo "PROXY_API_URL=$PROXY_API_URL" >> .env
+echo "SESSIONS_API_URL=$SESSIONS_API_URL" >> .env
+echo "EVENTS_API_URL=$EVENTS_API_URL" >> .env
