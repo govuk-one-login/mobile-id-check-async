b7a0f5bc478ddf9c814a1b69ea9a8363600fd921
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2025-02-05 11:05:45 +0000

    Update backend deploy script for consistency

--- backend-api/generate_env_file.sh
+++ backend-api/generate_env_file.sh
@@ -2,21 +2,22 @@
 set -eu
 
 if [ $# -ge 1 ] && [ -n "$1" ] ; then
-  SAM_STACK="$1"
+  STACK_IDENTIFIER="$1"
+  BACKEND_STACK_NAME="${STACK_IDENTIFIER}-async-backend"
 else
-  SAM_STACK="mob-async-backend" #default stack name in dev
+  BACKEND_STACK_NAME="mob-async-backend" #default stack name in dev
 fi
 
-echo "Generating .env file for the $SAM_STACK stack"
+echo "Generating .env file for the $BACKEND_STACK_NAME stack"
 
-SESSIONS_API_URL=$(aws cloudformation describe-stacks --stack-name "$SAM_STACK" --query "Stacks[0].Outputs[?OutputKey=='SessionsApiUrl'].OutputValue" --output text)
-PROXY_API_URL=$(aws cloudformation describe-stacks --stack-name "$SAM_STACK" --query "Stacks[0].Outputs[?OutputKey=='ProxyApiUrl'].OutputValue" --output text)
-PRIVATE_API_URL=$(aws cloudformation describe-stacks --stack-name "$SAM_STACK" --query "Stacks[0].Outputs[?OutputKey=='PrivateApiUrl'].OutputValue" --output text)
-STS_MOCK_API_URL=$(aws cloudformation describe-stacks --stack-name "$SAM_STACK" --query "Stacks[0].Outputs[?OutputKey=='StsMockApiUrl'].OutputValue" --output text)
+PRIVATE_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='PrivateApiUrl'].OutputValue" --output text)
+PROXY_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='ProxyApiUrl'].OutputValue" --output text)
+SESSIONS_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='SessionsApiUrl'].OutputValue" --output text)
+STS_MOCK_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='StsMockApiUrl'].OutputValue" --output text)
 
-echo "SESSIONS_API_URL=$SESSIONS_API_URL" > .env
-echo "PROXY_API_URL=$PROXY_API_URL" >> .env
-echo "PRIVATE_API_URL=$PRIVATE_API_URL" >> .env
-echo "STS_MOCK_API_URL=$STS_MOCK_API_URL" >> .env
-echo "TEST_ENVIRONMENT=dev" >> .env
+echo "TEST_ENVIRONMENT=dev" > .env
 echo "IS_LOCAL_TEST=true" >> .env
+echo "PRIVATE_API_URL=$PRIVATE_API_URL" >> .env
+echo "PROXY_API_URL=$PROXY_API_URL" >> .env
+echo "SESSIONS_API_URL=$SESSIONS_API_URL" >> .env
+echo "STS_MOCK_API_URL=$STS_MOCK_API_URL" >> .env
