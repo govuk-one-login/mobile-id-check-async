1407bc0852c496d445907ed856cafcc1d09a6c09
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-02-07 23:34:03 +0000

    update colours

--- helper-scripts/src/stackManagementTool/styling/styling.ts
+++ helper-scripts/src/stackManagementTool/styling/styling.ts
@@ -1,27 +1,25 @@
 import { chalk, echo } from "zx";
 
 export const multiColouredText = (text: string): string => {
-  // Define a rainbow array of chalk color functions.
-  // Using custom hex colors for orange, indigo, and violet.
   const colours = [
     chalk.rgb(196, 0, 0),
-    chalk.hex("#FFA500"), // Orange
-    chalk.yellow,
-    chalk.green,
+    chalk.rgb(255, 132, 0),
+    chalk.rgb(255, 251, 0),
+    chalk.rgb(4, 255, 0),
     chalk.rgb(0, 149, 255),
-    chalk.rgb(174, 0, 255), // Indigo
-    chalk.hex("#EE82EE"), // Violet
+    chalk.rgb(170, 0, 255),
+    chalk.rgb(255, 62, 181),
   ];
+
   let result = "";
-  // Loop over each character in the text
   for (let i = 0; i < text.length; i++) {
     const char = text[i];
-    // If the character is whitespace, just add it without coloring
+    // If the character is whitespace, add it without colour
     if (char === " ") {
       result += char;
       continue;
     }
-    // Choose a color based on the character's index (cycling through the rainbow colors)
+
     const colourFn = colours[i % colours.length];
     result += colourFn(char);
   }
