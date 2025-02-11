1a7b80f6bd3abbf849167dad3a8c7c22303b3ecd
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-02-11 17:16:54 +0000

    Create lambdaHandler for finishBiometricSession

---
+++ backend-api/src/functions/asyncFinishBiometricSession/asyncFinishBiometricSession.ts
@@ -0,0 +1,30 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+import { notImplementedResponse } from "../common/lambdaResponses";
+import {
+  IAsyncFinishBiometricSessionDependencies,
+  runtimeDependencies,
+} from "./handlerDenpendencies";
+import { logger } from "../common/logging/logger";
+import { LogMessage } from "../common/logging/LogMessage";
+
+export async function lambdaHandlerConstructor(
+  _dependencies: IAsyncFinishBiometricSessionDependencies,
+  _event: APIGatewayProxyEvent,
+  context: Context,
+): Promise<APIGatewayProxyResult> {
+  logger.resetKeys();
+  logger.addContext(context);
+  logger.info(LogMessage.FINISH_BIOMETRIC_SESSION_STARTED);
+
+  logger.info(LogMessage.FINISH_BIOMETRIC_SESSION_COMPLETED);
+  return notImplementedResponse;
+}
+
+export const lambdaHandler = lambdaHandlerConstructor.bind(
+  null,
+  runtimeDependencies,
+);
