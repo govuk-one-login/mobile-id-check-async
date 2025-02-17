5b1d84141eb2bfbbb6826a006c374e0ab559ce60
tmulberry-gds <tiernan.mulberry@digital.cabinet-office.gov.uk>
2025-02-17 09:26:46 +0000

    updating test nesting

--- backend-api/tests/api-tests/finishBiometricSession.test.ts
+++ backend-api/tests/api-tests/finishBiometricSession.test.ts
@@ -2,7 +2,7 @@ import { SESSIONS_API_INSTANCE } from "./utils/apiInstance";
 import { mockBiometricSessionId, mockSessionId } from "./utils/apiTestData";
 
 describe("POST /async/finishBiometricSession", () => {
-  describe("Given there is a request", () => {
+  describe("Given there is a valid request", () => {
     it("Returns an error and 501 status code", async () => {
       const response = await SESSIONS_API_INSTANCE.post(
         "/async/finishBiometricSession",
@@ -15,23 +15,22 @@ describe("POST /async/finishBiometricSession", () => {
       expect(response.status).toBe(501);
       expect(response.data).toStrictEqual({ error: "Not Implemented" });
     });
+  });
+  describe("Given the request body is invalid", () => {
+    it("Returns an error and 400 status code", async () => {
+      const mockInvalidSessionId = "invalidSessionId";
+      const response = await SESSIONS_API_INSTANCE.post(
+        "/async/finishBiometricSession",
+        {
+          sessionId: mockInvalidSessionId,
+          biometricSessionId: mockBiometricSessionId,
+        },
+      );
 
-    describe("Given the request body is invalid", () => {
-      it("Returns an error and 400 status code", async () => {
-        const mockInvalidSessionId = "invalidSessionId";
-        const response = await SESSIONS_API_INSTANCE.post(
-          "/async/finishBiometricSession",
-          {
-            sessionId: mockInvalidSessionId,
-            biometricSessionId: mockBiometricSessionId,
-          },
-        );
-
-        expect(response.status).toBe(400);
-        expect(response.data).toStrictEqual({
-          error: "invalid_request",
-          error_description: `sessionId in request body is not a valid v4 UUID. sessionId: ${mockSessionId}`,
-        });
+      expect(response.status).toBe(400);
+      expect(response.data).toStrictEqual({
+        error: "invalid_request",
+        error_description: `sessionId in request body is not a valid v4 UUID. sessionId: ${mockSessionId}`,
       });
     });
   });
