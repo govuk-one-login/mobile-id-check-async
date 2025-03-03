36fcac5b6b737252e6a7b50ac0aa177ee313a9f2
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-03-03 16:11:53 +0000

    fix spelling mistake in sqs types

--- backend-api/src/functions/adapters/sqs/types.ts
+++ backend-api/src/functions/adapters/sqs/types.ts
@@ -1,6 +1,6 @@
-export type AsyncSqsMessages = VendorProccessingMessage;
+export type AsyncSqsMessages = VendorProcessingMessage;
 
-interface VendorProccessingMessage {
+interface VendorProcessingMessage {
   biometricSessionId: string;
   sessionId: string;
 }
