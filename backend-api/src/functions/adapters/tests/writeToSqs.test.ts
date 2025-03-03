219a2059ab26917f0701b4d21209ef0217a6ac32
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-03-03 11:13:24 +0000

    create bespoke writeToSqs function

---
+++ backend-api/src/functions/adapters/tests/writeToSqs.test.ts
@@ -0,0 +1,76 @@
+import { sqsClient, writeToSqs } from "../writeToSqs";
+import { expect } from "@jest/globals";
+import "../../../../tests/testUtils/matchers";
+import { AwsStub, mockClient } from "aws-sdk-client-mock";
+import {
+  SendMessageCommand,
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  SQSClientResolvedConfig,
+} from "@aws-sdk/client-sqs";
+import { emptyFailure, emptySuccess, Result } from "../../utils/result";
+
+describe("Write to SQS", () => {
+  let consoleDebugSpy: jest.SpyInstance;
+  let consoleErrorSpy: jest.SpyInstance;
+  let sqsMock: AwsStub<
+    ServiceInputTypes,
+    ServiceOutputTypes,
+    SQSClientResolvedConfig
+  >;
+  let result: Result<void, void>;
+
+  beforeEach(() => {
+    consoleDebugSpy = jest.spyOn(console, "debug");
+    consoleErrorSpy = jest.spyOn(console, "error");
+    sqsMock = mockClient(sqsClient);
+  });
+
+  describe("On every invocation", () => {
+    beforeEach(async () => {
+      await writeToSqs("www.mockQueueArn.com", "mockMessage");
+    });
+
+    it("Logs attempt at debug level", () => {
+      expect(consoleDebugSpy).toHaveBeenCalledWithLogFields({
+        messageCode: "MOBILE_ASYNC_WRITE_TO_SQS_ATTEMPT",
+      });
+    });
+  });
+
+  describe("Given writing to SQS fails", () => {
+    beforeEach(async () => {
+      sqsMock.on(SendMessageCommand).rejects("Failed to write to SQS");
+
+      result = await writeToSqs("www.mockQueueArn.com", "mockMessage");
+    });
+
+    it("Logs failed attempt", () => {
+      expect(consoleErrorSpy).toHaveBeenCalledWithLogFields({
+        messageCode: "MOBILE_ASYNC_WRITE_TO_SQS_FAILURE",
+      });
+    });
+
+    it("Returns emptyFailure Result", () => {
+      expect(result).toEqual(emptyFailure());
+    });
+  });
+
+  describe("Given writing to SQS succeeds", () => {
+    beforeEach(async () => {
+      sqsMock.on(SendMessageCommand).resolves({});
+
+      result = await writeToSqs("www.mockQueueArn.com", "mockMessage");
+    });
+
+    it("Logs successful attempt at debug level", () => {
+      expect(consoleDebugSpy).toHaveBeenCalledWithLogFields({
+        messageCode: "MOBILE_ASYNC_WRITE_TO_SQS_SUCCESS",
+      });
+    });
+
+    it("Returns an emptySuccess Result", () => {
+      expect(result).toEqual(emptySuccess());
+    });
+  });
+});
---
+++ backend-api/src/functions/adapters/writeToSqs.ts
@@ -0,0 +1,42 @@
+import { emptyFailure, emptySuccess, Result } from "../utils/result";
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+import { NodeHttpHandler } from "@smithy/node-http-handler";
+import { LogMessage } from "../common/logging/LogMessage";
+import { logger } from "../common/logging/logger";
+
+export const writeToSqs = async (
+  sqsQueue: string,
+  message: string,
+): Promise<Result<void, void>> => {
+  try {
+    logger.debug(LogMessage.WRITE_TO_SQS_ATTEMPT, {
+      data: {
+        sqsQueue,
+        message,
+      },
+    });
+    await sqsClient.send(
+      new SendMessageCommand({
+        QueueUrl: sqsQueue,
+        MessageBody: JSON.stringify(message),
+      }),
+    );
+  } catch (error: unknown) {
+    logger.error(LogMessage.WRITE_TO_SQS_FAILURE, {
+      error,
+    });
+    return emptyFailure();
+  }
+
+  logger.debug(LogMessage.WRITE_TO_SQS_SUCCESS);
+  return emptySuccess();
+};
+
+export const sqsClient = new SQSClient({
+  region: process.env.REGION,
+  maxAttempts: 3,
+  requestHandler: new NodeHttpHandler({
+    connectionTimeout: 5000,
+    requestTimeout: 5000,
+  }),
+});
