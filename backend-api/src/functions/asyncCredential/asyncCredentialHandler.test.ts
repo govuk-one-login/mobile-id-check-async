6f749f0601a97a7f0724deb80ef99d231688a077
Tiernan Mulberry <tiernan.mulberry@digital.cabinet-office.gov.uk>
2025-03-04 22:56:25 +0000

    resolving merge conflicts

--- backend-api/src/functions/asyncCredential/asyncCredentialHandler.test.ts
+++ backend-api/src/functions/asyncCredential/asyncCredentialHandler.test.ts
@@ -973,7 +973,6 @@ describe("Async Credential", () => {
               "DCMAW_ASYNC_CRI_START",
             );
 
-
             //Check the event config contains redirect url
             expect(mockEventService.eventConfig).toEqual(
               expect.objectContaining({
@@ -992,7 +991,6 @@ describe("Async Credential", () => {
             expect(consoleInfoSpy).toHaveBeenCalledWithLogFields({
               messageCode: "MOBILE_ASYNC_CREDENTIAL_COMPLETED",
             });
-
 
             expect(result).toStrictEqual({
               headers: { "Content-Type": "application/json" },
