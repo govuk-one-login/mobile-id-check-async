ca3f3245d0be8f7ebafe8010c160787f6304d2f1
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2025-03-12 16:32:59 +0000

    Revert changes

--- backend-api/generate_env_file.sh
+++ backend-api/generate_env_file.sh
@@ -11,22 +11,18 @@ fi
 
 echo "Generating .env file for the $BACKEND_STACK_NAME stack"
 
-STS_MOCK_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='StsMockApiUrl'].OutputValue" --output text)
 PRIVATE_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='PrivateApiUrl'].OutputValue" --output text)
 PROXY_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='ProxyApiUrl'].OutputValue" --output text)
-EVENTS_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='EventsApiUrl'].OutputValue" --output text)
 SESSIONS_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='SessionsApiUrl'].OutputValue" --output text)
-SESSION_TABLE_NAME=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='SessionsTableName'].OutputValue" --output text)
-TXMA_SQS=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='TxMASQSQueueUrl'].OutputValue" --output text)
+STS_MOCK_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='StsMockApiUrl'].OutputValue" --output text)
+EVENTS_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='EventsApiUrl'].OutputValue" --output text)
 
-echo "IS_LOCAL_TEST=true" > .env
+echo "TEST_ENVIRONMENT=dev" > .env
 {
-  echo "TEST_ENVIRONMENT=dev"
-  echo "STS_MOCK_API_URL=${STS_MOCK_API_URL}"
+  echo "IS_LOCAL_TEST=true"
   echo "PRIVATE_API_URL=${PRIVATE_API_URL}"
   echo "PROXY_API_URL=${PROXY_API_URL}"
-  echo "EVENTS_API_URL=${EVENTS_API_URL}"
   echo "SESSIONS_API_URL=${SESSIONS_API_URL}"
-  echo "SESSION_TABLE_NAME=${SESSION_TABLE_NAME}"
-  echo "TXMA_SQS=${TXMA_SQS}"
+  echo "STS_MOCK_API_URL=${STS_MOCK_API_URL}"
+  echo "EVENTS_API_URL=${EVENTS_API_URL}"
 } >> .env
