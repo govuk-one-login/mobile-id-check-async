3f542123f26e9b7e3052c86e4dcf7c7cb1ae9959
Tiernan Mulberry <tiernan.mulberry@digital.cabinet-office.gov.uk>
2025-03-18 11:50:07 +0000

    updating api tests

--- backend-api/tests/api-tests/abortSession.test.ts
+++ backend-api/tests/api-tests/abortSession.test.ts
@@ -2,6 +2,20 @@ import { SESSIONS_API_INSTANCE } from "./utils/apiInstance";
 
 describe("POST /async/abortSession", () => {
   describe("Given there is a request", () => {
+    it("Returns correct headers", async () => {
+      const response = await SESSIONS_API_INSTANCE.post("/async/abortSession");
+
+      expect(response.headers).toEqual(
+        expect.objectContaining({
+          "cache-control": "no-store",
+          "content-type": "application/json",
+          "strict-transport-security": "max-age=31536000",
+          "x-content-type-options": "nosniff",
+          "x-frame-options": "DENY",
+        }),
+      );
+    });
+
     it("Returns an error and 501 status code", async () => {
       const response = await SESSIONS_API_INSTANCE.post("/async/abortSession");
 
