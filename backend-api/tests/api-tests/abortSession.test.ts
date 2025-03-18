cef90b2080c7f88875b9a199b6cdf408cde9c69c
Tiernan Mulberry <tiernan.mulberry@digital.cabinet-office.gov.uk>
2025-03-18 12:52:14 +0000

    updating security

--- backend-api/tests/api-tests/abortSession.test.ts
+++ backend-api/tests/api-tests/abortSession.test.ts
@@ -1,26 +1,16 @@
 import { SESSIONS_API_INSTANCE } from "./utils/apiInstance";
+import { expectedSecurityHeaders } from "./utils/apiTestData";
 
 describe("POST /async/abortSession", () => {
   describe("Given there is a request", () => {
-    it("Returns correct headers", async () => {
-      const response = await SESSIONS_API_INSTANCE.post("/async/abortSession");
-
-      expect(response.headers).toEqual(
-        expect.objectContaining({
-          "cache-control": "no-store",
-          "content-type": "application/json",
-          "strict-transport-security": "max-age=31536000",
-          "x-content-type-options": "nosniff",
-          "x-frame-options": "DENY",
-        }),
-      );
-    });
-
     it("Returns an error and 501 status code", async () => {
       const response = await SESSIONS_API_INSTANCE.post("/async/abortSession");
 
       expect(response.status).toBe(501);
       expect(response.data).toStrictEqual({ error: "Not Implemented" });
+      expect(response.headers).toEqual(
+        expect.objectContaining(expectedSecurityHeaders),
+      );
     });
   });
 });
