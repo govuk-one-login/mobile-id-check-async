4f5256a67b9f2b0698f200ec887f85abfec9ae65
Tiernan Mulberry <tiernan.mulberry@digital.cabinet-office.gov.uk>
2025-03-19 14:10:32 +0000

    DCMAW-11234: POST async/abortSession request body validation (#497)

--- backend-api/src/functions/asyncAbortSession/asyncAbortSessionHandler.ts
+++ backend-api/src/functions/asyncAbortSession/asyncAbortSessionHandler.ts
@@ -3,7 +3,10 @@ import {
   APIGatewayProxyResult,
   Context,
 } from "aws-lambda";
-import { notImplementedResponse } from "../common/lambdaResponses";
+import {
+  badRequestResponse,
+  notImplementedResponse,
+} from "../common/lambdaResponses";
 import {
   IAsyncAbortSessionDependencies,
   runtimeDependencies,
@@ -11,14 +14,24 @@ import {
 import { logger } from "../common/logging/logger";
 import { LogMessage } from "../common/logging/LogMessage";
 import { setupLogger } from "../common/logging/setupLogger";
+import { validateRequestBody } from "./validateRequestBody/validateRequestBody";
 
 export async function lambdaHandlerConstructor(
   _dependencies: IAsyncAbortSessionDependencies,
-  _event: APIGatewayProxyEvent,
+  event: APIGatewayProxyEvent,
   context: Context,
 ): Promise<APIGatewayProxyResult> {
   setupLogger(context);
   logger.info(LogMessage.ABORT_SESSION_STARTED);
+
+  const validateResult = validateRequestBody(event.body);
+  if (validateResult.isError) {
+    const errorMessage = validateResult.value.errorMessage;
+    logger.error(LogMessage.ABORT_SESSION_REQUEST_BODY_INVALID, {
+      errorMessage,
+    });
+    return badRequestResponse("invalid_request", errorMessage);
+  }
 
   logger.info(LogMessage.ABORT_SESSION_COMPLETED);
   return notImplementedResponse;
