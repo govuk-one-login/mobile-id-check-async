bc949dcdb5e66b20f3fdc9d32490335f8815266e
Tiernan Mulberry <tiernan.mulberry@digital.cabinet-office.gov.uk>
2025-03-21 10:34:52 +0000

    Fixing na,ing issues with IPVCoreOutboundSQSQueueDeadLetterQueue

--- backend-api/tests/infra-tests/application.test.ts
+++ backend-api/tests/infra-tests/application.test.ts
@@ -937,7 +937,7 @@ describe("Backend application infrastructure", () => {
       const deadLetterQueueNames = [
         "TxMASQSQueueDeadLetterQueue",
         "VendorProcessingDLQ",
-        "IPVCoreOutboundDeadLetterQueue",
+        "IPVCoreOutboundSQSQueueDeadLetterQueue",
       ];
       const queueList = Object.keys(queues).filter(
         (queueName: string) => !deadLetterQueueNames.includes(queueName),
