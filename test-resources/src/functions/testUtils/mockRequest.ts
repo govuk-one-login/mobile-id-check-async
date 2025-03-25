96fe0cab227caa5453f95237cde056de010333e6
Sandy May <51717217+sandymay-dd@users.noreply.github.com>
2025-03-25 20:53:21 +0000

    Copy lambda request body data

--- test-resources/src/functions/testUtils/mockRequest.ts
+++ test-resources/src/functions/testUtils/mockRequest.ts
@@ -1,26 +1,35 @@
-import { APIGatewayEventIdentity, APIGatewayProxyEvent } from "aws-lambda";
+import { APIGatewayProxyEvent } from "aws-lambda";
 
-export function buildTokenRequest(
-  requestBody: string | undefined,
-): APIGatewayProxyEvent {
-  return {
-    httpMethod: "post",
-    body: requestBody,
+export function buildRequest(overrides?: any): APIGatewayProxyEvent {
+  const defaultRequest = {
+    httpMethod: "get",
+    body: "",
+    headers: {
+      "x-correlation-id": "correlationId",
+      "Txma-Audit-Encoded": "mockTxmaAuditEncodedHeader",
+    },
+    isBase64Encoded: false,
+    multiValueHeaders: {},
+    multiValueQueryStringParameters: {},
+    path: "/hello",
     pathParameters: {},
     queryStringParameters: {},
     requestContext: {
       accountId: "123456789012",
       apiId: "1234",
       authorizer: {},
-      httpMethod: "post",
-      identity: {} as APIGatewayEventIdentity,
-      path: "",
+      httpMethod: "get",
+      identity: { sourceIp: "1.1.1.1" },
+      path: "/hello",
       protocol: "HTTP/1.1",
       requestId: "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
       requestTimeEpoch: 1428582896000,
       resourceId: "123456",
-      resourcePath: "",
+      resourcePath: "/hello",
       stage: "dev",
     },
-  } as APIGatewayProxyEvent;
+    resource: "",
+    stageVariables: {},
+  };
+  return { ...defaultRequest, ...overrides };
 }
