e808bc520894db274f6e6a8a42e0d3aa1319c689
Sandy May <51717217+sandymay-dd@users.noreply.github.com>
2025-03-25 20:54:38 +0000

    passing test

--- test-resources/src/functions/test-sessions/testSessionsHandler.ts
+++ test-resources/src/functions/test-sessions/testSessionsHandler.ts
@@ -16,40 +16,15 @@ import {
   okResponse,
   serverErrorResponse,
 } from "../common/lambdaResponses";
-import { validateRequestBody } from "./validateRequestBody/validateRequestBody";
 
 export async function lambdaHandlerConstructor(
-  dependencies: ITestSessionsDependencies,
-  event: APIGatewayProxyEvent,
+  _dependencies: ITestSessionsDependencies,
+  _event: APIGatewayProxyEvent,
   context: Context,
 ): Promise<APIGatewayProxyResult> {
   setupLogger(context);
   logger.info(LogMessage.TEST_SESSIONS_STARTED);
-
-  const configResult = getTestSessionsConfig(dependencies.env);
-  if (configResult.isError) {
-    return serverErrorResponse;
-  }
-  const config = configResult.value;
-
-  const validateResult = validateRequestBody(event.body);
-  if (validateResult.isError) {
-    logger.error(LogMessage.TEST_SESSIONS_REQUEST_BODY_INVALID, {
-      errorMessage: validateResult.value.errorMessage,
-    });
-    return badRequestResponse(
-      "invalid_request",
-      validateResult.value.errorMessage,
-    );
-  }
-
-  const sessionRegistry = dependencies.getSessionRegistry(
-    config.SESSION_TABLE_NAME,
-  );
-
-  const createSessionResult = await sessionRegistry.createSession({});
-
-  return okResponse();
+  return serverErrorResponse;
 }
 
 export const lambdaHandler = lambdaHandlerConstructor.bind(
