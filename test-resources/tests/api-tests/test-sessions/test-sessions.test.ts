856dd6bc77a4810c69ca23acb7be013e3831d111
Sandy May <51717217+sandymay-dd@users.noreply.github.com>
2025-03-31 12:42:48 +0100

    update test sessions api tests

--- test-resources/tests/api-tests/test-sessions/test-sessions.test.ts
+++ test-resources/tests/api-tests/test-sessions/test-sessions.test.ts
@@ -1,36 +1,27 @@
-import { TEST_SESSIONS_API_INSTANCE } from "../../testUtils/apiTestHelpers";
+import { AxiosResponse } from "axios";
+import { TEST_SESSIONS_API_INSTANCE } from "../utils/apiInstances";
 
 describe("Test Sessions api tests", () => {
-  it("Returns a 400 with validation reason if request fails api validation", async () => {
-  const response = await TEST_SESSIONS_API_INSTANCE.put("/test-sessions/1", {});
-    console.log(response.data);
-    expect(response.data).toEqual({
-      error:
-        '[object has missing required properties (["clientId","clientState","createdAt","govukSigninJourneyId","issuer","sessionId","sessionState","subjectIdentifier","timeToLive"])]',
-      message: "Invalid request body",
+  describe("Given there is no sessionId path parameter", () => {
+    let response: AxiosResponse;
+    beforeEach(async() => {
+      response = await TEST_SESSIONS_API_INSTANCE.put("/test-sessions", {});
+    })
+    // note: this validation is done by the API gateway. It treats a missing path parameter as an unknown API gateway method. 
+    it("Returns a 404 Not Found response", async () => {
+      expect(response.data).toEqual({message: "No method found matching route test-sessions for http method PUT."}) 
+      expect(response.status).toBe(404);
     });
-    expect(response.status).toBe(400);
-  });
+  })
 
-  it("Returns a 201 Created if validation passes", async () => {
-    const validSession = {
-      sessionId: crypto.randomUUID(),
-      sessionState: "ASYNC_AUTH_SESSION_CREATED",
-      clientState: "mockClientState",
-      govukSigninJourneyId: "mockJourneyId",
-      subjectIdentifier: crypto.randomUUID(),
-      clientId: "mockClientId",
-      issuer: "mockIssuer",
-      createdAt: Date.now(),
-      timeToLive: Date.now() + 3600000,
-    };
-
-    const response = await TEST_SESSIONS_API_INSTANCE.put(
-      "/session",
-      validSession,
-    );
-    expect(response.data).toEqual({});
-    expect(response.status).toBe(201);
-    expect(response.statusText).toBe("Created");
-  });
-});
+  describe("Given there is a valid sessionId parameter", () => {
+    let response: AxiosResponse;
+    beforeEach(async() => {
+      response = await TEST_SESSIONS_API_INSTANCE.put("/test-sessions/1", {});
+    })
+    it("Returns a 501 Not Implemented response", async () => {
+      expect(response.data).toEqual("Not Implemented") 
+      expect(response.status).toBe(501);
+    });
+  })
+})
