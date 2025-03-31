b9229acb9db102d488a699709fdc36f4a07bd351
Sandy May <51717217+sandymay-dd@users.noreply.github.com>
2025-03-31 13:03:13 +0100

    remove session registry

--- test-resources/src/functions/common/session/SessionRegistry.ts
+++
@@ -1,8 +0,0 @@
-import { Result } from "../utils/result";
-import { SessionAttributes } from "./session";
-
-export interface SessionRegistry {
-  createSession(
-    sessionAttributes: SessionAttributes,
-  ): Promise<Result<void, void>>;
-}
