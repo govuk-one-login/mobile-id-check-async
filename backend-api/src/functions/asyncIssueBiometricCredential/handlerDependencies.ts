d337b03334b864bf6a90349d3f8e26d002896eec
Tiernan Mulberry <tiernan.mulberry@digital.cabinet-office.gov.uk>
2025-04-10 11:26:11 +0100

    DCMAW-12265: Issuing VC | Get Biometric Viewer Access Key from Parameter Store (#571)
    Co-authored-by: James Mooney <178897394+jmooney-dd@users.noreply.github.com>

--- backend-api/src/functions/asyncIssueBiometricCredential/handlerDependencies.ts
+++ backend-api/src/functions/asyncIssueBiometricCredential/handlerDependencies.ts
@@ -1,7 +1,12 @@
+import { GetSecrets } from "../common/config/secrets";
+import { getSecretsFromParameterStore } from "../adapters/aws/parameterStore/getSecretsFromParameterStore";
+
 export type IssueBiometricCredentialDependencies = {
   env: NodeJS.ProcessEnv;
+  getSecrets: GetSecrets;
 };
 
 export const runtimeDependencies: IssueBiometricCredentialDependencies = {
   env: process.env,
+  getSecrets: getSecretsFromParameterStore,
 };
