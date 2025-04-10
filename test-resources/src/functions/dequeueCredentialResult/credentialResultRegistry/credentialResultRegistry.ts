d36cec1b3d9c9c390d72257cfdcb4d1ab5440b3f
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2025-04-10 11:31:57 +0100

    Remove comment

--- test-resources/src/functions/dequeueCredentialResult/credentialResultRegistry/credentialResultRegistry.ts
+++ test-resources/src/functions/dequeueCredentialResult/credentialResultRegistry/credentialResultRegistry.ts
@@ -4,12 +4,3 @@ import { Result } from "../../common/utils/result";
 export interface ICredentialResultRegistry {
   putItem(putItemOperation: PutItemOperation): Promise<Result<void, void>>;
 }
-
-// export class CredentialResultRegistry implements ICredentialResultRegistry {
-//   putItem(
-//     item: ICredentialResult,
-//     putItemOperation: PutItemOperation,
-//   ): Promise<Result<void, void>> {
-//     throw new Error("Method not implemented.");
-//   }
-// }
