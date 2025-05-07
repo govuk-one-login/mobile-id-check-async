b270acc4186152d23b74daafd7f05cd2d4245a55
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-05-07 16:36:21 +0100

    create appendPersistentIdentifiersToLogger

---
+++ backend-api/src/functions/common/logging/helpers/appendPersistentIdentifiersToLogger.ts
@@ -0,0 +1,9 @@
+import { logger } from "../logger";
+
+export const appendPersistentIdentifiersToLogger = (attributes: {
+  biometricSessionId?: string;
+  govukSigninJourneyId?: string;
+  sessionId?: string;
+}): void => {
+  logger.appendKeys({ persistentIdentifiers: attributes });
+};
