4463581470adfb0d137c8a206b622bc5c80b7368
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2025-05-07 15:56:03 +0100

    Refactor generate_env_file.sh to make fewer network calls

--- backend-api/generate_env_file.sh
+++ backend-api/generate_env_file.sh
@@ -11,20 +11,21 @@ fi
 
 echo "Generating .env file for the $BACKEND_STACK_NAME stack"
 
-PRIVATE_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='PrivateApiUrl'].OutputValue" --output text)
-PROXY_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='ProxyApiUrl'].OutputValue" --output text)
-SESSIONS_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='SessionsApiUrl'].OutputValue" --output text)
-STS_MOCK_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='StsMockApiUrl'].OutputValue" --output text)
-EVENTS_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='EventsApiUrl'].OutputValue" --output text)
-TEST_RESOURCES_API_URL=$(aws cloudformation describe-stacks --stack-name "$BACKEND_STACK_NAME" --query "Stacks[0].Outputs[?OutputKey=='TestResourcesApiUrl'].OutputValue" --output text)
+# Retrieve backend stack outputs, format to key=value pairs, and export
+eval $(awk 'export $1' <(\
+  aws cloudformation describe-stacks \
+  --stack-name $BACKEND_STACK_NAME \
+  --query "Stacks[0].Outputs[*].{key: OutputKey, value: OutputValue}" \
+  --output text \
+  | sed -r "s/\t/=/"))
 
 echo "TEST_ENVIRONMENT=dev" > .env
 {
   echo "IS_LOCAL_TEST=true"
-  echo "PRIVATE_API_URL=${PRIVATE_API_URL}"
-  echo "PROXY_API_URL=${PROXY_API_URL}"
-  echo "SESSIONS_API_URL=${SESSIONS_API_URL}"
-  echo "STS_MOCK_API_URL=${STS_MOCK_API_URL}"
-  echo "EVENTS_API_URL=${EVENTS_API_URL}"
-  echo "TEST_RESOURCES_API_URL=${TEST_RESOURCES_API_URL}"
+  echo "PRIVATE_API_URL=${PrivateApiUrl}"
+  echo "PROXY_API_URL=${ProxyApiUrl}"
+  echo "SESSIONS_API_URL=${SessionsApiUrl}"
+  echo "STS_MOCK_API_URL=${StsMockApiUrl}"
+  echo "EVENTS_API_URL=${EventsApiUrl}"
+  echo "TEST_RESOURCES_API_URL=${TestResourcesApiUrl}"
 } >> .env
