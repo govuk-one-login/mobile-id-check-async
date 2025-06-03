07019ceebf24b2efedea6bd379470a6c8e2fe163
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-06-03 12:29:02 +0100

    Updates mob-async-backend-high-threshold-async-abort-session-4xx-api-gw to critical

--- backend-api/tests/infra-tests/application.test.ts
+++ backend-api/tests/infra-tests/application.test.ts
@@ -368,7 +368,7 @@ describe("Backend application infrastructure", () => {
         "high-threshold-async-finish-biometric-session-5xx-api-gw": true,
         "high-threshold-async-finish-biometric-session-4xx-api-gw": true,
         "high-threshold-async-abort-session-5xx-api-gw": false,
-        "high-threshold-async-abort-session-4xx-api-gw": false,
+        "high-threshold-async-abort-session-4xx-api-gw": true,
         "high-threshold-vendor-processing-dlq-age-of-oldest-message": false,
         "high-threshold-ipv-core-dlq-age-of-oldest-message": false,
         "issue-biometric-credential-lambda-invalid-sqs-event": true,
