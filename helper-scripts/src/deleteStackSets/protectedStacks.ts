a20945d1da6a36a69e7fbcccc88465a78cc79dcb
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-06-12 13:27:33 +0100

    add readid mock stack names to protected stacks

--- helper-scripts/src/deleteStackSets/protectedStacks.ts
+++ helper-scripts/src/deleteStackSets/protectedStacks.ts
@@ -6,4 +6,10 @@ export const protectedStacks = [
   "mob-async-backend-tir",
   "mob-async-backend-pl",
   "mob-async-backend-cf-dist",
+  "mob-readid-mock",
+  "mob-readid-mock-tir",
+  "mob-readid-mock-pl",
+  "mob-async-readid-proxy",
+  "mob-async-readid-proxy-tir",
+  "mob-async-readid-proxy-pl",
 ];
