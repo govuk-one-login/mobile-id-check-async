52a89863b876199df415b3d64fe5656875e7fd0e
jmooney-dd <178897394+jmooney-dd@users.noreply.github.com>
2025-06-26 10:37:05 +0100

    Revert "test console log only - to be reverted"
    This reverts commit 0f104f54630e2f01276250e00bab1506453db7a6.

--- backend-api/src/functions/tests/pact/pact.test.ts
+++ backend-api/src/functions/tests/pact/pact.test.ts
@@ -50,8 +50,6 @@ describe("Provider API contract verification", () => {
       },
     };
 
-    console.log("HERE >> process.env.GITHUB_SHA >> ", process.env.GITHUB_SHA)
-
     const verifier = new Verifier({
       consumerVersionSelectors: [
         {
