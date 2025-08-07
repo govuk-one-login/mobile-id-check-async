8fe47b5598f0258f7404f4934b75bace37d821a7
Chris Cooksley <chris.cooksley@digital.cabinet-office.gov.uk>
2025-08-07 16:36:48 +0100

    Try removing body

--- backend-api/src/functions/asyncToken/asyncTokenHandler.ts
+++ backend-api/src/functions/asyncToken/asyncTokenHandler.ts
@@ -115,11 +115,6 @@ export async function lambdaHandlerConstructor(
       "Content-Type": "application/json",
     },
     statusCode: 200,
-    body: JSON.stringify({
-      access_token: accessToken,
-      token_type: "Bearer",
-      expires_in: 3600,
-    }),
   };
 }
 
