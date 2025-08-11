072a083b27cc9dd835962d86b3a44b4329761b6c
Chris Cooksley <chris.cooksley@digital.cabinet-office.gov.uk>
2025-08-11 17:05:11 +0100

    Revert createApp changes

--- backend-api/src/functions/tests/pact/createApp.ts
+++ backend-api/src/functions/tests/pact/createApp.ts
@@ -13,18 +13,13 @@ export async function createApp(): Promise<Application> {
   app.use(express.json());
 
   app.post("/async/token", async (req: Request, res: Response) => {
-    console.log("req", req);
     const result = await tokenLambdaHandlerConstructor(
       asyncTokenDependencies.dependencies,
       buildRequest({ headers: req.headers, body: req.body }),
       buildLambdaContext(),
     );
-    console.log("result", result);
-    console.log(result.statusCode);
-    console.log(JSON.parse(result.body));
     res.status(result.statusCode);
     res.send(JSON.parse(result.body));
-    console.log("response sent");
   });
 
   app.post("/async/credential", async (req: Request, res: Response) => {
