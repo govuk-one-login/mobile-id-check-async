830586b1563ec45347c26d8dae6c90b895b5be93
Chris Cooksley <chris.cooksley@digital.cabinet-office.gov.uk>
2025-08-11 11:04:29 +0100

    Revert some changes

--- backend-api/src/functions/tests/pact/createApp.ts
+++ backend-api/src/functions/tests/pact/createApp.ts
@@ -22,8 +22,8 @@ export async function createApp(): Promise<Application> {
     console.log("result", result);
     console.log(result.statusCode);
     console.log(JSON.parse(result.body));
-    res.status(200);
-    res.send({});
+    res.status(result.statusCode);
+    res.send(JSON.parse(result.body));
     console.log("response sent");
   });
 
