7e26637a3a1cb3f1fc85e85887733e5bb7b520d1
Sandy May <51717217+sandymay-dd@users.noreply.github.com>
2026-01-28 15:35:51 +0000

    add vcIssuedEvent type

--- backend-api/src/functions/asyncIssueBiometricCredential/buildVcIssuedEvent.ts
+++ backend-api/src/functions/asyncIssueBiometricCredential/buildVcIssuedEvent.ts
@@ -1,15 +1,47 @@
-export const getVcIssuedEvent = (userId: string, sessionId: string, journeyId: string, transactionId: string) => {
+import { CredentialSubject, FailEvidence, FlaggedRecord, PassEvidence, TxmaContraIndicator } from "@govuk-one-login/mobile-id-check-biometric-credential";
+
+type VcEvidence =
+  (PassEvidence | FailEvidence) &
+  {
+    txmaContraIndicators: string[],
+    ciReasons?: string[],
+    dcmawFlagsPassport?: Record<string, unknown>
+    dcmawFlagsDL?: Record<string, unknown>
+    dcmawFlagsBRP?: Record<string, unknown>
+  }
+
+type VcIssuedTxMAEvent = {
+  event_name: "DCMAW_ASYNC_CRI_VC_ISSUED",
+  user: {
+    user_id: string,
+    session_id: string,
+    govuk_signin_journey_id: string,
+    transaction_id: string
+  },
+  timestamp: number,
+  event_timestamp_ms: number,
+  component_id: string,
+  restricted: CredentialSubject & {
+    flaggedRecord?: FlaggedRecord[]
+  },
+  extensions: {
+    redirect_uri?: string,
+    evidence: VcEvidence[]
+  },
+};
+
+export const getVcIssuedEvent = (userId: string, sessionId: string, journeyId: string, transactionId: string): VcIssuedTxMAEvent => {
   return {
     user: {
       user_id: userId,
-        session_id
-    :
-      sessionId,
-        govuk_signin_journey_id
-    :
+      session_id
+        :
+        sessionId,
+      govuk_signin_journey_id
+        :
         journeyId,
-        transaction_id
-    :
+      transaction_id
+        :
         transactionId,
     }
   }
