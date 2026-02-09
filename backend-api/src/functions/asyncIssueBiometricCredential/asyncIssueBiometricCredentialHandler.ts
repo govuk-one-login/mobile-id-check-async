96623087440a6a6f488d1e1480a626a10e973090
Sandy May <51717217+sandymay-dd@users.noreply.github.com>
2026-02-09 15:35:14 +0000

    feedback: typo

--- backend-api/src/functions/asyncIssueBiometricCredential/asyncIssueBiometricCredentialHandler.ts
+++ backend-api/src/functions/asyncIssueBiometricCredential/asyncIssueBiometricCredentialHandler.ts
@@ -638,11 +638,11 @@ const writeVcIssuedEvent = async (
   credential: BiometricCredential,
   audit: AuditData,
 ): Promise<Result<void, void>> => {
-  const sendMessageToSqsresult = await sendMessageToSqs(
+  const sendMessageToSqsResult = await sendMessageToSqs(
     txmaSqsArn,
     getVcIssuedEvent(credential, audit, sessionAttributes),
   );
-  if (sendMessageToSqsresult.isError) {
+  if (sendMessageToSqsResult.isError) {
     logger.error(LogMessage.ERROR_WRITING_AUDIT_EVENT, {
       data: { auditEventName: "DCMAW_ASYNC_CRI_VC_ISSUED" },
     });
