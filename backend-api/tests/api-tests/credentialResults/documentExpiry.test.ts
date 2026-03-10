74b82e6da16c9fe9c9057b703b6b0309ccc154e8
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2026-03-10 17:38:42 +0000

    wip - add API tests - DVLA, grace period 0

--- backend-api/tests/api-tests/credentialResults/documentExpiry.test.ts
+++ backend-api/tests/api-tests/credentialResults/documentExpiry.test.ts
@@ -16,61 +16,59 @@ describe("Driving licence expiry", () => {
   let biometricSessionId: string;
   let criTxmaEvents: EventResponse[];
   let verifiedJwt: JWTVerifyResult & ResolvedKey;
+  let expiryDate: string;
 
   describe("Expiry grace period", () => {
-    describe("Given a DVA document", () => {
-      describe("Given grace period is 0", () => {
-        describe("Given vendor checks fail", () => {
-          describe("Given document has expired", () => {
-            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
-          });
-          describe("Given document has not expired", () => {
-            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
-          });
-        });
-        describe("Given vendor checks pass", () => {
-          describe("Given document has expired", () => {
-            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
-          });
-          describe("Given document has not expired", () => {
-            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with pass evidence", () => {});
-          });
-        });
-      });
-      describe("Given grace period is greater than 0", () => {
-        describe("Given vendor checks fail", () => {
-          describe("Given document has expired and is within the grace period", () => {
-            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
-          });
-          describe("Given document has expired and is beyond the grace period", () => {
-            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
-          });
-          describe("Given document has not expired", () => {
-            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
-          });
-        });
-        describe("Given vendor checks pass", () => {
-          describe("Given document has expired and is beyond the grace period", () => {
-            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
-          });
-          describe("Given document has expired and is within the grace period", () => {
-            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
-          });
-          describe("Given document has not expired", () => {
-            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with pass evidence", () => {});
-          });
-        });
-      });
-    });
+    // describe("Given a DVA document", () => {
+    //   describe("Given grace period is 0", () => {
+    //     describe("Given vendor checks fail", () => {
+    //       describe("Given document has expired", () => {
+    //         it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
+    //       });
+    //       describe("Given document has not expired", () => {
+    //         it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
+    //       });
+    //     });
+    //     describe("Given vendor checks pass", () => {
+    //       describe("Given document has expired", () => {
+    //         it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
+    //       });
+    //       describe("Given document has not expired", () => {
+    //         it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with pass evidence", () => {});
+    //       });
+    //     });
+    //   });
+    //   describe("Given grace period is greater than 0", () => {
+    //     describe("Given vendor checks fail", () => {
+    //       describe("Given document has expired and is within the grace period", () => {
+    //         it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
+    //       });
+    //       describe("Given document has expired and is beyond the grace period", () => {
+    //         it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
+    //       });
+    //       describe("Given document has not expired", () => {
+    //         it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
+    //       });
+    //     });
+    //     describe("Given vendor checks pass", () => {
+    //       describe("Given document has expired and is beyond the grace period", () => {
+    //         it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
+    //       });
+    //       describe("Given document has expired and is within the grace period", () => {
+    //         it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
+    //       });
+    //       describe("Given document has not expired", () => {
+    //         it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with pass evidence", () => {});
+    //       });
+    //     });
+    //   });
+    // });
     describe("Given a DVLA document", () => {
       describe("Given grace period is 0", () => {
         describe("Given vendor checks fail", () => {
           describe("Given document has expired", () => {
-            let expiryDate: string;
-
             beforeEach(async () => {
               expiryDate = getIsoStringDateNDaysFromToday(-1);
-              // const mockBiometricCredential = getMockCredentialWithCustomDrivingLicenceExpiryDate(mockVcFailEvidence, expiryDate);
               ({ biometricSessionId, sessionId, subjectIdentifier } =
                 await doAsyncJourney(
                   Scenario.DRIVING_LICENCE_FAILURE_WITH_CIS,
@@ -179,15 +177,356 @@ describe("Driving licence expiry", () => {
             });
           });
           describe("Given document has not expired", () => {
-            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
+            beforeEach(async () => {
+              expiryDate = getIsoStringDateNDaysFromToday(1);
+              ({ biometricSessionId, sessionId, subjectIdentifier } =
+                await doAsyncJourney(
+                  Scenario.DRIVING_LICENCE_FAILURE_WITH_CIS,
+                  {
+                    drivingLicence: {
+                      issuedBy: "DVLA",
+                      validUntil: expiryDate,
+                    },
+                  },
+                ));
+
+              verifiedJwt = await getVerifiedJwt(subjectIdentifier);
+
+              criTxmaEvents = await pollForEvents({
+                partitionKey: `SESSION#${sessionId}`,
+                sortKeyPrefix: `TXMA#EVENT_NAME#DCMAW_ASYNC_CRI_`,
+                numberOfEvents: 4, // CRI_START, CRI_APP_START, CRI_VC_ISSUED, CRI_END
+              });
+            }, 60000);
+
+            it("Writes verified credential with fail evidence to the IPV Core outbound queue", () => {
+              const { protectedHeader, payload } = verifiedJwt;
+
+              expect(protectedHeader).toEqual({
+                alg: "ES256",
+                kid: expect.any(String),
+                typ: "JWT",
+              });
+
+              expect(payload).toEqual({
+                iat: expect.any(Number),
+                iss: `https://review-b-async.${process.env.TEST_ENVIRONMENT}.account.gov.uk`,
+                jti: expect.stringContaining("urn:uuid:"),
+                nbf: expect.any(Number),
+                sub: subjectIdentifier,
+                vc: expect.objectContaining({
+                  evidence: [
+                    expect.objectContaining({
+                      strengthScore: 3,
+                      validityScore: 0,
+                      activityHistoryScore: 0,
+                    }),
+                  ],
+                }),
+              });
+            });
+
+            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event", () => {
+              const actualEvent = getVcIssuedEventObject(criTxmaEvents);
+
+              expect(actualEvent).toStrictEqual({
+                timestamp: expect.any(Number),
+                user: {
+                  user_id: subjectIdentifier,
+                  session_id: sessionId,
+                  govuk_signin_journey_id:
+                    "44444444-4444-4444-4444-444444444444",
+                  transaction_id: biometricSessionId,
+                },
+                event_name: "DCMAW_ASYNC_CRI_VC_ISSUED",
+                event_timestamp_ms: expect.any(Number),
+                component_id: `https://review-b-async.${process.env.TEST_ENVIRONMENT}.account.gov.uk`,
+                restricted: {
+                  name: [
+                    {
+                      nameParts: expect.any(Array),
+                    },
+                  ],
+                  birthDate: [],
+                  deviceId: [
+                    {
+                      value: expect.any(String),
+                    },
+                  ],
+                  address: [expect.any(Object)],
+                },
+                extensions: {
+                  redirect_uri: "https://mockRedirectUri.com",
+                  evidence: [
+                    {
+                      type: "IdentityCheck",
+                      strengthScore: 3,
+                      validityScore: 0,
+                      activityHistoryScore: 0,
+                      ci: expect.arrayContaining([expect.any(String)]),
+                      failedCheckDetails: expect.arrayContaining([
+                        expect.objectContaining({
+                          biometricVerificationProcessLevel: 3,
+                          checkMethod: "bvr",
+                        }),
+                      ]),
+                      ciReasons: expect.arrayContaining([expect.any(Object)]),
+                      txmaContraIndicators: expect.any(Array),
+                      txn: expect.any(String),
+                    },
+                  ],
+                },
+              });
+            });
+
+            it("Writes DCMAW_ASYNC_CRI_END TxMA event", () => {
+              expectTxmaEventToHaveBeenWritten(
+                criTxmaEvents,
+                "DCMAW_ASYNC_CRI_END",
+              );
+            });
           });
         });
         describe("Given vendor checks pass", () => {
           describe("Given document has expired", () => {
-            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with fail evidence", () => {});
+            beforeEach(async () => {
+              expiryDate = getIsoStringDateNDaysFromToday(-1);
+              ({ biometricSessionId, sessionId, subjectIdentifier } =
+                await doAsyncJourney(Scenario.DRIVING_LICENCE_SUCCESS, {
+                  drivingLicence: {
+                    issuedBy: "DVLA",
+                    validUntil: expiryDate,
+                  },
+                }));
+
+              verifiedJwt = await getVerifiedJwt(subjectIdentifier);
+
+              criTxmaEvents = await pollForEvents({
+                partitionKey: `SESSION#${sessionId}`,
+                sortKeyPrefix: `TXMA#EVENT_NAME#DCMAW_ASYNC_CRI_`,
+                numberOfEvents: 4, // CRI_START, CRI_APP_START, CRI_VC_ISSUED, CRI_END
+              });
+            }, 60000);
+
+            it("Writes verified credential with fail evidence to the IPV Core outbound queue", () => {
+              const { protectedHeader, payload } = verifiedJwt;
+
+              expect(protectedHeader).toEqual({
+                alg: "ES256",
+                kid: expect.any(String),
+                typ: "JWT",
+              });
+
+              expect(payload).toEqual({
+                iat: expect.any(Number),
+                iss: `https://review-b-async.${process.env.TEST_ENVIRONMENT}.account.gov.uk`,
+                jti: expect.stringContaining("urn:uuid:"),
+                nbf: expect.any(Number),
+                sub: subjectIdentifier,
+                vc: expect.objectContaining({
+                  credentialSubject: expect.objectContaining({
+                    drivingPermit: [
+                      expect.objectContaining({
+                        expiryDate,
+                        issuedBy: "DVLA",
+                      }),
+                    ],
+                  }),
+                  evidence: [
+                    expect.objectContaining({
+                      strengthScore: 3,
+                      validityScore: 0,
+                      activityHistoryScore: 0,
+                    }),
+                  ],
+                }),
+              });
+            });
+
+            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event", () => {
+              const actualEvent = getVcIssuedEventObject(criTxmaEvents);
+
+              expect(actualEvent).toStrictEqual({
+                timestamp: expect.any(Number),
+                user: {
+                  user_id: subjectIdentifier,
+                  session_id: sessionId,
+                  govuk_signin_journey_id:
+                    "44444444-4444-4444-4444-444444444444",
+                  transaction_id: biometricSessionId,
+                },
+                event_name: "DCMAW_ASYNC_CRI_VC_ISSUED",
+                event_timestamp_ms: expect.any(Number),
+                component_id: `https://review-b-async.${process.env.TEST_ENVIRONMENT}.account.gov.uk`,
+                restricted: {
+                  name: [
+                    {
+                      nameParts: expect.any(Array),
+                    },
+                  ],
+                  birthDate: [expect.any(Object)],
+                  deviceId: [
+                    {
+                      value: expect.any(String),
+                    },
+                  ],
+                  address: [expect.any(Object)],
+                  drivingPermit: [
+                    expect.objectContaining({
+                      expiryDate,
+                      issuedBy: "DVLA",
+                    }),
+                  ],
+                },
+                extensions: {
+                  redirect_uri: "https://mockRedirectUri.com",
+                  evidence: [
+                    {
+                      type: "IdentityCheck",
+                      strengthScore: 3,
+                      validityScore: 0,
+                      activityHistoryScore: 0,
+                      ci: [],
+                      failedCheckDetails: expect.arrayContaining([
+                        expect.objectContaining({
+                          biometricVerificationProcessLevel: 3,
+                          checkMethod: "bvr",
+                        }),
+                      ]),
+                      ciReasons: [],
+                      txmaContraIndicators: expect.any(Array),
+                      txn: expect.any(String),
+                    },
+                  ],
+                },
+              });
+            });
+
+            it("Writes DCMAW_ASYNC_CRI_END TxMA event", () => {
+              expectTxmaEventToHaveBeenWritten(
+                criTxmaEvents,
+                "DCMAW_ASYNC_CRI_END",
+              );
+            });
           });
           describe("Given document has not expired", () => {
-            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event with pass evidence", () => {});
+            beforeEach(async () => {
+              expiryDate = getIsoStringDateNDaysFromToday(1);
+              ({ biometricSessionId, sessionId, subjectIdentifier } =
+                await doAsyncJourney(Scenario.DRIVING_LICENCE_SUCCESS, {
+                  drivingLicence: {
+                    issuedBy: "DVLA",
+                    validUntil: expiryDate,
+                  },
+                }));
+
+              verifiedJwt = await getVerifiedJwt(subjectIdentifier);
+
+              criTxmaEvents = await pollForEvents({
+                partitionKey: `SESSION#${sessionId}`,
+                sortKeyPrefix: `TXMA#EVENT_NAME#DCMAW_ASYNC_CRI_`,
+                numberOfEvents: 4, // CRI_START, CRI_APP_START, CRI_VC_ISSUED, CRI_END
+              });
+            }, 60000);
+
+            it("Writes verified credential with pass evidence to the IPV Core outbound queue", () => {
+              const { protectedHeader, payload } = verifiedJwt;
+
+              expect(protectedHeader).toEqual({
+                alg: "ES256",
+                kid: expect.any(String),
+                typ: "JWT",
+              });
+
+              expect(payload).toEqual({
+                iat: expect.any(Number),
+                iss: `https://review-b-async.${process.env.TEST_ENVIRONMENT}.account.gov.uk`,
+                jti: expect.stringContaining("urn:uuid:"),
+                nbf: expect.any(Number),
+                sub: subjectIdentifier,
+                vc: expect.objectContaining({
+                  credentialSubject: expect.objectContaining({
+                    drivingPermit: [
+                      expect.objectContaining({
+                        expiryDate,
+                        issuedBy: "DVLA",
+                      }),
+                    ],
+                  }),
+                  evidence: [
+                    expect.objectContaining({
+                      strengthScore: 3,
+                      validityScore: 2,
+                      activityHistoryScore: 1,
+                    }),
+                  ],
+                }),
+              });
+            });
+
+            it("Writes DCMAW_ASYNC_CRI_VC_ISSUED TxMA event", () => {
+              const actualEvent = getVcIssuedEventObject(criTxmaEvents);
+
+              expect(actualEvent).toStrictEqual({
+                timestamp: expect.any(Number),
+                user: {
+                  user_id: subjectIdentifier,
+                  session_id: sessionId,
+                  govuk_signin_journey_id:
+                    "44444444-4444-4444-4444-444444444444",
+                  transaction_id: biometricSessionId,
+                },
+                event_name: "DCMAW_ASYNC_CRI_VC_ISSUED",
+                event_timestamp_ms: expect.any(Number),
+                component_id: `https://review-b-async.${process.env.TEST_ENVIRONMENT}.account.gov.uk`,
+                restricted: {
+                  name: [
+                    {
+                      nameParts: expect.any(Array),
+                    },
+                  ],
+                  birthDate: [expect.any(Object)],
+                  deviceId: [
+                    {
+                      value: expect.any(String),
+                    },
+                  ],
+                  address: [expect.any(Object)],
+                  drivingPermit: [
+                    expect.objectContaining({
+                      expiryDate,
+                      issuedBy: "DVLA",
+                    }),
+                  ],
+                },
+                extensions: {
+                  redirect_uri: "https://mockRedirectUri.com",
+                  evidence: [
+                    {
+                      type: "IdentityCheck",
+                      strengthScore: 3,
+                      validityScore: 2,
+                      activityHistoryScore: 1,
+                      checkDetails: expect.arrayContaining([
+                        expect.objectContaining({
+                          biometricVerificationProcessLevel: 3,
+                          checkMethod: "bvr",
+                        }),
+                      ]),
+                      txmaContraIndicators: expect.any(Array),
+                      txn: expect.any(String),
+                    },
+                  ],
+                },
+              });
+            });
+
+            it("Writes DCMAW_ASYNC_CRI_END TxMA event", () => {
+              expectTxmaEventToHaveBeenWritten(
+                criTxmaEvents,
+                "DCMAW_ASYNC_CRI_END",
+              );
+            });
           });
         });
       });
