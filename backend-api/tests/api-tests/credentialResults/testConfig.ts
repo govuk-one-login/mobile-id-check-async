e49e33566d4e13218bdc924a50ec3b842cb9e46f
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2026-03-26 12:30:23 +0000

    fix test config

--- backend-api/tests/api-tests/credentialResults/testConfig.ts
+++ backend-api/tests/api-tests/credentialResults/testConfig.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config({ quiet: true });
+
 export const EXPECTED_DVLA_DRIVING_LICENCE_EXPIRY_GRACE_PERIOD_IN_DAYS: number =
   Number(process.env.EXPECTED_DVLA_DRIVING_LICENCE_EXPIRY_GRACE_PERIOD_IN_DAYS);
 export const EXPIRY_GRACE_PERIOD_IN_DAYS_PLUS_1 =
