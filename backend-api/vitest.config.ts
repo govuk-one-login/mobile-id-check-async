4d5bbbc744de2fd59c9967618b465151a0258164
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2026-03-31 12:52:15 +0100

    Add initial `vitest.config.ts`

---
+++ backend-api/vitest.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    coverage: {
+      enabled: true,
+      exclude: ["**/testUtils/**", "**/node-modules/**"],
+    },
+    environment: "node",
+    include: ["**/*.test.ts"],
+    clearMocks: true,
+    reporters: [
+      "default",
+      [
+        "junit",
+        {
+          outputFile: "./results/report.xml",
+        },
+      ],
+    ],
+  },
+});
+
+process.env.POWERTOOLS_DEV = "true";
+process.env.AWS_LAMBDA_LOG_LEVEL = "DEBUG";
