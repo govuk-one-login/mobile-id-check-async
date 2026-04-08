7cc1bd1e3115a252ea9242e86c6d868ae8f19109
Jason Humbert <178896864+jhumbert-dd@users.noreply.github.com>
2026-04-08 15:23:58 +0100

    update vitest config and npm scripts

--- test-resources/vitest.config.ts
+++ test-resources/vitest.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "vitest/config";
 
+const reportName = process.env.VITEST_JUNIT_FILE || "report.xml";
+const reporters =
+  process.env.GITHUB_ACTIONS === "true"
+    ? ["default", "github-actions"]
+    : ["default", "junit"];
+
 export default defineConfig({
   test: {
+    silent: "passed-only",
     include: ["**/*.test.ts"],
     setupFiles: ["dotenv/config", "testSetup.ts"],
-    reporters: ["junit", "default"],
+    reporters,
+    outputFile: {
+      junit: `results/${reportName}`,
+    },
     coverage: {
       provider: "v8",
       include: ["**/*.ts"],
