6210479f1db9c66c47af8e1f1bfb36be4fb31918
Paul Hadfield <paul.hadfield@digital.cabinet-office.gov.uk>
2026-06-01 11:00:09 +0100

    Remove unused helper function.

--- backend-api/tests/api-tests/credentialResults/drivingLicence/DVLA/dvlaExpiryTestSetup.ts
+++ backend-api/tests/api-tests/credentialResults/drivingLicence/DVLA/dvlaExpiryTestSetup.ts
@@ -5,20 +5,3 @@ export const EXPECTED_DVLA_DRIVING_LICENCE_EXPIRY_GRACE_PERIOD_IN_DAYS: number =
   Number(process.env.EXPECTED_DVLA_DRIVING_LICENCE_EXPIRY_GRACE_PERIOD_IN_DAYS);
 export const EXPIRY_GRACE_PERIOD_IN_DAYS_PLUS_1 =
   EXPECTED_DVLA_DRIVING_LICENCE_EXPIRY_GRACE_PERIOD_IN_DAYS + 1;
-
-function throwIfExpiryGracePeriodNotValid(expiryGracePeriod: any) {
-  if (expiryGracePeriod == null)
-    throw new Error(
-      "EXPECTED_DVLA_DRIVING_LICENCE_EXPIRY_GRACE_PERIOD_IN_DAYS is not set in test environment variables",
-    );
-  if (
-    !Number.isNaN(expiryGracePeriod) &&
-    typeof expiryGracePeriod === "number" &&
-    expiryGracePeriod >= 0
-  )
-    return;
-
-  throw new Error(
-    `EXPECTED_DVLA_DRIVING_LICENCE_EXPIRY_GRACE_PERIOD_IN_DAYS is not valid: ${expiryGracePeriod}`,
-  );
-}
